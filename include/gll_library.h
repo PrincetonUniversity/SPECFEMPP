85dc03ecd3d2cdeb4936e61ec7d26569a7ab71fa
Rohit R Kakodkar <rk9481@tigercpu.princeton.edu>
2022-07-13 10:41:50 -0400

    Refactor GLL class

--- include/gll_library.h
+++ include/gll_library.h
@@ -54,28 +54,6 @@ double pndglj(const double z, const int n);
 void zwgljd(Kokkos::View<double *, Kokkos::LayoutRight, Kokkos::HostSpace> z,
             Kokkos::View<double *, Kokkos::LayoutRight, Kokkos::HostSpace> w,
             const int np, const double alpha, const double beta);
-
-/**
- * @warning GLL class is still in progress,
- * will get to it as I understand the Fortran code more
- * and what things need to be added here
- *
- */
-class gll {
-public:
-  gll();
-  gll(const double alpha, const double beta);
-  gll(const double alpha, const double beta, const int ngll);
-  gll(const double alpha, const double beta, const int ngllx, const int ngllz);
-
-private:
-  double alpha, beta;
-  int ngllx, ngllz;
-  Kokkos::View<double *, Kokkos::LayoutRight, Kokkos::HostSpace> xigll, zigll,
-      wxgll, wzgll;
-  Kokkos::View<double **, Kokkos::LayoutRight, Kokkos::HostSpace> hprime_xx,
-      hprime_zz, hprimewgll_xx, hprimewgll_zz;
-};
 } // namespace gll_library
 
 #endif // GLL_LIBRARY_H
--- src/gll_library.cpp
+++ src/gll_library.cpp
@@ -4,42 +4,6 @@
 #include <iostream>
 #include <stdexcept>
 
-gll_library::gll::gll() : alpha(0.0), beta(0.0), ngllx(5), ngllz(5){};
-
-gll_library::gll::gll(const double alpha, const double beta)
-    : alpha(alpha), beta(beta), ngllx(5), ngllz(5) {
-  if ((alpha <= -1.0) || (beta <= -1.0))
-    throw std::invalid_argument("alpha and beta must be greater than -1");
-}
-
-gll_library::gll::gll(const double alpha, const double beta, const int ngll)
-    : alpha(alpha), beta(beta), ngllx(ngll), ngllz(ngll) {
-  if (ngll <= 1)
-    throw std::invalid_argument("Minimum number of Gauss-Labatto points is 2");
-  if (ngll <= 2)
-    throw std::invalid_argument(
-        "Minimum number of Gauss-Lobatto points for the SEM is 3");
-  if ((alpha <= -1.0) || (beta <= -1.0))
-    throw std::invalid_argument("alpha and beta must be greater than -1");
-}
-
-gll_library::gll::gll(const double alpha, const double beta, const int ngllx,
-                      const int ngllz)
-    : alpha(alpha), beta(beta), ngllx(ngllx), ngllz(ngllz) {
-  if (ngllx <= 1)
-    throw std::invalid_argument("Minimum number of Gauss-Labatto points is 2");
-  if (ngllx <= 2)
-    throw std::invalid_argument(
-        "Minimum number of Gauss-Lobatto points for the SEM is 3");
-  if ((alpha <= -1.0) || (beta <= -1.0))
-    throw std::invalid_argument("alpha and beta must be greater than -1");
-  if (ngllx != ngllz) {
-    throw std::invalid_argument(
-        "ngllz != ngllx: Cannot handle unstructured meshes due to mismatch in "
-        "polynomials at boundaries");
-  }
-}
-
 double gll_library::pnleg(const double z, const int n) {
   // Generate Lagendre polynomials using recurrance relation
   // (l+1)P_(l+1)(x)-(2l+1)xP_l(x)+lP_(l-1)(x)=0
