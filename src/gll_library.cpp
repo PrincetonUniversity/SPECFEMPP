622b817b3c713934894035c65ee1f83c1e1f947a
Rohit R Kakodkar <rk9481@tigercpu.princeton.edu>
2022-07-13 09:25:35 -0400

    Overloaded constructors for gll class

--- src/gll_library.cpp
+++ src/gll_library.cpp
@@ -4,10 +4,16 @@
 #include <iostream>
 #include <stdexcept>
 
-gll_library::gll::gll() : alpha(0.0), beta(0.0), ngll(5){};
+gll_library::gll::gll() : alpha(0.0), beta(0.0), ngllx(5), ngllz(5){};
+
+gll_library::gll::gll(const double alpha, const double beta)
+    : alpha(alpha), beta(beta), ngllx(5), ngllz(5) {
+  if ((alpha <= -1.0) || (beta <= -1.0))
+    throw std::invalid_argument("alpha and beta must be greater than -1");
+}
 
 gll_library::gll::gll(const double alpha, const double beta, const int ngll)
-    : alpha(alpha), beta(beta), ngll(ngll) {
+    : alpha(alpha), beta(beta), ngllx(ngll), ngllz(ngll) {
   if (ngll <= 1)
     throw std::invalid_argument("Minimum number of Gauss-Labatto points is 2");
   if (ngll <= 2)
@@ -15,6 +21,23 @@ gll_library::gll::gll(const double alpha, const double beta, const int ngll)
         "Minimum number of Gauss-Lobatto points for the SEM is 3");
   if ((alpha <= -1.0) || (beta <= -1.0))
     throw std::invalid_argument("alpha and beta must be greater than -1");
+}
+
+gll_library::gll::gll(const double alpha, const double beta, const int ngllx,
+                      const int ngllz)
+    : alpha(alpha), beta(beta), ngllx(ngllx), ngllz(ngllz) {
+  if (ngllx <= 1)
+    throw std::invalid_argument("Minimum number of Gauss-Labatto points is 2");
+  if (ngllx <= 2)
+    throw std::invalid_argument(
+        "Minimum number of Gauss-Lobatto points for the SEM is 3");
+  if ((alpha <= -1.0) || (beta <= -1.0))
+    throw std::invalid_argument("alpha and beta must be greater than -1");
+  if (ngllx != ngllz) {
+    throw std::invalid_argument(
+        "ngllz != ngllx: Cannot handle unstructured meshes due to mismatch in "
+        "polynomials at boundaries");
+  }
 }
 
 double gll_library::pnleg(const double z, const int n) {
