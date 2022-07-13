761084deffdf1e28f5412e84caa110815a02a673
Rohit R Kakodkar <rk9481@tigercpu.princeton.edu>
2022-07-13 13:43:53 -0400

    Typographical error

--- include/lagrange_poly.h
+++ include/lagrange_poly.h
@@ -1,4 +1,5 @@
-#include <Kokkos_Core.h>
+#include "../include/gll_library.h"
+#include <Kokkos_Core.hpp>
 
 namespace Lagrange {
 
@@ -36,31 +37,30 @@ void compute_lagrange_interpolants(
 void compute_lagrange_derivatives_GLL(
     Kokkos::View<double **, Kokkos::LayoutRight, Kokkos::HostSpace> hprime_ii,
     const Kokkos::View<double *, Kokkos::LayoutRight, Kokkos::HostSpace> xigll,
-    const int ngll) {}
+    const int ngll) {
 
-int degpoly = ngll - 1;
-for (int i = 0; i < ngll; i++) {
-  for (int j = 0; j < ngll; j++) {
-    if (i == 0 && j == 0) {
-      hprime_ii(i, j) = -1.0 * static_cast<double>(degpoly) *
-                        (static_cast<double>(degpoly) + 1.0) * 0.25;
-    } else if (i == degpoly &&j = degpoly) {
-      hprime_ii(i, j) = -1.0 * static_cast<double>(degpoly) *
-                        (static_cast<double>(degpoly) + 1.0) * 0.25;
-    } else if (i == j) {
-      hprime_ii(i, j) = 0.0;
-    } else {
-      hprime_ii(i, j) =
-          gll_library::pnleg(xigll(j), degpoly) /
-              (gll_library::pnleg(xigll(i), degpoly) * (zgll(j) - zgll(i))) +
-          (1.0 - zgll(j) * zgll(j)) * gll_library::pndleg(zgll(j), degpoly) /
-              (dble(degpoly) * (dble(degpoly) + 1.0) *
-               gll_library::pnleg(zgll(i), degpoly) * (zgll(j) - zgll(i)) *
-               (zgll(j) - zgll(i)));
+  int degpoly = ngll - 1;
+  for (int i = 0; i < ngll; i++) {
+    for (int j = 0; j < ngll; j++) {
+      if (i == 0 && j == 0) {
+        hprime_ii(i, j) = -1.0 * static_cast<double>(degpoly) *
+                          (static_cast<double>(degpoly) + 1.0) * 0.25;
+      } else if (i == degpoly &&j = degpoly) {
+        hprime_ii(i, j) = -1.0 * static_cast<double>(degpoly) *
+                          (static_cast<double>(degpoly) + 1.0) * 0.25;
+      } else if (i == j) {
+        hprime_ii(i, j) = 0.0;
+      } else {
+        hprime_ii(i, j) =
+            gll_library::pnleg(xigll(j), degpoly) /
+                (gll_library::pnleg(xigll(i), degpoly) * (zgll(j) - zgll(i))) +
+            (1.0 - zgll(j) * zgll(j)) * gll_library::pndleg(zgll(j), degpoly) /
+                (dble(degpoly) * (dble(degpoly) + 1.0) *
+                 gll_library::pnleg(zgll(i), degpoly) * (zgll(j) - zgll(i)) *
+                 (zgll(j) - zgll(i)));
+      }
     }
   }
+  return;
 }
-
-return;
-
 } // namespace Lagrange
