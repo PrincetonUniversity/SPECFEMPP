4d30a712fa9ee8998261db4d3b7947727ab03235
Rohit R Kakodkar <rk9481@tigercpu.princeton.edu>
2022-07-13 13:38:40 -0400

    Lagrange interpolants and derivs

---
+++ src/lagrange_poly.cpp
@@ -0,0 +1,15 @@
+#include <Kokkos_Core.h>
+
+namespace Lagrange {
+
+void compute_lagrange_interpolants(
+    Kokkos::View<double *, Kokkos::LayoutRight, Kokkos::HostSpace> h,
+    Kokkos::View<double *, Kokkos::LayoutRight, Kokkos::HostSpace> hprime,
+    const double xi, const int ngll,
+    const Kokkos::View<double *, Kokkos::LayoutRight, Kokkos::HostSpace> xigll);
+void compute_lagrange_derivatives_GLL(
+    Kokkos::View<double **, Kokkos::LayoutRight, Kokkos::HostSpace> hprime_ii,
+    const Kokkos::View<double *, Kokkos::LayoutRight, Kokkos::HostSpace> xigll,
+    const int ngll);
+
+} // namespace Lagrange
