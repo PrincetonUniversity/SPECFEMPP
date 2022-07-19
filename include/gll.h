cb027ec0227b6d908d50708dfb7cfe44dd3e9ae3
Rohit R Kakodkar <rk9481@tigercpu.princeton.edu>
2022-07-19 13:34:44 -0400

    GLL class

--- include/gll.h
+++ include/gll.h
@@ -13,6 +13,19 @@ public:
   gll(const double alpha, const double beta);
   gll(const double alpha, const double beta, const int ngll);
   gll(const double alpha, const double beta, const int ngllx, const int ngllz);
+  void set_derivation_matrices();
+  Kokkos::View<double *, Kokkos::LayoutRight, Kokkos::HostSpace>
+  get_xigll() const;
+  Kokkos::View<double *, Kokkos::LayoutRight, Kokkos::HostSpace>
+  get_zigll() const;
+  Kokkos::View<double *, Kokkos::LayoutRight, Kokkos::HostSpace>
+  get_wxgll() const;
+  Kokkos::View<double *, Kokkos::LayoutRight, Kokkos::HostSpace>
+  get_wzgll() const;
+  Kokkos::View<double **, Kokkos::LayoutRight, Kokkos::HostSpace>
+  get_hprime_xx() const;
+  Kokkos::View<double **, Kokkos::LayoutRight, Kokkos::HostSpace>
+  get_hprime_zz() const;
 
 private:
   double alpha, beta;
@@ -20,6 +33,7 @@ private:
   Kokkos::View<double *, Kokkos::LayoutRight, Kokkos::HostSpace> xigll, zigll,
       wxgll, wzgll;
   Kokkos::View<double **, Kokkos::LayoutRight, Kokkos::HostSpace> hprime_xx,
-      hprime_zz, hprimewgll_xx, hprimewgll_zz;
+      hprime_zz;
+  void set_allocations();
 };
 } // namespace gll
