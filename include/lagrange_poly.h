97cc74714decf2d5f5f683e4c8acd65fa53ad405
Rohit R Kakodkar <rk9481@tigercpu.princeton.edu>
2022-08-15 16:49:55 -0400

    Docs update

--- include/lagrange_poly.h
+++ include/lagrange_poly.h
@@ -12,13 +12,13 @@ namespace Lagrange {
 /**
  * @brief Compute lagrange interpolants and its derivatives at xi
  *
- * @param h Values of lagrange interpolants calculated at xi i.e. h[i] =
- * l_{i}(xi) && h.extent() == N && h.rank() == 1
- * @param hprime Values of derivatives of lagrange interpolants calculated at xi
- * i.e. h[i] = l_{i}(xi) && h.extent() == N && h.rank() == 1
+ * @param h HostMirror1d Values of lagrange interpolants calculated at xi i.e.
+ * h[i] = l_{i}(xi) && h.extent() == N && h.rank() == 1
+ * @param hprime HostMirror1d Values of derivatives of lagrange interpolants
+ * calculated at xi i.e. h[i] = l_{i}(xi) && h.extent() == N && h.rank() == 1
  * @param xi Value to calculate lagrange interpolants and its derivatives
  * @param ngll Order used to approximate functions
- * @param xigll Array of GLL points generally calculated using
+ * @param xigll HostMirror1d Array of GLL points generally calculated using
  * gll_library::zwgljd
  */
 void compute_lagrange_interpolants(HostMirror1d h, HostMirror1d hprime,
@@ -29,10 +29,11 @@ void compute_lagrange_interpolants(HostMirror1d h, HostMirror1d hprime,
  * @brief Compute the derivatives of Lagrange functions at GLL points
  * @note Please refer Nisser-Meyer et.al. 2007 equation (A19)
  *
- * @param hprime_ii Derivates of lagrange polynomials at GLL points i.e.
- * \f$h'(i,j) = \partial_{\xi}l_{j}(\xi_i)\f$ where \f$\xi_i \epsilon
+ * @param hprime_ii HostMirror2d Derivates of lagrange polynomials at GLL points
+ * i.e. \f$h'(i,j) = \partial_{\xi}l_{j}(\xi_i)\f$ where \f$\xi_i \epsilon
  * \textrm{GLL points}\f$
- * @param xigll GLL points generally calculated using gll_library::zwgljd
+ * @param xigll HostMirror1d GLL points generally calculated using
+ * gll_library::zwgljd
  * @param ngll Order used to approximate functions
  */
 void compute_lagrange_derivatives_GLL(HostMirror2d hprime_ii,
@@ -45,10 +46,12 @@ void compute_lagrange_derivatives_GLL(HostMirror2d hprime_ii,
  * @warning This function hasn't been tested yet. Could be potential source of
  * error.
  *
- * @param hprimeBar_ii Derivates of Jacobi polynomials at GLJ points at \f$
+ * @param hprimeBar_ii HostMirror2d Derivates of Jacobi polynomials at GLJ
+ * points at \f$
  * (\alpha, \beta) = (0,1) i.e. \f$h'(i,j) = \partial_{\xi}l_{j}(\xi_i)\f$ where
  * \f$\xi_i \epsilon \textrm{GLJpoints}\f$
- * @param xiglj GLJ points generally calculated using gll_library::zwgljd
+ * @param xiglj HostMirror1d GLJ points generally calculated using
+ * gll_library::zwgljd
  * @param nglj Order used to approximate functions
  */
 void compute_jacobi_derivatives_GLJ(HostMirror2d hprimeBar_ii,
