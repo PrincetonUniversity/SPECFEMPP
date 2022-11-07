f2cfb542d03415454667e83fe90b34a8c62b69b8
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2022-11-07 10:32:29 -0500

    Documented kokkos abstractions and mesh

--- include/mesh.h
+++ include/mesh.h
@@ -16,26 +16,74 @@
 
 namespace specfem {
 
+/**
+ * @brief Mesh Interface
+ *
+ * The mesh is implemented as a C++ struct. The mesh struct defines all the
+ * variables nacessary to compute mass and stiffness matrices. The mesh is
+ * grouped into several logical structs which help keep the code
+ * concise/readable/maintainable.
+ *
+ */
 struct mesh {
-  int npgeo, nspec, nproc;
-  specfem::HostView2d<type_real> coorg;
-  specfem::materials::material_ind material_ind;
-  specfem::interfaces::interface interface;
-  specfem::boundaries::absorbing_boundary abs_boundary;
-  specfem::properties parameters;
-  specfem::surfaces::acoustic_free_surface acfree_surface;
-  specfem::boundaries::forcing_boundary acforcing_boundary;
-  specfem::elements::tangential_elements tangential_nodes;
-  specfem::elements::axial_elements axial_nodes;
+
+  int npgeo; ///< Total number of spectral element control nodes
+  int nspec; ///< Total number of spectral elements
+  int nproc; ///< Total number of processors
+  specfem::HostView2d<type_real> coorg; ///< (x,z) for every spectral element
+                                        ///< control node
+
+  specfem::materials::material_ind material_ind; ///< Struct used to store
+                                                 ///< material information for
+                                                 ///< every spectral element
+
+  specfem::interfaces::interface interface; ///< Struct used to store data
+                                            ///< required to implement MPI
+                                            ///< interfaces
+
+  specfem::boundaries::absorbing_boundary abs_boundary; ///< Struct used to
+                                                        ///< store data required
+                                                        ///< to implement
+                                                        ///< absorbing boundary
+
+  specfem::properties parameters; ///< Struct to store simulation launch
+                                  ///< parameters
+
+  specfem::surfaces::acoustic_free_surface
+      acfree_surface; ///< Struct used to store data required to implement
+                      ///< acoustic free surface
+
+  specfem::boundaries::forcing_boundary
+      acforcing_boundary; ///< Struct used to store data required to implement
+                          ///< acoustic forcing boundary
+
+  specfem::elements::tangential_elements tangential_nodes; ///< Defines
+                                                           ///< tangential nodes
+
+  specfem::elements::axial_elements axial_nodes; ///< Defines axial nodes
+
+  /**
+   * @warning Need to add documentation for compute
+   *
+   */
   specfem::compute::compute compute;
 
+  /**
+   * @brief Default mesh constructor
+   *
+   */
   mesh(){};
+
+  /**
+   * @brief Construct mesh from a fortran binary database file
+   *
+   * @param filename Fortran binary database filename
+   * @param quadx quadrature in x-direction
+   * @param quadz quadrature in z-direction
+   * @param mpi pointer to MPI object to manage communication
+   */
   mesh(const std::string filename, const quadrature::quadrature &quadx,
        const quadrature::quadrature &quadz, const specfem::MPI *mpi);
-
-  void setup(std::vector<specfem::material *> &materials,
-             const quadrature::quadrature &quadx,
-             const quadrature::quadrature &quadz, specfem::MPI *mpi);
 };
 } // namespace specfem
 
