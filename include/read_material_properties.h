bbb8681dd43c4d2f23718943453ea08856384c42
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2022-11-07 11:42:34 -0500

    Documentated material class and helpers

--- include/read_material_properties.h
+++ include/read_material_properties.h
@@ -9,9 +9,19 @@
 
 namespace IO {
 
+/**
+ * Read material properties from a fotran binary database
+ *
+ * @param stream Stream object for fortran binary file buffered to materials
+ * section
+ * @param numat Number of materials to be read
+ * @param mpi Pointer to MPI object
+ * @return std::vector<specfem::material *> Pointer to material objects read
+ * from the database file
+ */
 std::vector<specfem::material *>
 read_material_properties(std::ifstream &stream, const int numat,
                          const specfem::MPI *mpi);
-}
+} // namespace IO
 
 #endif
