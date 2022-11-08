19eb2fd4fa07a3addc02a175fb459342887bc643
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2022-11-08 14:27:14 -0500

    Updated latex rendering

--- include/shape_functions.h
+++ include/shape_functions.h
@@ -4,16 +4,13 @@
 #include "../include/kokkos_abstractions.h"
 #include <Kokkos_Core.hpp>
 
-using HostView1d = specfem::HostView1d<type_real>;
-using HostView2d = specfem::HostView2d<type_real>;
-
 namespace shape_functions {
 
 /**
  * @brief Compute shape functions at particular point (xi, gamma)
  *
- * @param xi xi value of the point
- * @param gamma gamma value of the point
+ * @param xi \f$ \xi \f$ value of the point
+ * @param gamma \f$ \gamma \f$ value of the point
  * @param ngod Total number of control nodes per element
  * @return specfem::HostView1d<type_real> View defining the shape function
  */
@@ -22,11 +19,12 @@ define_shape_functions(const double xi, const double gamma, const int ngod);
 /**
  * @brief Derivates of shape function at a particular point
  *
- * @param xi xi value of the point
- * @param gamma gamma value of the point
+ * @param xi \f$ \xi \f$ value of the point
+ * @param gamma \f$ \gamma \f$ value of the point
  * @param ngod Total number of control nodes per element
  * @return specfem::HostView2d<type_real> View defining the derivative of shape
- * function (dN/dxi, dN/dgamma)
+ * function (\f$ \partial N/\partial \xi \f$, \f$ \partial N/\partial \gamma
+ * \f$)
  */
 specfem::HostView2d<type_real>
 define_shape_functions_derivatives(const double xi, const double gamma,
