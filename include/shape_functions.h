eace7ee84c340f5c57cde2137f57ba4890f7a3a4
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2022-11-08 14:00:06 -0500

    Documented jacobian and shape functions

--- include/shape_functions.h
+++ include/shape_functions.h
@@ -9,11 +9,28 @@ using HostView2d = specfem::HostView2d<type_real>;
 
 namespace shape_functions {
 
-HostView1d define_shape_functions(const double xi, const double gamma,
-                                  const int ngod);
-HostView2d define_shape_functions_derivatives(const double xi,
-                                              const double gamma,
-                                              const int ngod);
+/**
+ * @brief Compute shape functions at particular point (xi, gamma)
+ *
+ * @param xi xi value of the point
+ * @param gamma gamma value of the point
+ * @param ngod Total number of control nodes per element
+ * @return specfem::HostView1d<type_real> View defining the shape function
+ */
+specfem::HostView1d<type_real>
+define_shape_functions(const double xi, const double gamma, const int ngod);
+/**
+ * @brief Derivates of shape function at a particular point
+ *
+ * @param xi xi value of the point
+ * @param gamma gamma value of the point
+ * @param ngod Total number of control nodes per element
+ * @return specfem::HostView2d<type_real> View defining the derivative of shape
+ * function (dN/dxi, dN/dgamma)
+ */
+specfem::HostView2d<type_real>
+define_shape_functions_derivatives(const double xi, const double gamma,
+                                   const int ngod);
 } // namespace shape_functions
 
 #endif // SHAPE_FUNCTIONS_H
