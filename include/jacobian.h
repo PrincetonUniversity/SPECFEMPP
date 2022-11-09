8702b7f5fef699b9ac5b22889a9b32b62de43c65
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2022-11-09 10:28:26 -0500

    Removed commented code

--- include/jacobian.h
+++ include/jacobian.h
@@ -152,18 +152,6 @@ type_real compute_jacobian(const specfem::HostTeam::member_type &teamMember,
                            const specfem::HostScratchView2d<type_real> s_coorg,
                            const int ngnod,
                            const specfem::HostView2d<type_real> dershape2D);
-
-// HostArray2d compute_partial_derivatives(const HostArray1d greekcoord,
-//                                         const HostArray2d coorg,
-//                                         const int ngod);
-// double jacobian::compute_jacobian(const HostArray2d partial_der);
-// double compute_jacobian(const HostArray1d greekcoord, const HostArray2d
-// coorg,
-//                         const int ngod);
-
-// HostArray2d compute_inverse_partial_derivatives(const HostArray1d greekcoord,
-//                                                 const HostArray2d coorg,
-//                                                 const int ngod);
 } // namespace jacobian
 
 #endif
