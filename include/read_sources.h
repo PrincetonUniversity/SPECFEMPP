a801955feda8bf8d3d38e64447744cacb217478a
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2022-11-22 13:13:57 -0500

    Added read sources routines

--- include/read_sources.h
+++ include/read_sources.h
@@ -1,2 +1,13 @@
+#ifndef READ_SOURCES_H
+#define READ_SOURCES_H
+
 #include "../include/config.h"
-#include <yaml-cpp>
+#include "../include/source.h"
+#include <vector>
+
+namespace specfem {
+std::vector<specfem::sources::source *> read_sources(std::string sources_file,
+                                                     specfem::MPI *mpi)
+}
+
+#endif
