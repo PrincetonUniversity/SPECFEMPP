c6ee0231cfb3e31e61efbc3e9314ba361a859520
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2023-04-21 00:20:27 -0400

    Added simd_abstractions and static dims template

--- include/config.h
+++ include/config.h
@@ -1,11 +1,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-using type_real = float;
+using type_real = double;
 const static int ndim{ 2 };
 const static int fint{ 4 }, fdouble{ 8 }, fbool{ 4 }, fchar{ 512 };
 const static bool use_best_location{ true };
-
-using simd_type = type_real;
 
 #endif
