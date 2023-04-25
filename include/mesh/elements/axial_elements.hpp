e052d465d2d3ca77cd4bc21f74a79e18912b536c
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2023-04-25 01:20:38 -0400

    Updated elements

---
+++ include/mesh/elements/axial_elements.hpp
@@ -0,0 +1,27 @@
+#ifndef _AXIAL_ELEMENTS_HPP
+#define _AXIAL_ELEMENTS_HPP
+
+#include "kokkos_abstractions.h"
+#include "specfem_mpi.h"
+
+namespace specfem {
+namespace mesh {
+namespace elements {
+/**
+ * Define axial elements
+ *
+ * @note Need to still document this section
+ *
+ */
+struct axial_elements {
+  specfem::kokkos::HostView1d<bool> is_on_the_axis;
+  axial_elements(){};
+  axial_elements(const int nspec);
+  axial_elements(std::ifstream &stream, const int nelem_on_the_axis,
+                 const int nspec, const specfem::MPI::MPI *mpi);
+};
+} // namespace elements
+} // namespace mesh
+} // namespace specfem
+
+#endif
---
+++ src/mesh/elements/axial_elements.cpp
@@ -0,0 +1,31 @@
+#include "fortran_IO.h"
+#include "mesh/elements/elements.hpp"
+#include "specfem_mpi.h"
+
+specfem::mesh::elements::axial_elements::axial_elements(const int nspec) {
+  this->is_on_the_axis = specfem::kokkos::HostView1d<bool>(
+      "specfem::mesh::axial_element::is_on_the_axis", nspec);
+
+  for (int inum = 0; inum < nspec; inum++) {
+    this->is_on_the_axis(nspec) = false;
+  }
+
+  return;
+}
+
+specfem::mesh::elements::axial_elements::axial_elements(
+    std::ifstream &stream, const int nelem_on_the_axis, const int nspec,
+    const specfem::MPI::MPI *mpi) {
+  int ispec;
+
+  *this = specfem::mesh::elements::axial_elements(nspec);
+  for (int inum = 0; inum < nelem_on_the_axis; inum++) {
+    specfem::fortran_IO::fortran_read_line(stream, &ispec);
+    if (ispec < 0 || ispec > nspec - 1)
+      throw std::runtime_error(
+          "ispec out of range when reading axial elements");
+    this->is_on_the_axis(ispec) = true;
+  }
+
+  return;
+}
