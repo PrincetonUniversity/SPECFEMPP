ed2985c74406d85cf25c380a0f9e97963642ee43
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2023-08-24 16:35:08 -0400

    Removed non-merged implementations

--- include/domain/acoustic/interface.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef _ACOUSTIC_DOMAIN_INTERFACE_HPP
-#define _ACOUSTIC_DOMAIN_INTERFACE_HPP
-
-#include "acoustic_domain.hpp"
-#include "acoustic_domain.tpp"
-
-#endif
--- include/domain/elastic/interface.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef _ELASTIC_DOMAIN_INTERFACE_HPP
-#define _ELASTIC_DOMAIN_INTERFACE_HPP
-
-#include "elastic_domain.hpp"
-#include "elastic_domain.tpp"
-
-#endif
