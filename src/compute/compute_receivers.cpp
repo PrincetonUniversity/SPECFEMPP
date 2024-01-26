fa5c8f3c46b319e7c65c2c58e82d605814659efb
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2024-01-26 13:30:05 -0500

    Reimplemented compute receivers

--- src/compute/compute_receivers.cpp
+++ src/compute/compute_receivers.cpp
@@ -8,91 +8,64 @@
 #include <Kokkos_Core.hpp>
 #include <vector>
 
+specfem::compute::receivers::receivers(const int nreceivers,
+                                       const int max_sig_step, const int N,
+                                       const int n_seis_types)
+    : receiver_array("specfem::compute::receiver::receiver_array", nreceivers,
+                     ndim, N, N),
+      h_receiver_array(Kokkos::create_mirror_view(receiver_array)),
+      ispec_array("specfem::compute::receivers::ispec_array", nreceivers),
+      h_ispec_array(Kokkos::create_mirror_view(ispec_array)),
+      cos_recs("specfem::compute::receivers::cos_recs", nreceivers),
+      h_cos_recs(Kokkos::create_mirror_view(cos_recs)),
+      sin_recs("specfem::compute::receivers::sin_recs", nreceivers),
+      h_sin_recs(Kokkos::create_mirror_view(sin_recs)),
+      seismogram("specfem::compute::receivers::seismogram", max_sig_step,
+                 n_seis_types, nreceivers, ndim),
+      h_seismogram(Kokkos::create_mirror_view(seismogram)),
+      seismogram_types("specfem::compute::receivers::seismogram_types",
+                       n_seis_types),
+      h_seismogram_types(Kokkos::create_mirror_view(seismogram_types)),
+      receiver_field("specfem::compute::receivers::receiver_field",
+                     max_sig_step, nreceivers, n_seis_types, ndim, N, N),
+      h_receiver_field(Kokkos::create_mirror_view(receiver_field)) {}
+
 specfem::compute::receivers::receivers(
+    const int max_sig_step,
     const std::vector<std::shared_ptr<specfem::receivers::receiver> >
         &receivers,
     const std::vector<specfem::enums::seismogram::type> &stypes,
-    const specfem::quadrature::quadrature *quadx,
-    const specfem::quadrature::quadrature *quadz, const type_real xmax,
-    const type_real xmin, const type_real zmax, const type_real zmin,
-    const int max_sig_step, specfem::MPI::MPI *mpi) {
+    const specfem::compute::mesh &mesh) {
 
-  // Get  sources which lie in processor
-  std::vector<std::shared_ptr<specfem::receivers::receiver> > my_receivers;
-  for (auto &receiver : receivers) {
-    if (receiver->get_islice() == mpi->get_rank()) {
-      my_receivers.push_back(receiver);
-    }
-  }
+  const int nreceivers = receivers.size();
+  const int N = mesh.quadratures.gll.N;
+  const int n_seis_types = stypes.size();
 
-  // allocate source array view
-  this->receiver_array = specfem::kokkos::DeviceView4d<type_real>(
-      "specfem::compute::receiver::receiver_array", my_receivers.size(),
-      quadz->get_N(), quadx->get_N(), ndim);
+  *this =
+      specfem::compute::receivers(nreceivers, max_sig_step, N, n_seis_types);
 
-  this->h_receiver_array = Kokkos::create_mirror_view(this->receiver_array);
-
-  this->ispec_array = specfem::kokkos::DeviceView1d<int>(
-      "specfem::compute::receivers::ispec_array", my_receivers.size());
-
-  this->h_ispec_array = Kokkos::create_mirror_view(ispec_array);
-
-  this->cos_recs = specfem::kokkos::DeviceView1d<type_real>(
-      "specfem::compute::receivers::cos_recs", my_receivers.size());
-
-  this->h_cos_recs = Kokkos::create_mirror_view(cos_recs);
-
-  this->sin_recs = specfem::kokkos::DeviceView1d<type_real>(
-      "specfem::compute::receivers::sin_recs", my_receivers.size());
-
-  this->h_sin_recs = Kokkos::create_mirror_view(sin_recs);
-
-  this->field = specfem::kokkos::DeviceView5d<type_real>(
-      "specfem::compute::receivers::field", stypes.size(), my_receivers.size(),
-      2, quadz->get_N(), quadx->get_N());
-
-  this->seismogram = specfem::kokkos::DeviceView4d<type_real>(
-      "specfem::compute::receivers::seismogram", max_sig_step, stypes.size(),
-      my_receivers.size(), 2);
-
-  this->h_seismogram = Kokkos::create_mirror_view(this->seismogram);
-
-  // store source array for sources in my islice
-  for (int irec = 0; irec < my_receivers.size(); irec++) {
-
-    my_receivers[irec]->check_locations(xmax, xmin, zmax, zmin, mpi);
-
+  for (int irec = 0; irec < nreceivers; irec++) {
     auto sv_receiver_array = Kokkos::subview(
         this->h_receiver_array, irec, Kokkos::ALL, Kokkos::ALL, Kokkos::ALL);
-    my_receivers[irec]->compute_receiver_array(quadx, quadz, sv_receiver_array);
+    receivers[irec]->compute_receiver_array(mesh, sv_receiver_array);
 
-    this->h_ispec_array(irec) = my_receivers[irec]->get_ispec();
-    this->h_cos_recs(irec) = my_receivers[irec]->get_cosine();
-    this->h_sin_recs(irec) = my_receivers[irec]->get_sine();
+    specfem::point::gcoord2 coord = { receivers[irec]->get_x(),
+                                      receivers[irec]->get_z() };
+    auto lcoord = specfem::algorithms::locate_point(coord, mesh);
+
+    this->h_ispec_array(irec) = lcoord.ispec;
+    const auto angle = receivers[irec]->get_angle();
+
+    this->h_cos_recs(irec) =
+        std::cos(Kokkos::numbers::pi_v<type_real> / 180 * angle);
+    this->h_sin_recs(irec) =
+        std::sin(Kokkos::numbers::pi_v<type_real> / 180 * angle);
   }
 
-  this->seismogram_types =
-      specfem::kokkos::DeviceView1d<specfem::enums::seismogram::type>(
-          "specfem::compute::receivers::seismogram_types", stypes.size());
-
-  this->h_seismogram_types = Kokkos::create_mirror_view(this->seismogram_types);
-
-  for (int i = 0; i < stypes.size(); i++) {
+  for (int i = 0; i < n_seis_types; i++) {
     this->h_seismogram_types(i) = stypes[i];
   }
 
-  this->receiver_field = specfem::kokkos::DeviceView6d<type_real>(
-      "specfem::compute::receivers::receiver_field", max_sig_step,
-      my_receivers.size(), stypes.size(), 2, quadz->get_N(), quadx->get_N());
-
-  this->h_receiver_field = Kokkos::create_mirror_view(this->receiver_field);
-
-  this->sync_views();
-
-  return;
-};
-
-void specfem::compute::receivers::sync_views() {
   Kokkos::deep_copy(receiver_array, h_receiver_array);
   Kokkos::deep_copy(ispec_array, h_ispec_array);
   Kokkos::deep_copy(cos_recs, h_cos_recs);
@@ -100,7 +73,7 @@ void specfem::compute::receivers::sync_views() {
   Kokkos::deep_copy(seismogram_types, h_seismogram_types);
 
   return;
-}
+};
 
 void specfem::compute::receivers::sync_seismograms() {
   Kokkos::deep_copy(h_seismogram, seismogram);
