277a8679d4c929083d1f006a28a6e130cb3cbda4
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2024-08-20 08:42:05 -0400

    Documentation for elementary datatypes

---
+++ docs/api/datatypes/base_datatypes/chunk_element.rst
@@ -0,0 +1,11 @@
+
+Chunk Element View Types
+========================
+
+Elementary datatypes used to store field values within a chunk of elements.
+
+.. doxygenstruct:: specfem::datatype::ScalarChunkViewType
+    :members:
+
+.. doxygenstruct:: specfem::datatype::VectorChunkViewType
+    :members:
--- include/datatypes/simd.hpp
+++ include/datatypes/simd.hpp
@@ -5,42 +5,67 @@
 
 namespace specfem {
 namespace datatype {
-
+/**
+ * @brief Wrapper around Kokkos::Experimental::native_simd to provide a
+ * consistent interface for SIMD operations.
+ *
+ * @tparam T The base type of the SIMD vector.
+ * @tparam UseSIMD Whether to use SIMD operations or not. If false, base type is
+ * used directly.
+ */
 template <typename T, bool UseSIMD> struct simd;
 
+/**
+ * @brief Specialization of simd for when UseSIMD is false.
+ *
+ * @tparam T The base type of the SIMD vector.
+ */
 template <typename T> struct simd<T, false> {
-  using base_type = T;
-  using datatype = T;
-  constexpr static bool using_simd = false;
-  using mask_type = bool;
+  using base_type = T; ///< The base type of the SIMD vector.
+  using datatype = T;  ///< The type of the SIMD vector. In this case, it is the
+                       ///< same as the base type.
+  constexpr static bool using_simd =
+      false;              ///< Whether SIMD operations are used or not.
+  using mask_type = bool; ///< The type of the mask used for SIMD operations.
+  /**
+   * @brief Returns the size of the SIMD vector.
+   *
+   * @return constexpr static int The size of the SIMD vector.
+   */
   KOKKOS_FUNCTION constexpr static int size() { return 1; }
 };
 
+template <typename T> struct simd<T, true> {
+  using base_type = T; ///< The base type of the SIMD vector.
 #ifdef ENABLE_SIMD
-template <typename T> struct simd<T, true> {
-  using base_type = T;
-  using datatype = Kokkos::Experimental::native_simd<T>;
-  constexpr static bool using_simd = true;
+  using datatype = Kokkos::Experimental::native_simd<T>; ///< The type of the
+                                                         ///< SIMD vector.
+#else
+  using datatype = Kokkos::Experimental::simd<
+      T, Kokkos::Experimental::simd_abi::scalar>; ///< The type of the SIMD
+                                                  ///< vector.
+#endif
+  constexpr static bool using_simd =
+      true; ///< Whether SIMD operations are used or not.
+  /**
+   * @brief Returns the size of the SIMD vector.
+   *
+   * @return constexpr static int The size of the SIMD vector.
+   */
   KOKKOS_FUNCTION constexpr static int size() {
+#ifdef ENABLE_SIMD
     return Kokkos::Experimental::native_simd<T>::size();
-  }
-  using mask_type = typename datatype::mask_type;
-  using tag_type = Kokkos::Experimental::element_aligned_tag;
-};
-#else  // ENABLE_SIMD
-template <typename T> struct simd<T, true> {
-  using base_type = T;
-  using datatype =
-      Kokkos::Experimental::simd<T, Kokkos::Experimental::simd_abi::scalar>;
-  constexpr static bool using_simd = true;
-  KOKKOS_FUNCTION constexpr static int size() {
+#else
     return Kokkos::Experimental::simd<
         T, Kokkos::Experimental::simd_abi::scalar>::size();
+#endif
   }
-  using mask_type = typename datatype::mask_type;
-  using tag_type = Kokkos::Experimental::element_aligned_tag;
+  using mask_type = typename datatype::mask_type; ///< The type of the mask used
+                                                  ///< for SIMD operations.
+  using tag_type =
+      Kokkos::Experimental::element_aligned_tag; ///< The tag type used for SIMD
+                                                 ///< operations.
 };
-#endif // ENABLE_SIMD
 
 namespace impl {
 template <typename T, typename simd_type, bool UseSIMD>
@@ -93,10 +118,27 @@ public:
 
 } // namespace impl
 
+/**
+ * @brief SIMD like data type that can be used when SIMD operations are not
+ * defined for type T.
+ *
+ * @tparam T Type of the data.
+ * @tparam simd_type Type of mask used for SIMD operations.
+ * @tparam UseSIMD Whether to use SIMD operations or not.
+ */
 template <typename T, typename simd_type, bool UseSIMD> struct simd_like {
-  using datatype = impl::simd_like_value_type<T, simd_type, UseSIMD>;
-  constexpr static bool using_simd = UseSIMD;
-  using mask_type = typename datatype::mask_type;
+  using datatype =
+      impl::simd_like_value_type<T, simd_type, UseSIMD>; ///< The data type.
+  using base_type = T; ///< The base type of the data type.
+  constexpr static bool using_simd =
+      UseSIMD; ///< Whether SIMD operations are used or not.
+  using mask_type = typename datatype::mask_type; ///< The type of the mask used
+                                                  ///< for SIMD operations.
+  /**
+   * @brief Returns the size of the SIMD vector.
+   *
+   * @return constexpr static int The size of the SIMD vector.
+   */
   constexpr static int size() { return datatype::size(); }
 };
 } // namespace datatype
