dbe601b951c177ecd0dcec426271507323eaa826
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2024-08-20 10:30:37 -0400

    Documentation update for point properties

--- include/point/properties.hpp
+++ include/point/properties.hpp
@@ -7,31 +7,50 @@
 namespace specfem {
 namespace point {
 
+/**
+ * @brief Store properties of the medium at a quadrature point
+ *
+ * @tparam DimensionType Dimension of the spectral element
+ * @tparam MediumTag Tag indicating the medium of the element
+ * @tparam PropertyTag Tag indicating the property of the medium
+ * @tparam UseSIMD Boolean indicating whether to use SIMD
+ */
 template <specfem::dimension::type DimensionType,
           specfem::element::medium_tag MediumTag,
           specfem::element::property_tag PropertyTag, bool UseSIMD>
-struct properties {};
+struct properties;
 
+/**
+ * @brief Template specialization for 2D isotropic elastic media
+ *
+ * @tparam UseSIMD Boolean indicating whether to use SIMD
+ */
 template <bool UseSIMD>
 struct properties<specfem::dimension::type::dim2,
                   specfem::element::medium_tag::elastic,
                   specfem::element::property_tag::isotropic, UseSIMD> {
 
-  using simd = specfem::datatype::simd<type_real, UseSIMD>;
+  /**
+   * @name Typedefs
+   *
+   */
+  ///@{
+  using simd = specfem::datatype::simd<type_real, UseSIMD>; ///< SIMD type
   constexpr static auto dimension = specfem::dimension::type::dim2;
   constexpr static auto medium_tag = specfem::element::medium_tag::elastic;
   constexpr static auto property_tag =
       specfem::element::property_tag::isotropic;
+  using value_type =
+      typename simd::datatype; ///< Value type to store properties
+  ///@}
 
-  using value_type = typename simd::datatype;
+  value_type mu;  ///< shear modulus @f$ \mu @f$
+  value_type rho; ///< density @f$ \rho @f$
 
-  value_type lambdaplus2mu;
-  value_type mu;
-  value_type rho;
-
-  value_type rho_vp;
-  value_type rho_vs;
-  value_type lambda;
+  value_type rho_vp;        ///< P-wave velocity @f$ \rho v_p @f$
+  value_type rho_vs;        ///< S-wave velocity @f$ \rho v_s @f$
+  value_type lambda;        ///< Lame's parameter @f$ \lambda @f$
+  value_type lambdaplus2mu; ///< Lame's parameter @f$ \lambda + 2\mu @f$
 
 private:
   KOKKOS_FUNCTION
@@ -49,34 +68,63 @@ private:
         rho_vs(Kokkos::sqrt(rho * mu)), lambda(lambdaplus2mu - 2.0 * mu) {}
 
 public:
+  /**
+   * @name Constructors
+   *
+   */
+  ///@{
+  /**
+   * @brief Default constructor
+   *
+   */
   KOKKOS_FUNCTION
   properties() = default;
 
+  /**
+   * @brief Construct a new properties object
+   *
+   * @param lambdaplus2mu @f$ \lambda + 2\mu @f$
+   * @param mu @f$ \mu @f$
+   * @param rho @f$ \rho @f$
+   */
   KOKKOS_FUNCTION
   properties(const value_type &lambdaplus2mu, const value_type &mu,
              const value_type &rho)
       : properties(lambdaplus2mu, mu, rho,
                    std::integral_constant<bool, UseSIMD>{}) {}
+  ///@}
 };
 
+/**
+ * @brief Template specialization for 2D isotropic acoustic media
+ *
+ * @tparam UseSIMD Boolean indicating whether to use SIMD
+ */
 template <bool UseSIMD>
 struct properties<specfem::dimension::type::dim2,
                   specfem::element::medium_tag::acoustic,
                   specfem::element::property_tag::isotropic, UseSIMD> {
 
-  using simd = specfem::datatype::simd<type_real, UseSIMD>;
+  /**
+   * @name Typedefs
+   *
+   */
+  ///@{
+  using simd = specfem::datatype::simd<type_real, UseSIMD>; ///< SIMD type
   constexpr static auto dimension = specfem::dimension::type::dim2;
   constexpr static auto medium_tag = specfem::element::medium_tag::acoustic;
   constexpr static auto property_tag =
       specfem::element::property_tag::isotropic;
 
-  using value_type = typename simd::datatype;
+  using value_type =
+      typename simd::datatype; ///< Value type to store properties
+  ///@}
 
-  value_type lambdaplus2mu_inverse;
-  value_type rho_inverse;
-  value_type kappa;
+  value_type lambdaplus2mu_inverse; ///< @f$ \frac{1}{\lambda + 2\mu} @f$
+  value_type rho_inverse;           ///< @f$ \frac{1}{\rho} @f$
+  value_type kappa;                 ///< Bulk modulus @f$ \kappa @f$
 
-  value_type rho_vpinverse;
+  value_type rho_vpinverse; ///< @f$ \frac{1}{\rho v_p} @f$
 
 private:
   KOKKOS_FUNCTION
@@ -96,14 +144,32 @@ private:
         rho_vpinverse(Kokkos::sqrt(rho_inverse * lambdaplus2mu_inverse)) {}
 
 public:
+  /**
+   * @name Constructors
+   *
+   */
+  ///@{
+
+  /**
+   * @brief Default constructor
+   *
+   */
   KOKKOS_FUNCTION
   properties() = default;
 
+  /**
+   * @brief Construct a new properties object
+   *
+   * @param lambdaplus2mu_inverse @f$ \frac{1}{\lambda + 2\mu} @f$
+   * @param rho_inverse @f$ \frac{1}{\rho} @f$
+   * @param kappa Bulk modulus @f$ \kappa @f$
+   */
   KOKKOS_FUNCTION
   properties(const value_type &lambdaplus2mu_inverse,
              const value_type &rho_inverse, const value_type &kappa)
       : properties(lambdaplus2mu_inverse, rho_inverse, kappa,
                    std::integral_constant<bool, UseSIMD>{}) {}
+  ///@}
 };
 
 } // namespace point
