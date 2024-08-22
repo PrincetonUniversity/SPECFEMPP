b286e1bf110681a86eb116af8575765f64978f3f
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2024-08-22 13:57:29 -0400

    Documentation update for range policy

--- include/parallel_configuration/range_config.hpp
+++ include/parallel_configuration/range_config.hpp
@@ -3,15 +3,26 @@
 namespace specfem {
 namespace parallel_config {
 
-template <typename SIMDType> struct range_config { using simd = SIMDType; };
+/**
+ * @brief Parallel configuration for range policy
+ *
+ * @tparam SIMD SIMD type @ref specfem::datatype::simd
+ * @tparam ExecutionSpace Execution space
+ */
+template <typename SIMD, typename ExecutionSpace> struct range_config {
+  using simd = SIMD;
+  using execution_space = ExecutionSpace;
+  static constexpr bool is_point_parallel_config = true;
+};
 
-#ifdef KOKKOS_ENABLE_CUDA
-template <typename SIMDType>
-using default_range_config = range_config<SIMDType>;
-#else
-template <typename SIMDType>
-using default_range_config = range_config<SIMDType>;
-#endif
+/**
+ * @brief Type alias for default range configuration
+ *
+ * @tparam SIMD SIMD type @ref specfem::datatype::simd
+ * @tparam ExecutionSpace Execution space
+ */
+template <typename SIMD, typename ExecutionSpace>
+using default_range_config = range_config<SIMD, ExecutionSpace>;
 
 } // namespace parallel_config
 } // namespace specfem
