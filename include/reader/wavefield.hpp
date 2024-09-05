4bdb320f98c4f19b74316931ab9e95d992616843
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2024-09-05 13:34:58 -0400

    Documentation update for wavefield reader/writer

--- include/reader/wavefield.hpp
+++ include/reader/wavefield.hpp
@@ -9,30 +9,34 @@ namespace specfem {
 namespace reader {
 
 /**
- * @brief Base reader class
+ * @brief Reader to read wavefield data from disk
  *
  */
 template <typename IOLibrary> class wavefield : public reader {
 
 public:
   /**
-   * @brief Construct a new wavefield object
+   * @brief Construct a new reader object
    *
-   * @param output_folder Path to output folder
+   * @param output_folder Path to output folder or .h5 file
    */
   wavefield(const std::string &output_folder,
             const specfem::compute::assembly &assembly);
 
   /**
-   * @brief Method to execute the read operation
+   * @brief Read the wavefield data from disk
    *
    */
   void read() override;
 
 private:
   std::string output_folder; ///< Path to output folder
-  specfem::compute::simulation_field<specfem::wavefield::type::buffer> buffer;
-  specfem::compute::boundary_values boundary_values;
+  specfem::compute::simulation_field<specfem::wavefield::type::buffer>
+      buffer; ///< Buffer wavefield to store the data
+  specfem::compute::boundary_values boundary_values; ///< Boundary values used
+                                                     ///< for backward
+                                                     ///< reconstruction during
+                                                     ///< adjoint simulations
 };
 
 } // namespace reader
