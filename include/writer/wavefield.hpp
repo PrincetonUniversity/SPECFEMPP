ba43d5285946bb823b76a5ea534fb7df1413b7bc
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2024-09-05 13:34:58 -0400

    Documentation update for wavefield reader/writer

--- include/writer/wavefield.hpp
+++ include/writer/wavefield.hpp
@@ -8,6 +8,11 @@
 namespace specfem {
 namespace writer {
 
+/**
+ * @brief Writer to output wavefield data to disk
+ *
+ * @tparam OutputLibrary Library to use for output (HDF5, ASCII, etc.)
+ */
 template <typename OutputLibrary> class wavefield : public writer {
 
 public:
@@ -18,15 +23,37 @@ public:
       specfem::medium::medium<specfem::dimension::type::dim2,
                               specfem::element::medium_tag::acoustic>;
 
+  /**
+   * @name Constructors
+   *
+   */
+  ///@{
+
+  /**
+   * @brief Construct a writer object
+   *
+   * @param assembly SPECFEM++ assembly
+   * @param output_folder Path to output location (will be an .h5 file if using
+   * HDF5, and a folder if using ASCII)
+   */
   wavefield(const specfem::compute::assembly &assembly,
             const std::string output_folder);
+  ///@}
 
+  /**
+   * @brief Write the wavefield data to disk
+   *
+   */
   void write() override;
 
 private:
   std::string output_folder; ///< Path to output folder
-  specfem::compute::simulation_field<specfem::wavefield::type::forward> forward;
-  specfem::compute::boundary_values boundary_values;
+  specfem::compute::simulation_field<specfem::wavefield::type::forward>
+      forward;                                       ///< Forward wavefield
+  specfem::compute::boundary_values boundary_values; ///< Boundary values used
+                                                     ///< for backward
+                                                     ///< reconstruction during
+                                                     ///< adjoint simulations
 };
 } // namespace writer
 } // namespace specfem
