1c589924f56a23026b9c5e920e54c3faeb1ed12e
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2024-09-17 10:28:06 -0400

    Only accept meshes with NGNOD = 9

--- src/mesh/properties/properties.cpp
+++ src/mesh/properties/properties.cpp
@@ -10,6 +10,15 @@ specfem::mesh::properties::properties(std::ifstream &stream,
                                  &this->nspec, &this->pointsdisp,
                                  &this->plot_lowerleft_corner_only);
 
+  // ---------------------------------------------------------------------
+  if (this->ngnod != 9) {
+    std::ostringstream error_message;
+    error_message << "Number of control nodes per element must be 9, but is "
+                  << this->ngnod << "\n"
+                  << "Currently, there is a bug when NGNOD == 4 \n";
+    throw std::runtime_error(error_message.str());
+  }
+
   specfem::IO::fortran_read_line(
       stream, &this->nelemabs, &this->nelem_acforcing,
       &this->nelem_acoustic_surface, &this->num_fluid_solid_edges,
