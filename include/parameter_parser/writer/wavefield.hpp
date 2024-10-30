aef92d1d55e2a95b1562c096792993e3250727b2
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2024-10-30 15:21:35 -0400

    Documentation update

--- include/parameter_parser/writer/wavefield.hpp
+++ include/parameter_parser/writer/wavefield.hpp
@@ -8,19 +8,57 @@
 
 namespace specfem {
 namespace runtime_configuration {
+
+/**
+ * @brief Wavefield configuration class is used to instantiate wavefield writers
+ *
+ */
 class wavefield {
 
 public:
+  /**
+   * @name Constructors
+   *
+   */
+  ///@{
+  /**
+   * @brief Construct a new wavefield configuration object
+   *
+   * @param output_format Output wavefield file format
+   * @param output_folder Path to folder location where wavefield will be stored
+   * @param type Type of simulation (forward or adjoint)
+   */
   wavefield(const std::string output_format, const std::string output_folder,
             const specfem::simulation::type type)
       : output_format(output_format), output_folder(output_folder),
         simulation_type(type) {}
 
+  /**
+   * @brief Construct a new wavefield configuration object from YAML node
+   *
+   * @param Node YAML node describing the wavefield writer
+   * @param type Type of simulation (forward or adjoint)
+   */
   wavefield(const YAML::Node &Node, const specfem::simulation::type type);
+  ///@}
 
+  /**
+   * @brief Instantiate a wavefield writer object
+   *
+   * @param assembly SPECFEM++ assembly object
+   * @return std::shared_ptr<specfem::writer::writer> Pointer to an instantiated
+   * writer object
+   */
   std::shared_ptr<specfem::writer::writer> instantiate_wavefield_writer(
       const specfem::compute::assembly &assembly) const;
 
+  /**
+   * @brief Instantiate a wavefield reader object
+   *
+   * @param assembly SPECFEM++ assembly object
+   * @return std::shared_ptr<specfem::reader::reader> Pointer to an instantiated
+   * reader object
+   */
   std::shared_ptr<specfem::reader::reader> instantiate_wavefield_reader(
       const specfem::compute::assembly &assembly) const;
 
