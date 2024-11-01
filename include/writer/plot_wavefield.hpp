c38b5dfdaba6fe0e3c1f36f5979554158f095cc5
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2024-11-01 15:15:58 -0400

    Added compiler guards for builds without VTK

--- include/writer/plot_wavefield.hpp
+++ include/writer/plot_wavefield.hpp
@@ -5,6 +5,9 @@
 #include "enumerations/wavefield.hpp"
 #include "writer.hpp"
 #include <boost/filesystem.hpp>
+#ifdef NO_VTK
+#include <sstream>
+#endif
 
 namespace specfem {
 namespace writer {
@@ -17,6 +20,14 @@ public:
                  const std::string &output_folder)
       : output_format(output_format), component(component),
         output_folder(output_folder), wavefield(wavefield), assembly(assembly) {
+#ifdef NO_VTK
+    std::ostringstream message;
+    message << "Display section is not enabled, since SPECFEM++ was built "
+               "without VTK\n"
+            << "Please install VTK and rebuild SPECFEM++ with "
+               "-DVTK_DIR=/path/to/vtk";
+    throw std::runtime_error(message.str());
+#endif
   }
 
   void write() override;
