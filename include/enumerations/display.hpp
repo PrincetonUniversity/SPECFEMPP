dfcf97fa39fa678379d8a84108da9f7d93ee40a6
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2024-11-01 10:10:13 -0400

    Added write method for the plotter

--- include/enumerations/display.hpp
+++ include/enumerations/display.hpp
@@ -5,6 +5,14 @@ namespace display {
 
 enum class format { PNG, JPG };
 
-enum class wavefield { displacement, velocity, acceleration };
+enum class wavefield {
+  displacement_x,
+  displacement_z,
+  velocity_x,
+  velocity_z,
+  acceleration_x,
+  acceleration_z,
+  pressure
+};
 } // namespace display
 } // namespace specfem
