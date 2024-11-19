be8e7b5b1510da077792818c2ee0bb600f8d1b18
Lucas Sawade <lsawade@princeton.edu>
2024-11-19 15:29:57 -0500

    Small fixes

--- src/mesh/coupled_interfaces/coupled_interfaces.cpp
+++ src/mesh/coupled_interfaces/coupled_interfaces.cpp
@@ -2,18 +2,18 @@
 #include "mesh/coupled_interfaces/interface_container.hpp"
 #include "mesh/coupled_interfaces/interface_container.tpp"
 
-specfem::mesh::coupled_interfaces coupled_interfaces(
-    specfem::mesh::interface_container<specfem::element::medium_tag::elastic,
-                                       specfem::element::medium_tag::acoustic>
-        elastic_acoustic,
-    specfem::mesh::interface_container<
-        specfem::element::medium_tag::acoustic,
-        specfem::element::medium_tag::poroelastic>
-        acoustic_poroelastic,
-    specfem::mesh::interface_container<
-        specfem::element::medium_tag::elastic,
-        specfem::element::medium_tag::poroelastic>
-        elastic_poroelastic) {}
+// specfem::mesh::coupled_interfaces::coupled_interfaces(
+//     specfem::mesh::interface_container<specfem::element::medium_tag::elastic,
+//                                        specfem::element::medium_tag::acoustic>
+//         elastic_acoustic,
+//     specfem::mesh::interface_container<
+//         specfem::element::medium_tag::acoustic,
+//         specfem::element::medium_tag::poroelastic>
+//         acoustic_poroelastic,
+//     specfem::mesh::interface_container<
+//         specfem::element::medium_tag::elastic,
+//         specfem::element::medium_tag::poroelastic>
+//         elastic_poroelastic) {}
 
 template <specfem::element::medium_tag medium1,
           specfem::element::medium_tag medium2>
