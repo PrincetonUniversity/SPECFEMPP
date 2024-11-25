2c3aff531c2225757e8e7c87e6958615c1fbcdf0
Lucas Sawade <lsawade@princeton.edu>
2024-11-25 16:51:50 -0500

    Just added more documentation and updated read_mesh"s doxygen blurb

--- include/IO/mesh/read_mesh.hpp
+++ include/IO/mesh/read_mesh.hpp
@@ -8,11 +8,13 @@ namespace specfem {
 
 namespace IO {
 
-/* @brief Construct a mesh object from a Fortran binary database file
+/**
+ * @brief Construct a mesh object from a Fortran binary database file
  *
  * @param filename Fortran binary database filename
  * @param mpi pointer to MPI object to manage communication
  * @return specfem::mesh::mesh Specfem mesh object
+ *
  */
 specfem::mesh::mesh read_mesh(const std::string filename,
                               const specfem::MPI::MPI *mpi);
