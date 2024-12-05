06cea0d6c0edd1da09fdf05d6b284de8493a162e
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2024-12-05 16:08:24 -0500

    Added snakemake workflow with jinja templating

--- examples/fluid-solid-bathymetry/visualize_seismograms.py
+++ examples/fluid-solid-bathymetry/visualize_seismograms.py
@@ -2,20 +2,27 @@ import os
 import numpy as np
 import obspy
 
+
 def get_traces(directory):
     traces = []
     ## iterate over all seismograms
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
         station_name = os.path.splitext(filename)[0]
-        trace = np.loadtxt(f, delimiter=' ')
-        starttime = trace[0,0]
-        dt = trace[1,0] - trace[0,0]
-        traces.append(obspy.Trace(trace[:,1], {'network': station_name, 'starttime': starttime, 'delta': dt}))
+        trace = np.loadtxt(f, delimiter=" ")
+        starttime = trace[0, 0]
+        dt = trace[1, 0] - trace[0, 0]
+        traces.append(
+            obspy.Trace(
+                trace[:, 1],
+                {"network": station_name, "starttime": starttime, "delta": dt},
+            )
+        )
 
     stream = obspy.Stream(traces)
 
     return stream
+
 
 directory = "OUTPUT_FILES/seismograms"
 stream = get_traces(directory)
