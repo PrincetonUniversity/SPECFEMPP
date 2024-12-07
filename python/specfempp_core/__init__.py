e5604bb2838f3ea088c694a2310574c696fbc56e
ccui <ccui@princeton.edu>
2024-12-07 15:36:55 -0500

    Add dependency for pip install

--- python/specfempp_core/__init__.py
+++ python/specfempp_core/__init__.py
@@ -1,1 +1,7 @@
-from ._core import *
+from ._core import initialize, execute, finalize
+import atexit
+
+if initialize([]):
+    atexit.register(finalize)
+
+__all__ = ["initialize", "execute", "finalize"]
