01faa2d071575173c38c82944d9e056f6169d272
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2024-12-10 12:02:22 -0500

    Added density to point properties

--- include/material/elastic_properties.hpp
+++ include/material/elastic_properties.hpp
@@ -176,7 +176,7 @@ public:
    */
   inline specfem::point::properties<dimension, medium_tag, property_tag, false>
   get_properties() const {
-    return { c11, c13, c15, c33, c35, c55, c12, c23, c25 };
+    return { c11, c13, c15, c33, c35, c55, c12, c23, c25, density };
   }
 
   inline std::string print() const {
