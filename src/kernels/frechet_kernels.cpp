45771f10e646d7151f2e1c2bf520c5b328a6afee
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2024-12-18 13:37:09 -0500

    Macro implementation frechet kernels

--- src/kernels/frechet_kernels.cpp
+++ src/kernels/frechet_kernels.cpp
@@ -1,6 +1,21 @@
 #include "kernels/frechet_kernels.hpp"
 #include "kernels/frechet_kernels.tpp"
 
+#define INSTANTIATION_MACRO(DIMENSION_TAG, MEDIUM_TAG, PROPERTY_TAG)           \
+  template void specfem::kernels::frechet_kernels<DIMENSION_TAG, 5>::          \
+      compute_material_derivatives<MEDIUM_TAG, PROPERTY_TAG>(                  \
+          const type_real &);                                                  \
+  template void specfem::kernels::frechet_kernels<DIMENSION_TAG, 8>::          \
+      compute_material_derivatives<MEDIUM_TAG, PROPERTY_TAG>(                  \
+          const type_real &);
+
+CALL_MACRO_FOR_ALL_MATERIAL_SYSTEMS(
+    INSTANTIATION_MACRO,
+    WHERE(DIMENSION_TAG_DIM2) WHERE(MEDIUM_TAG_ELASTIC, MEDIUM_TAG_ACOUSTIC)
+        WHERE(PROPERTY_TAG_ISOTROPIC, PROPERTY_TAG_ANISOTROPIC))
+
+#undef INSTANTIATION_MACRO
+
 // Explicit template instantiation
 template class specfem::kernels::frechet_kernels<specfem::dimension::type::dim2,
                                                  5>;
