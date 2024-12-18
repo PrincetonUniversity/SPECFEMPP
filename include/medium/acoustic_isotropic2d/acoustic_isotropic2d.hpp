a462a3f1288016d7ac67c58c64af5b3f04097f7e
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2024-12-18 14:34:57 -0500

    Moved frechet derivatives computation to `specfem::medium`

--- include/medium/acoustic_isotropic2d/acoustic_isotropic2d.hpp
+++ include/medium/acoustic_isotropic2d/acoustic_isotropic2d.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "algorithms/dot.hpp"
 #include "algorithms/gradient.hpp"
 #include "enumerations/dimension.hpp"
 #include "enumerations/medium.hpp"
@@ -31,6 +32,37 @@ impl_compute_stress(
   T(1, 0) = properties.rho_inverse * du(1, 0);
 
   return { T };
+}
+
+template <typename PointPropertiesType, typename AdjointPointFieldType,
+          typename BackwardPointFieldType, typename PointFieldDerivativesType>
+KOKKOS_FUNCTION specfem::point::kernels<
+    PointPropertiesType::dimension, PointPropertiesType::medium_tag,
+    PointPropertiesType::property_tag, PointPropertiesType::simd::using_simd>
+impl_compute_frechet_derivatives(
+    const std::integral_constant<specfem::dimension::type,
+                                 specfem::dimension::type::dim2>,
+    const std::integral_constant<specfem::element::medium_tag,
+                                 specfem::element::medium_tag::acoustic>,
+    const std::integral_constant<specfem::element::property_tag,
+                                 specfem::element::property_tag::isotropic>,
+    const PointPropertiesType &properties,
+    const AdjointPointFieldType &adjoint_field,
+    const BackwardPointFieldType &backward_field,
+    const PointFieldDerivativesType &adjoint_derivatives,
+    const PointFieldDerivativesType &backward_derivatives,
+    const type_real &dt) {
+
+  const auto rho_kl =
+      (adjoint_derivatives.du(0, 0) * backward_derivatives.du(0, 0) +
+       adjoint_derivatives.du(1, 0) * backward_derivatives.du(1, 0)) *
+      properties.rho_inverse * dt;
+
+  const auto kappa_kl = specfem::algorithms::dot(adjoint_field.acceleration,
+                                                 backward_field.displacement) *
+                        static_cast<type_real>(1.0) / properties.kappa * dt;
+
+  return { rho_kl, kappa_kl };
 }
 
 template <typename MemberType, typename IteratorType, typename ChunkFieldType,
