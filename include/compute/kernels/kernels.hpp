d67413b5283fe62926b354ae0772018e75dc6daf
ccui <ccui@princeton.edu>
2024-12-18 10:38:47 -0500

    Implement element indices getter for kernels.

--- include/compute/kernels/kernels.hpp
+++ include/compute/kernels/kernels.hpp
@@ -102,6 +102,52 @@ public:
     elastic_anisotropic.copy_to_device();
     acoustic_isotropic.copy_to_device();
   }
+
+  /**
+   * @brief Get the indices of elements of a given type as a view on the device
+   *
+   * @param medium Medium tag of the elements
+   * @return Kokkos::View<int *, Kokkos::LayoutLeft,
+   * Kokkos::DefaultExecutionSpace> View of the indices of elements of the given
+   * type
+   */
+  Kokkos::View<int *, Kokkos::LayoutLeft, Kokkos::DefaultExecutionSpace>
+  get_elements_on_device(const specfem::element::medium_tag medium) const;
+
+  /**
+   * @brief Get the indices of elements of a given type as a view on the device
+   *
+   * @param medium Medium tag of the elements
+   * @param property Property tag of the elements
+   * @return Kokkos::View<int *, Kokkos::LayoutLeft,
+   * Kokkos::DefaultExecutionSpace> View of the indices of elements of the given
+   * type
+   */
+  Kokkos::View<int *, Kokkos::LayoutLeft, Kokkos::DefaultExecutionSpace>
+  get_elements_on_device(const specfem::element::medium_tag medium,
+                         const specfem::element::property_tag property) const;
+
+  /**
+   * @brief Get the indices of elements of a given type as a view on the host
+   *
+   * @param medium Medium tag of the elements
+   * @return Kokkos::View<int *, Kokkos::LayoutLeft, Kokkos::HostSpace> View of
+   * the indices of elements of the given type
+   */
+  Kokkos::View<int *, Kokkos::LayoutLeft, Kokkos::HostSpace>
+  get_elements_on_host(const specfem::element::medium_tag medium) const;
+
+  /**
+   * @brief Get the indices of elements of a given type as a view on the host
+   *
+   * @param medium Medium tag of the elements
+   * @param property Property tag of the elements
+   * @return Kokkos::View<int *, Kokkos::LayoutLeft, Kokkos::HostSpace> View of
+   * the indices of elements of the given type
+   */
+  Kokkos::View<int *, Kokkos::LayoutLeft, Kokkos::HostSpace>
+  get_elements_on_host(const specfem::element::medium_tag medium,
+                       const specfem::element::property_tag property) const;
 };
 
 /**
