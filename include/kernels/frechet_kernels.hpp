3fc0aba5c9b11c12438d59b707e8c34a21cd4fbe
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2024-12-18 13:37:09 -0500

    Macro implementation frechet kernels

--- include/kernels/frechet_kernels.hpp
+++ include/kernels/frechet_kernels.hpp
@@ -5,7 +5,6 @@
 #include "enumerations/dimension.hpp"
 #include "enumerations/material_definitions.hpp"
 #include "enumerations/medium.hpp"
-#include "frechet_derivatives/frechet_derivatives.hpp"
 
 namespace specfem {
 namespace kernels {
@@ -40,28 +39,6 @@ private:
             specfem::element::property_tag PropertyTag>
   void compute_material_derivatives(const type_real &dt);
 };
-
-// template <specfem::dimension::type DimensionType, int NGLL>
-// class frechet_kernels {
-// public:
-//   frechet_kernels(const specfem::compute::assembly &assembly)
-//       : elastic_elements(assembly), acoustic_elements(assembly) {}
-
-//   inline void compute_derivatives(const type_real &dt) {
-//     elastic_elements.compute(dt);
-//     acoustic_elements.compute(dt);
-//   }
-
-// private:
-//   specfem::frechet_derivatives::frechet_derivatives<
-//       DimensionType, specfem::element::medium_tag::elastic, NGLL>
-//       elastic_elements;
-
-//   specfem::frechet_derivatives::frechet_derivatives<
-//       DimensionType, specfem::element::medium_tag::acoustic, NGLL>
-//       acoustic_elements;
-// };
-
 } // namespace kernels
 } // namespace specfem
 
