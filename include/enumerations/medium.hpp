9bf3f8c7e36ea398a6b01fef3990999f498e2a61
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2024-12-17 20:57:34 -0500

    Initial commit for macros

--- include/enumerations/medium.hpp
+++ include/enumerations/medium.hpp
@@ -35,52 +35,6 @@ enum class boundary_tag {
   composite_stacey_dirichlet
 };
 
-constexpr auto medium_types() {
-  constexpr int total_medium_types = 2;
-  constexpr std::array<medium_tag, total_medium_types> medium_types{
-    medium_tag::elastic, medium_tag::acoustic
-  };
-  return medium_types;
-}
-
-constexpr auto material_systems() {
-  constexpr int total_material_systems = 3;
-  constexpr
-      std::array<std::tuple<medium_tag, property_tag>, total_material_systems>
-          material_systems{
-            std::make_tuple(medium_tag::elastic, property_tag::isotropic),
-            std::make_tuple(medium_tag::elastic, property_tag::anisotropic),
-            std::make_tuple(medium_tag::acoustic, property_tag::isotropic)
-          };
-  return material_systems;
-}
-
-constexpr auto element_types() {
-  constexpr int total_element_types = 8;
-  constexpr std::array<std::tuple<medium_tag, property_tag, boundary_tag>,
-                       total_element_types>
-      element_types{
-        std::make_tuple(medium_tag::elastic, property_tag::isotropic,
-                        boundary_tag::none),
-        std::make_tuple(medium_tag::elastic, property_tag::isotropic,
-                        boundary_tag::stacey),
-        std::make_tuple(medium_tag::elastic, property_tag::anisotropic,
-                        boundary_tag::none),
-        std::make_tuple(medium_tag::elastic, property_tag::anisotropic,
-                        boundary_tag::stacey),
-        std::make_tuple(medium_tag::acoustic, property_tag::isotropic,
-                        boundary_tag::none),
-        std::make_tuple(medium_tag::acoustic, property_tag::isotropic,
-                        boundary_tag::acoustic_free_surface),
-        std::make_tuple(medium_tag::acoustic, property_tag::isotropic,
-                        boundary_tag::stacey),
-        std::make_tuple(medium_tag::acoustic, property_tag::isotropic,
-                        boundary_tag::composite_stacey_dirichlet)
-      };
-
-  return element_types;
-}
-
 template <specfem::dimension::type Dimension,
           specfem::element::medium_tag MediumTag>
 class attributes;
