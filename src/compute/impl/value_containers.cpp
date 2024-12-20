a0eb508842bc28953afd52d991eb8434b1178bf5
ccui <ccui@princeton.edu>
2024-12-20 12:49:12 -0500

    Update error message

--- src/compute/impl/value_containers.cpp
+++ src/compute/impl/value_containers.cpp
@@ -10,34 +10,42 @@ void specfem::compute::impl::compute_number_of_elements_per_medium(
     int &n_elastic_isotropic, int &n_elastic_anisotropic, int &n_acoustic) {
 
   Kokkos::parallel_reduce(
-      "specfem::compute::impl::compute_number_of_elements_per_medium",
+      "specfem::compute::properties::compute_number_of_elements_per_medium",
       specfem::kokkos::HostRange(0, nspec),
       [=](const int ispec, int &n_elastic_isotropic, int &n_elastic_anisotropic,
           int &n_acoustic) {
         const int ispec_mesh = mapping.compute_to_mesh(ispec);
         if (tags.tags_container(ispec_mesh).medium_tag ==
             specfem::element::medium_tag::elastic) {
-          h_medium_tags(ispec) = specfem::element::medium_tag::elastic;
+          h_element_types(ispec) = specfem::element::medium_tag::elastic;
           if (tags.tags_container(ispec_mesh).property_tag ==
               specfem::element::property_tag::isotropic) {
             n_elastic_isotropic++;
-            h_property_tags(ispec) = specfem::element::property_tag::isotropic;
+            h_element_property(ispec) =
+                specfem::element::property_tag::isotropic;
           } else if (tags.tags_container(ispec_mesh).property_tag ==
                      specfem::element::property_tag::anisotropic) {
             n_elastic_anisotropic++;
-            h_property_tags(ispec) =
+            h_element_property(ispec) =
                 specfem::element::property_tag::anisotropic;
           } else {
+            std::cout << "Unknown property tag: "
+                      << "File: " << __FILE__ << " Line: " << __LINE__
+                      << std::endl;
             throw std::runtime_error("Unknown property tag");
           }
         } else if (tags.tags_container(ispec_mesh).medium_tag ==
                    specfem::element::medium_tag::acoustic) {
           n_acoustic++;
-          h_medium_tags(ispec) = specfem::element::medium_tag::acoustic;
+          h_element_types(ispec) = specfem::element::medium_tag::acoustic;
           if (tags.tags_container(ispec_mesh).property_tag ==
               specfem::element::property_tag::isotropic) {
-            h_property_tags(ispec) = specfem::element::property_tag::isotropic;
+            h_element_property(ispec) =
+                specfem::element::property_tag::isotropic;
           } else {
+            std::cout << "Unknown property tag: "
+                      << "File: " << __FILE__ << " Line: " << __LINE__
+                      << std::endl;
             throw std::runtime_error("Unknown property tag");
           }
         }
