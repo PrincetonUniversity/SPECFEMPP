54eb716dd92cbf9236ee337d387b730491e5fcfa
Lucas Sawade <lsawade@princeton.edu>
2025-01-03 10:41:26 +0100

    Fixed some function signature that I erroneously wrote (false/true) combos for Use_SIMD

--- include/medium/2d/acoustic/isotropic/mass_matrix.hpp
+++ include/medium/2d/acoustic/isotropic/mass_matrix.hpp
@@ -10,14 +10,14 @@
 namespace specfem {
 namespace medium {
 
-template <bool UseSIMD, specfem::element::property_tag PropertyTag>
+template <bool UseSIMD>
 KOKKOS_FUNCTION specfem::point::field<specfem::dimension::type::dim2,
                                       specfem::element::medium_tag::acoustic,
                                       false, false, false, true, UseSIMD>
 impl_mass_matrix_component(
-    const specfem::point::properties<specfem::dimension::type::dim2,
-                                     specfem::element::medium_tag::acoustic,
-                                     PropertyTag, UseSIMD> &properties,
+    const specfem::point::properties<
+        specfem::dimension::type::dim2, specfem::element::medium_tag::acoustic,
+        specfem::element::property_tag::isotropic, UseSIMD> &properties,
     const specfem::point::partial_derivatives<
         specfem::dimension::type::dim2, true, UseSIMD> &partial_derivatives);
 
--- src/medium/compute_mass_matrix.cpp
+++ src/medium/compute_mass_matrix.cpp
@@ -6,20 +6,23 @@
 // This file only contains explicit template instantiations
 
 #define INSTANTIATION_MACRO(DIMENSION_TAG, MEDIUM_TAG, PROPERTY_TAG)           \
-  template specfem::point::field<DIMENSION_TAG, MEDIUM_TAG, false, false,      \
-                                 false, true, false>                           \
+  template specfem::point::field<GET_TAG(DIMENSION_TAG), GET_TAG(MEDIUM_TAG),  \
+                                 false, false, false, true, false>             \
   specfem::medium::impl_mass_matrix_component(                                 \
-      const specfem::point::properties<DIMENSION_TAG, MEDIUM_TAG,              \
-                                       PROPERTY_TAG, false> &,                 \
-      const specfem::point::partial_derivatives<DIMENSION_TAG, true, false>    \
-          &);                                                                  \
+      const specfem::point::properties<GET_TAG(DIMENSION_TAG),                 \
+                                       GET_TAG(MEDIUM_TAG),                    \
+                                       GET_TAG(PROPERTY_TAG), false> &,        \
+      const specfem::point::partial_derivatives<GET_TAG(DIMENSION_TAG), true,  \
+                                                false> &);                     \
                                                                                \
-  template specfem::point::field<DIMENSION_TAG, MEDIUM_TAG, false, false,      \
-                                 false, true, true>                            \
+  template specfem::point::field<GET_TAG(DIMENSION_TAG), GET_TAG(MEDIUM_TAG),  \
+                                 false, false, false, true, true>              \
   specfem::medium::impl_mass_matrix_component(                                 \
-      const specfem::point::properties<DIMENSION_TAG, MEDIUM_TAG,              \
-                                       PROPERTY_TAG, false> &,                 \
-      const specfem::point::partial_derivatives<DIMENSION_TAG, true, true> &);
+      const specfem::point::properties<GET_TAG(DIMENSION_TAG),                 \
+                                       GET_TAG(MEDIUM_TAG),                    \
+                                       GET_TAG(PROPERTY_TAG), true> &,         \
+      const specfem::point::partial_derivatives<GET_TAG(DIMENSION_TAG), true,  \
+                                                true> &);
 
 CALL_MACRO_FOR_ALL_MATERIAL_SYSTEMS(INSTANTIATION_MACRO,
                                     WHERE(DIMENSION_TAG_DIM2)
