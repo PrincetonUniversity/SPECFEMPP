d00416af0ea692b858bb7f9142c0b3d5e09a9cf2
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2025-01-08 13:53:11 -0500

    Receiver documentation update

--- include/compute/receivers/receivers.hpp
+++ include/compute/receivers/receivers.hpp
@@ -67,6 +67,19 @@ public:
                     station_names, network_names, seismogram_types);
   }
 
+  /** @brief Get the station iterator object
+   *
+   * The iterator object can be used to iterate over the stations. See example
+   * below:
+   * @code
+   * // Iterator over all the stations
+   * for (auto [station_name, network_name, type] :
+   * this->get_stations()) {
+   * // Use get_seismogram(station_name, network_name, type) to get the traces
+   * }
+   * @endcode
+   * @return StationIterator Iterator over the stations
+   */
   StationIterator &get_stations() { return *this; }
 
 private:
@@ -74,9 +87,9 @@ private:
   int nseismograms;
 
 protected:
-  std::vector<std::string> station_names;
-  std::vector<std::string> network_names;
-  std::vector<specfem::wavefield::type> seismogram_types;
+  std::vector<std::string> station_names;                 ///< Station names
+  std::vector<std::string> network_names;                 ///< Network names
+  std::vector<specfem::wavefield::type> seismogram_types; ///< Seismogram types
 };
 
 class SeismogramIterator {
@@ -178,6 +191,27 @@ public:
                     h_cosine_receiver_angle, h_seismogram_components);
   }
 
+  /**
+   * @brief Get the seismogram iterator object associated with a given station
+   * and seismogram type
+   *
+   * The iterator object can be used to get the traces associated with the
+   * station. See example below:
+   *
+   * @code
+   * // Writing the x-component of the displacement seismogram in CSV
+   * std::cout << "Time, value" << std::endl;
+   * for (auto [time, seismograms] :
+   * this->get_seismogram_iterator("STATION_NAME", "NETWORK_NAME",
+   * specfem::wavefield::type::displacement)) {
+   *  std::cout << time << ", " << seismograms[0] << std::endl;
+   * }
+   * @endcode
+   * @param station_name Name of the station
+   * @param network_name Name of the network
+   * @param type Type of the seismogram (displacement, velocity, acceleration or
+   * pressure)
+   */
   SeismogramIterator &get_seismogram(const std::string &station_name,
                                      const std::string &network_name,
                                      const specfem::wavefield::type type) {
@@ -186,17 +220,25 @@ public:
     return *this;
   }
 
-  SeismogramType get_seismogram_components() const {
-    return seismogram_components;
-  }
-
+  /** @brief Set the seismogram step
+   *
+   * @param isig_step Seismogram step
+   */
   void set_seismogram_step(const int isig_step) { this->seis_step = isig_step; }
 
+  /** @brief Set the seismogram type
+   *
+   * @param iseis Seismogram type
+   */
   void set_seismogram_type(const int iseis) { this->iseis = iseis; }
 
   KOKKOS_FUNCTION int get_seismogram_step() const { return seis_step; }
   KOKKOS_FUNCTION int get_seis_type() const { return iseis; }
 
+  /**
+   * @brief Synchronize the seismograms from the device to the host
+   *
+   */
   void sync_seismograms() {
     Kokkos::deep_copy(h_seismogram_components, seismogram_components);
   }
@@ -213,28 +255,60 @@ private:
   type_real t0;
 
 protected:
-  ReceiverAngleType h_sine_receiver_angle;
-  ReceiverAngleType h_cosine_receiver_angle;
-  SeismogramType seismogram_components;
-  SeismogramType::HostMirror h_seismogram_components;
+  ReceiverAngleType h_sine_receiver_angle;   ///< Sine of the receiver angle
+  ReceiverAngleType h_cosine_receiver_angle; ///< Cosine of the receiver angle
+  SeismogramType seismogram_components;      ///< Seismogram components
+  SeismogramType::HostMirror h_seismogram_components; ///< Seismogram components
+                                                      ///< stored on the host
   std::unordered_map<std::string, std::unordered_map<std::string, int> >
       station_network_map;
   std::unordered_map<specfem::wavefield::type, int> seismogram_type_map;
 };
 } // namespace impl
 
+/**
+ * @brief Struct to store information related to the receivers
+ *
+ */
 struct receivers : private impl::element_types,
                    public impl::StationIterator,
                    public impl::SeismogramIterator {
 private:
-  using IndexViewType = Kokkos::View<int *, Kokkos::DefaultExecutionSpace>;
+  using IndexViewType =
+      Kokkos::View<int *, Kokkos::DefaultExecutionSpace>; ///< View to store the
+                                                          ///< elements
+                                                          ///< associated with
+                                                          ///< the receivers
   using LagrangeInterpolantType =
       Kokkos::View<type_real ***[2], Kokkos::LayoutLeft,
-                   Kokkos::DefaultExecutionSpace>;
+                   Kokkos::DefaultExecutionSpace>; ///< View to store the
+                                                   ///< Lagrange interpolant for
+                                                   ///< every receiver
 
 public:
+  /**
+   * @brief Construct a new receivers object
+   *
+   */
   receivers() = default;
 
+  /**
+   * @brief Construct a new receivers object
+   *
+   * @param nspec Total Number of spectral elements in the domain
+   * @param ngllz Total Number of GLL points in the z-direction
+   * @param ngllx Total Number of GLL points in the x-direction
+   * @param max_sig_step Maximum number seismogram sample points
+   * @param dt Time increament
+   * @param t0 Initial time
+   * @param nsteps_between_samples Number of time steps between samples
+   * @param receivers Vector of receivers
+   * @param stypes Vector of seismogram types (displacement, velocity,
+   * acceleration or pressure)
+   * @param mesh Mesh object
+   * @param tags Tags for every element in the mesh
+   * @param properties Properties object
+   */
   receivers(const int nspec, const int ngllz, const int ngllx,
             const int max_sig_step, const type_real dt, const type_real t0,
             const int nsteps_between_samples,
@@ -245,29 +319,58 @@ public:
             const specfem::mesh::tags<specfem::dimension::type::dim2> &tags,
             const specfem::compute::properties &properties);
 
-  LagrangeInterpolantType get_lagrange_interpolant() const {
-    return lagrange_interpolant;
-  }
-
+  /**
+   * @brief Get the spectral element indices in which the receivers are located
+   * on the device
+   *
+   * Returns only the indices of the elements that are associated with a
+   * specific medium and property tag
+   * @param medium Medium tag
+   * @property property Property tag
+   * @return Kokkos::View<int *, Kokkos::DefaultExecutionSpace> View of the
+   * elements indices associated with the receivers
+   */
   Kokkos::View<int *, Kokkos::DefaultExecutionSpace>
   get_elements_on_device(const specfem::element::medium_tag medium,
                          const specfem::element::property_tag property) const;
 
+  /**
+   * @brief Get the spectral element indices in which the receivers are located
+   * on the host
+   *
+   * Returns only the indices of the elements that are associated with a
+   * specific medium and property tag
+   * @param medium Medium tag
+   * @property property Property tag
+   * @return Kokkos::View<int *, Kokkos::DefaultExecutionSpace> View of the
+   * elements indices associated with the receivers
+   */
   Kokkos::View<int *, Kokkos::DefaultHostExecutionSpace>
   get_elements_on_host(const specfem::element::medium_tag medium,
                        const specfem::element::property_tag property) const;
 
+  /**
+   * @brief Get the seismogram types
+   *
+   * @return std::vector<specfem::wavefield::type> Vector of seismogram types
+   */
   std::vector<specfem::wavefield::type> get_seismogram_types() const {
     return seismogram_types;
   }
 
 private:
-  IndexViewType elements;
-  IndexViewType::HostMirror h_elements;
+  IndexViewType elements; ///< View to store the elements associated with the
+                          ///< receivers
+  IndexViewType::HostMirror h_elements; ///< Host view to store the
+                                        ///< elements associated with the
+                                        ///< receivers
   IndexViewType receiver_domain_index_mapping;
   IndexViewType::HostMirror h_receiver_domain_index_mapping;
-  LagrangeInterpolantType lagrange_interpolant;
-  LagrangeInterpolantType::HostMirror h_lagrange_interpolant;
+  LagrangeInterpolantType lagrange_interpolant; ///< Lagrange interpolant for
+                                                ///< every receiver
+  LagrangeInterpolantType::HostMirror
+      h_lagrange_interpolant; ///< Lagrange interpolant for every receiver
+                              ///< stored on the host
 
   template <typename MemberType, typename IteratorType, typename ViewType>
   friend KOKKOS_FUNCTION void
@@ -282,6 +385,23 @@ private:
                   const receivers &receivers);
 };
 
+/**
+ * @defgroup ComputeReceiversDataAccess
+ */
+
+/**
+ * @brief Load the Lagrange interpolant for receivers associated with the
+ * iterator on the device
+ *
+ * @ingroup ComputeReceiversDataAccess
+ *
+ * @tparam MemberType Kokkos team member type
+ * @tparam IteratorType Chunk policy iterator type @ref
+ * specfem::policy::element_chunk
+ * @param receivers Receivers object containing the receiver information
+ * @tparam ViewType Lagrange interpolant associated with the receivers in the
+ * iterator
+ */
 template <typename MemberType, typename IteratorType, typename ViewType>
 KOKKOS_FUNCTION void
 load_on_device(const MemberType &team_member, const IteratorType &iterator,
@@ -322,6 +442,22 @@ load_on_device(const MemberType &team_member, const IteratorType &iterator,
   return;
 }
 
+/**
+ * @brief Store the seismogram components for receivers associated with the
+ * iterator on the device
+ *
+ * Before you store the seismogram components, you need to set the seismogram
+ * time step and type. You can do this by calling the following functions:
+ * @c receivers.set_seismogram_step(isig_step);
+ * @c receivers.set_seismogram_type(iseis);
+ *
+ * @ingroup ComputeReceiversDataAccess
+ * @tparam MemberType Kokkos team member type
+ * @tparam IteratorType Chunk policy iterator type @ref
+ * specfem::policy::element_chunk
+ * @tparam SeismogramViewType View of the seismogram components
+ * @param receivers Receivers object containing the receiver information
+ */
 template <typename MemberType, typename IteratorType,
           typename SeismogramViewType>
 KOKKOS_FUNCTION void
