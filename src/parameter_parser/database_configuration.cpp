40bba2e366c8a9648d863ed71906bc01205814db
Lucas Sawade <lsawade@princeton.edu>
2025-01-15 15:48:36 -0500

    this adds capability to add a source into the yaml tree

--- src/parameter_parser/database_configuration.cpp
+++ src/parameter_parser/database_configuration.cpp
@@ -5,10 +5,20 @@
 specfem::runtime_configuration::database_configuration::database_configuration(
     const YAML::Node &Node) {
   try {
-    *this = specfem::runtime_configuration::database_configuration(
-        Node["mesh-database"].as<std::string>(),
-        Node["source-file"].as<std::string>());
+    if (const YAML::Node &source_node = Node["source-file"]) {
+      *this = specfem::runtime_configuration::database_configuration(
+          Node["mesh-database"].as<std::string>(),
+          source_node.as<std::string>());
+    } else if (const YAML::Node &source_node = Node["source-dict"]) {
+      *this = specfem::runtime_configuration::database_configuration(
+          Node["mesh-database"].as<std::string>(), source_node);
+    } else {
+      throw std::runtime_error("Error reading database configuration source "
+                               "field not recognized. \n");
+    }
+
   } catch (YAML::ParserException &e) {
+
     std::ostringstream message;
 
     message << "Error reading database configuration. \n" << e.what();
