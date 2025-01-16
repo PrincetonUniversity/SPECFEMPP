e6862aa519477830a3be999652fbf9187ca24f3f
Lucas Sawade <lsawade@princeton.edu>
2025-01-16 16:14:16 -0500

    Put the onus of deciding what format the stations-file is into the read_receivers function. setup.receivers now just a YAML node

--- src/parameter_parser/receivers.cpp
+++ src/parameter_parser/receivers.cpp
@@ -10,105 +10,114 @@
 #include <string>
 #include <vector>
 
-specfem::runtime_configuration::receivers::receivers(
-    const std::string stations_file, const type_real angle,
-    const int nstep_between_samples) {
+// specfem::runtime_configuration::receivers::receivers(
+//     const std::string stations_file, const type_real angle,
+//     const int nstep_between_samples) {
 
-  // Define basic parameters
-  this->angle = angle;
-  this->nstep_between_samples = nstep_between_samples;
+//   // Define basic parameters
+//   this->angle = angle;
+//   this->nstep_between_samples = nstep_between_samples;
 
-  // Open file stream
-  std::fstream stations;
+//   // Open file stream
+//   std::fstream stations;
 
-  // Define separator
-  boost::char_separator<char> sep(" ");
+//   // Define separator
+//   boost::char_separator<char> sep(" ");
 
-  // Create empty sequence Node
-  this->stations_node = YAML::Load("[]");
+//   // Create empty sequence Node
+//   this->stations_node = YAML::Load("[]");
 
-  // Open stations file
-  stations.open(stations_file, std::ios::in);
+//   // Open stations file
+//   stations.open(stations_file, std::ios::in);
 
-  if (stations.is_open()) {
+//   if (stations.is_open()) {
 
-    std::string line;
+//     std::string line;
 
-    // Read stations file line by line
-    while (std::getline(stations, line)) {
+//     // Read stations file line by line
+//     while (std::getline(stations, line)) {
 
-      // split every line with " " delimiter
-      boost::tokenizer<boost::char_separator<char> > tokens(line, sep);
+//       // split every line with " " delimiter
+//       boost::tokenizer<boost::char_separator<char> > tokens(line, sep);
 
-      // Create a vector to store the current station
-      std::vector<std::string> current_station;
+//       // Create a vector to store the current station
+//       std::vector<std::string> current_station;
 
-      for (const auto &t : tokens) {
-        current_station.push_back(t);
-      }
+//       for (const auto &t : tokens) {
+//         current_station.push_back(t);
+//       }
 
-      // check if the read line meets the format
-      assert(current_station.size() == 6);
+//       // check if the read line meets the format
+//       assert(current_station.size() == 6);
 
-      // get the x and z coordinates of the station;
-      const std::string network_name = current_station[0];
-      const std::string station_name = current_station[1];
-      const std::string x = current_station[2];
-      const std::string z = current_station[3];
+//       // get the x and z coordinates of the station;
+//       const std::string network_name = current_station[0];
+//       const std::string station_name = current_station[1];
+//       const std::string x = current_station[2];
+//       const std::string z = current_station[3];
 
-      // Current station node
-      YAML::Node current_station_node;
+//       // Current station node
+//       YAML::Node current_station_node;
 
-      // Edit node to include the current station
-      current_station_node["network"] = network_name;
-      current_station_node["station"] = station_name;
-      current_station_node["x"] = x;
-      current_station_node["z"] = z;
+//       // Edit node to include the current station
+//       current_station_node["network"] = network_name;
+//       current_station_node["station"] = station_name;
+//       current_station_node["x"] = x;
+//       current_station_node["z"] = z;
 
-      // Append current station to the stations node
-      this->stations_node.push_back(current_station_node);
-    }
-  } else {
-    std::ostringstream message;
-    message << "Could not open stations file: " << stations_file << "\n";
-    message << "Receivers empty.";
-    return;
-  }
-}
+//       // Append current station to the stations node
+//       this->stations_node.push_back(current_station_node);
+//     }
+//   } else {
+//     std::ostringstream message;
+//     message << "Could not open stations file: " << stations_file << "\n";
+//     message << "Receivers empty.";
+//     return;
+//   }
+// }
 
-specfem::runtime_configuration::receivers::receivers(const YAML::Node &Node) {
-  try {
-    if (const YAML::Node &n_stations = Node["stations-file"]) {
-      *this = specfem::runtime_configuration::receivers(
-          n_stations.as<std::string>(), Node["angle"].as<type_real>(),
-          Node["nstep_between_samples"].as<int>());
-    } else if (const YAML::Node &n_stations = Node["stations-dict"]) {
-      *this = specfem::runtime_configuration::receivers(
-          n_stations, Node["angle"].as<type_real>(),
-          Node["nstep_between_samples"].as<int>());
-    } else {
-      throw std::runtime_error("Error reading specfem receiver configuration.");
-    }
-  } catch (YAML::ParserException &e) {
-    std::ostringstream message;
+// specfem::runtime_configuration::receivers::receivers(const YAML::Node &Node)
+// {
+//   try {
+//     if (const YAML::Node &n_stations = Node["stations-file"]) {
+//       *this = specfem::runtime_configuration::receivers(
+//           n_stations.as<std::string>(), Node["angle"].as<type_real>(),
+//           Node["nstep_between_samples"].as<int>());
+//     } else if (const YAML::Node &n_stations = Node["stations-dict"]) {
+//       *this = specfem::runtime_configuration::receivers(
+//           n_stations, Node["angle"].as<type_real>(),
+//           Node["nstep_between_samples"].as<int>());
+//     } else {
+//       throw std::runtime_error("Error reading specfem receiver
+//       configuration.");
+//     }
+//   } catch (YAML::ParserException &e) {
+//     std::ostringstream message;
 
-    message << "Error reading specfem receiver configuration. \n" << e.what();
+//     message << "Error reading specfem receiver configuration. \n" <<
+//     e.what();
 
-    throw std::runtime_error(message.str());
-  }
+//     throw std::runtime_error(message.str());
+//   }
+// }
+
+std::vector<specfem::enums::seismogram::type>
+specfem::runtime_configuration::receivers::get_seismogram_types() const {
+
+  std::vector<specfem::enums::seismogram::type> stypes;
 
   // Allocate seismogram types
-  assert(Node["seismogram-type"].IsSequence());
+  assert(this->receivers_node["seismogram-type"].IsSequence());
 
-  for (YAML::Node seismogram_type : Node["seismogram-type"]) {
+  for (YAML::Node seismogram_type : this->receivers_node["seismogram-type"]) {
     if (seismogram_type.as<std::string>() == "displacement") {
-      this->stypes.push_back(specfem::enums::seismogram::type::displacement);
+      stypes.push_back(specfem::enums::seismogram::type::displacement);
     } else if (seismogram_type.as<std::string>() == "velocity") {
-      this->stypes.push_back(specfem::enums::seismogram::type::velocity);
+      stypes.push_back(specfem::enums::seismogram::type::velocity);
     } else if (seismogram_type.as<std::string>() == "acceleration") {
-      this->stypes.push_back(specfem::enums::seismogram::type::acceleration);
+      stypes.push_back(specfem::enums::seismogram::type::acceleration);
     } else if (seismogram_type.as<std::string>() == "pressure") {
-      this->stypes.push_back(specfem::enums::seismogram::type::pressure);
+      stypes.push_back(specfem::enums::seismogram::type::pressure);
     } else {
       std::ostringstream message;
 
@@ -119,4 +128,5 @@ specfem::runtime_configuration::receivers::receivers(const YAML::Node &Node) {
       std::runtime_error(message.str());
     }
   }
+  return stypes;
 }
