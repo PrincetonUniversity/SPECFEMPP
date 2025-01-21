df9e8688efee57f4822dbe0eafd1ff46efd1301b
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2025-01-21 04:02:47 -0500

    Added precomputed element variables to improve performance

--- include/compute/element_types/element_types.hpp
+++ include/compute/element_types/element_types.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "compute/compute_mesh.hpp"
+#include "enumerations/material_definitions.hpp"
 #include "enumerations/medium.hpp"
 #include "mesh/materials/materials.hpp"
 #include "point/coordinates.hpp"
@@ -26,9 +27,15 @@ protected:
                                                        ///< tags
   using BoundaryViewType =
       Kokkos::View<specfem::element::boundary_tag *,
-                   Kokkos::DefaultHostExecutionSpace>; //< Underlying view type
-                                                       // to store
-                                                       // boundary tags
+                   Kokkos::DefaultHostExecutionSpace>; ///< Underlying view type
+                                                       ///< to store
+                                                       ///< boundary tags
+
+  using IndexViewType =
+      Kokkos::View<int *, Kokkos::DefaultExecutionSpace>; ///< Underlying view
+                                                          ///< type to store
+                                                          ///< indices of
+                                                          ///< elements
 
 public:
   int nspec; ///< total number of spectral elements
@@ -94,6 +101,55 @@ public:
   specfem::element::boundary_tag get_boundary_tag(const int ispec) const {
     return boundary_tags(ispec);
   }
+
+private:
+#define MEDIUM_TAG_VARIABLES(DIMENSION_TAG, MEDIUM_TAG)                        \
+  IndexViewType CREATE_VARIABLE_NAME(elements, GET_NAME(DIMENSION_TAG),        \
+                                     GET_NAME(MEDIUM_TAG));                    \
+  IndexViewType::HostMirror CREATE_VARIABLE_NAME(                              \
+      h_elements, GET_NAME(DIMENSION_TAG), GET_NAME(MEDIUM_TAG));
+
+  CALL_MACRO_FOR_ALL_MEDIUM_TAGS(MEDIUM_TAG_VARIABLES,
+                                 WHERE(DIMENSION_TAG_DIM2)
+                                     WHERE(MEDIUM_TAG_ELASTIC,
+                                           MEDIUM_TAG_ACOUSTIC))
+
+#undef MEDIUM_TAG_VARIABLES
+
+#define MATERIAL_SYSTEMS_VARIABLE_NAMES(DIMENSION_TAG, MEDIUM_TAG,             \
+                                        PROPERTY_TAG)                          \
+  IndexViewType CREATE_VARIABLE_NAME(elements, GET_NAME(DIMENSION_TAG),        \
+                                     GET_NAME(MEDIUM_TAG),                     \
+                                     GET_NAME(PROPERTY_TAG));                  \
+  IndexViewType::HostMirror CREATE_VARIABLE_NAME(                              \
+      h_elements, GET_NAME(DIMENSION_TAG), GET_NAME(MEDIUM_TAG),               \
+      GET_NAME(PROPERTY_TAG));
+
+  CALL_MACRO_FOR_ALL_MATERIAL_SYSTEMS(
+      MATERIAL_SYSTEMS_VARIABLE_NAMES,
+      WHERE(DIMENSION_TAG_DIM2) WHERE(MEDIUM_TAG_ELASTIC, MEDIUM_TAG_ACOUSTIC)
+          WHERE(PROPERTY_TAG_ISOTROPIC, PROPERTY_TAG_ANISOTROPIC))
+
+#undef MATERIAL_SYSTEMS_VARIABLE_NAMES
+
+#define ELEMENT_TYPES_VARIABLE_NAMES(DIMENSION_TAG, MEDIUM_TAG, PROPERTY_TAG,  \
+                                     BOUNDARY_TAG)                             \
+  IndexViewType CREATE_VARIABLE_NAME(                                          \
+      elements, GET_NAME(DIMENSION_TAG), GET_NAME(MEDIUM_TAG),                 \
+      GET_NAME(PROPERTY_TAG), GET_NAME(BOUNDARY_TAG));                         \
+  IndexViewType::HostMirror CREATE_VARIABLE_NAME(                              \
+      h_elements, GET_NAME(DIMENSION_TAG), GET_NAME(MEDIUM_TAG),               \
+      GET_NAME(PROPERTY_TAG), GET_NAME(BOUNDARY_TAG));
+
+  CALL_MACRO_FOR_ALL_ELEMENT_TYPES(
+      ELEMENT_TYPES_VARIABLE_NAMES,
+      WHERE(DIMENSION_TAG_DIM2) WHERE(MEDIUM_TAG_ELASTIC, MEDIUM_TAG_ACOUSTIC)
+          WHERE(PROPERTY_TAG_ISOTROPIC, PROPERTY_TAG_ANISOTROPIC)
+              WHERE(BOUNDARY_TAG_NONE, BOUNDARY_TAG_ACOUSTIC_FREE_SURFACE,
+                    BOUNDARY_TAG_STACEY,
+                    BOUNDARY_TAG_COMPOSITE_STACEY_DIRICHLET))
+
+#undef ELEMENT_TYPES_VARIABLE_NAMES
 };
 
 } // namespace compute
