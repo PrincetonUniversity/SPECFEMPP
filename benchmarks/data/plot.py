d6e0a57f762e3c46c180107bf9380fd45e52501c
ccui <ccui@princeton.edu>
2025-01-24 11:32:26 -0500

    copy examples.

---
+++ benchmarks/data/plot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import glob
+import os
+import numpy as np
+import obspy
+
+
+def get_traces(directory):
+    traces = []
+    files = glob.glob(directory + "/*.sem*")
+    ## iterate over all seismograms
+    for filename in files:
+        station_name = os.path.splitext(filename)[0]
+        station_name = station_name.split("/")[-1]
+        trace = np.loadtxt(filename, delimiter=" ")
+        starttime = trace[0, 0]
+        dt = trace[1, 0] - trace[0, 0]
+        traces.append(
+            obspy.Trace(
+                trace[:, 1],
+                {"network": station_name, "starttime": starttime, "delta": dt},
+            )
+        )
+
+    stream = obspy.Stream(traces)
+
+    return stream
+
+
+stream = get_traces("results")
+stream.plot(size=(800, 1000)).savefig("results/plot.png")
