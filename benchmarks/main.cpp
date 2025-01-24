cc6dfa8a8e79674f5e406d69aa9dd6cf94ca581d
ccui <ccui@princeton.edu>
2025-01-24 13:23:05 -0500

    Split solver.

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -1,5 +1,88 @@
 #include "execute.hpp"
 
+void benchmark(specfem::compute::assembly &assembly,
+               std::shared_ptr<specfem::time_scheme::time_scheme> time_scheme) {
+  auto kernels = specfem::kokkos_kernels::domain_kernels<
+      specfem::wavefield::simulation_field::forward,
+      specfem::dimension::type::dim2, 5>(assembly);
+  constexpr auto elastic = specfem::element::medium_tag::elastic;
+
+  kernels.initialize(time_scheme->get_timestep());
+
+  const int nstep = time_scheme->get_max_timestep();
+
+  for (const auto [istep, dt] : time_scheme->iterate_forward()) {
+    time_scheme->apply_predictor_phase_forward(elastic);
+
+    kernels.template update_wavefields<elastic>(istep);
+    time_scheme->apply_corrector_phase_forward(elastic);
+
+    if ((istep + 1) % 100 == 0) {
+      std::cout << "Progress : executed " << istep + 1 << " steps of " << nstep
+                << " steps" << std::endl;
+    }
+  }
+
+  std::cout << std::endl;
+}
+
+void create_assembly(const YAML::Node &parameter_dict,
+                     const YAML::Node &default_dict, specfem::MPI::MPI *mpi) {
+
+  // --------------------------------------------------------------
+  //                    Read parameter file
+  // --------------------------------------------------------------
+  auto start_time = std::chrono::system_clock::now();
+  specfem::runtime_configuration::setup setup(parameter_dict, default_dict);
+  const auto database_filename = setup.get_databases();
+  mpi->cout(setup.print_header(start_time));
+
+  // --------------------------------------------------------------
+
+  // --------------------------------------------------------------
+  //                   Read mesh and materials
+  // --------------------------------------------------------------
+  const auto quadrature = setup.instantiate_quadrature();
+  const auto mesh = specfem::IO::read_mesh(database_filename, mpi);
+
+  // --------------------------------------------------------------
+
+  // --------------------------------------------------------------
+  //                   Instantiate Timescheme
+  // --------------------------------------------------------------
+  const auto time_scheme = setup.instantiate_timescheme();
+  if (mpi->main_proc())
+    std::cout << *time_scheme << std::endl;
+
+  const int max_seismogram_time_step = time_scheme->get_max_seismogram_step();
+
+  const int nstep_between_samples = time_scheme->get_nstep_between_samples();
+  const type_real dt = setup.get_dt();
+  // --------------------------------------------------------------
+
+  // --------------------------------------------------------------
+  //                   Generate Assembly
+  // --------------------------------------------------------------
+  mpi->cout("Generating assembly:");
+  mpi->cout("-------------------------------");
+  const std::vector<std::shared_ptr<specfem::sources::source> > sources;
+  const std::vector<std::shared_ptr<specfem::receivers::receiver> > receivers;
+  const int nsteps = setup.get_nsteps();
+  specfem::compute::assembly assembly(
+      mesh, quadrature, sources, receivers, setup.get_seismogram_types(),
+      setup.get_t0(), dt, nsteps, max_seismogram_time_step,
+      nstep_between_samples, setup.get_simulation_type(),
+      setup.instantiate_property_reader());
+  time_scheme->link_assembly(assembly);
+
+  const auto solver_start_time = std::chrono::system_clock::now();
+  benchmark(assembly, time_scheme);
+  const auto solver_end_time = std::chrono::system_clock::now();
+  std::chrono::duration<double> solver_time =
+      solver_end_time - solver_start_time;
+  std::cout << "Solver time: " << solver_time.count() << "s" << std::endl;
+}
+
 int main(int argc, char **argv) {
   // Initialize MPI
   specfem::MPI::MPI *mpi = new specfem::MPI::MPI(&argc, &argv);
@@ -7,11 +90,11 @@ int main(int argc, char **argv) {
   Kokkos::initialize(argc, argv);
   {
     const std::string default_file = __default_file__;
-    const std::string parameters_file = __parameters_file__;
+    const std::string parameters_file = __benchmark_parameters__;
     const YAML::Node parameter_dict = YAML::LoadFile(parameters_file);
     const YAML::Node default_dict = YAML::LoadFile(default_file);
     std::vector<std::shared_ptr<specfem::periodic_tasks::periodic_task> > tasks;
-    execute(parameter_dict, default_dict, tasks, mpi);
+    create_assembly(parameter_dict, default_dict, mpi);
   }
   // Finalize Kokkos
   Kokkos::finalize();
