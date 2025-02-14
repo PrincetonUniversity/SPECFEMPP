b5fec37e16d9d4e04801a81f5395332b09e682f2
Lucas Sawade <lsawade@princeton.edu>
2025-02-14 16:17:58 -0500

    Updated array reading into a templated utilities header

--- include/mesh/mesh.hpp
+++ include/mesh/mesh.hpp
@@ -10,6 +10,8 @@
 #include "materials/materials.hpp"
 #include "mesh/coordinates/coordinates.hpp"
 #include "mesh/mapping/mapping.hpp"
+#include "mesh/parameters/parameters.hpp"
+#include "mesh/partial_derivatives/partial_derivatives.hpp"
 #include "mesh/tags/tags.hpp"
 #include "parameters/parameters.hpp"
 #include "specfem_mpi/interface.hpp"
@@ -107,6 +109,8 @@ template <> struct mesh<specfem::dimension::type::dim3> {
   constexpr static auto dimension =
       specfem::dimension::type::dim3; ///< Dimension
 
+  template <typename T> using View1D = Kokkos::View<T *, Kokkos::HostSpace>;
+
   // Struct to store all the mesh parameter
   specfem::mesh::parameters<dimension> parameters;
 
@@ -115,6 +119,13 @@ template <> struct mesh<specfem::dimension::type::dim3> {
 
   // Struct to store the mapping information
   specfem::mesh::mapping<dimension> mapping;
+
+  // Irregular elements Kokkos
+  type_real xix_regular, jacobian_regular;
+  View1D<int> irregular_element_number;
+
+  // Struct to store the partial derivatives
+  specfem::mesh::partial_derivatives<dimension> partial_derivatives;
 
   //
   // int npgeo; ///< Total number of spectral element control nodes
--- src/mesh/coordinates/coordinates.cpp
+++ src/mesh/coordinates/coordinates.cpp
@@ -21,20 +21,3 @@ void specfem::mesh::coordinates<specfem::dimension::type::dim3>::print(
             << "z: " << z(iglob) << "\n"
             << "------------------------------\n";
 }
-
-void specfem::mesh::coordinates<specfem::dimension::type::dim3>::print(
-    int ispec, int igllx, int iglly, int igllz) const {
-  std::cout << "Coordinates parameters for spectral element " << ispec << ":\n"
-            << "------------------------------\n"
-            << "xix:..... " << xix(ispec, igllx, iglly, igllz) << "\n"
-            << "xiy:..... " << xiy(ispec, igllx, iglly, igllz) << "\n"
-            << "xiz:..... " << xiz(ispec, igllx, iglly, igllz) << "\n"
-            << "etax:.... " << etax(ispec, igllx, iglly, igllz) << "\n"
-            << "etay:.... " << etay(ispec, igllx, iglly, igllz) << "\n"
-            << "etaz:.... " << etaz(ispec, igllx, iglly, igllz) << "\n"
-            << "gammax:.. " << gammax(ispec, igllx, iglly, igllz) << "\n"
-            << "gammay:.. " << gammay(ispec, igllx, iglly, igllz) << "\n"
-            << "gammaz:.. " << gammaz(ispec, igllx, iglly, igllz) << "\n"
-            << "jacobian: " << jacobian(ispec, igllx, iglly, igllz) << "\n"
-            << "------------------------------\n";
-}
