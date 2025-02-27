7511696ee6b4349f51f58772b2badf32afff2f8c
Congyue Cui <ccui@princeton.edu>
2025-02-27 10:23:34 -0500

    Merge material_properties and properties_container

--- include/medium/properties_container.hpp
+++ include/medium/properties_container.hpp
@@ -41,6 +41,43 @@ struct impl_properties_container {
         data("specfem::benchmarks::properties::data", nspec, ngllz, ngllx, N),
         h_data(Kokkos::create_mirror_view(data)) {}
 
+  impl_properties_container(
+      const Kokkos::View<int *, Kokkos::DefaultHostExecutionSpace> elements,
+      const int ngllz, const int ngllx,
+      const specfem::mesh::materials &materials, const bool has_gll_model,
+      const specfem::kokkos::HostView1d<int> property_index_mapping)
+      : impl_properties_container(elements.extent(0), ngllz, ngllx) {
+
+    const int nelement = elements.extent(0);
+    int count = 0;
+    for (int i = 0; i < nelement; ++i) {
+      const int ispec = elements(i);
+      property_index_mapping(ispec) = count;
+      if (!has_gll_model) {
+        for (int iz = 0; iz < ngllz; ++iz) {
+          for (int ix = 0; ix < ngllx; ++ix) {
+            // Get the material at index from mesh::materials
+            auto material =
+                std::get<specfem::medium::material<medium_tag, property_tag> >(
+                    materials[ispec]);
+
+            // Assign the material property to the property container
+            auto point_property = material.get_properties();
+            this->assign(specfem::point::index<dimension>(count, iz, ix),
+                         point_property);
+          }
+        }
+      }
+      count++;
+    }
+
+    if (!has_gll_model) {
+      this->copy_to_device();
+    }
+
+    return;
+  }
+
 private:
   template <bool on_device, typename PointProperties>
   KOKKOS_FORCEINLINE_FUNCTION void
