191c23f41bf91f59d7fa776eb74bf43ddb6458fd
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2025-02-27 15:13:55 -0500

    Added mass matrix computation for SH domains

--- include/medium/dim2/elastic/isotropic/mass_matrix.hpp
+++ include/medium/dim2/elastic/isotropic/mass_matrix.hpp
@@ -22,5 +22,16 @@ impl_mass_matrix_component(
     const specfem::point::partial_derivatives<
         specfem::dimension::type::dim2, true, UseSIMD> &partial_derivatives);
 
+template <bool UseSIMD, specfem::element::property_tag PropertyTag>
+KOKKOS_FUNCTION specfem::point::field<specfem::dimension::type::dim2,
+                                      specfem::element::medium_tag::elastic_sh,
+                                      false, false, false, true, UseSIMD>
+impl_mass_matrix_component(
+    const specfem::point::properties<specfem::dimension::type::dim2,
+                                     specfem::element::medium_tag::elastic_sh,
+                                     PropertyTag, UseSIMD> &properties,
+    const specfem::point::partial_derivatives<
+        specfem::dimension::type::dim2, true, UseSIMD> &partial_derivatives);
+
 } // namespace medium
 } // namespace specfem
