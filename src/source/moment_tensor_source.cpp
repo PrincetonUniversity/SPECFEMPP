24f5502698ae8a02a5698a79ffe5f372c97cd4c5
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2025-03-19 15:08:31 -0400

    Another typo

--- src/source/moment_tensor_source.cpp
+++ src/source/moment_tensor_source.cpp
@@ -40,7 +40,7 @@ void specfem::sources::moment_tensor::compute_source_array(
 
   const int ncomponents = source_array.extent(0);
   if ((el_type == specfem::element::medium_tag::elastic_sv) ||
-      (el_type = specfem::element::medium_tag::electromagnetic_sv)) {
+      (el_type == specfem::element::medium_tag::electromagnetic_sv)) {
     if (ncomponents != 2) {
       throw std::runtime_error(
           "Moment tensor source requires 2 components for elastic medium");
