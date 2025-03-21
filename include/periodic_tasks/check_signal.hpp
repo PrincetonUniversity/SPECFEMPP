dc992b6f964b39748a7c57ebf6cb8aadb7d52f5b
Lucas Sawade <lsawade@princeton.edu>
2025-03-21 10:58:22 -0400

    Updated the error throwing in to interface with python

--- include/periodic_tasks/check_signal.hpp
+++ include/periodic_tasks/check_signal.hpp
@@ -1,7 +1,28 @@
 #pragma once
 #include "periodic_task.hpp"
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
+#include <stdexcept>
+#include <csignal>
+#include <string>
+
+/**
+ * @brief Catch signals
+ * 
+ * This function is catching the keyboard interrupt signal and other interrupt 
+ * signals to parse them as exceptions to Python without crashing or hanging
+ * the program. This is to avoid the program to hang via 
+ * @code
+ * if (PyErr_CheckSignals() != 0) {
+ *     throw pybind11::error_already_set();
+ * }
+ * @endcode
+ * if releasing the GIL in execute() function.
+ */
+void catch_signals() {
+  auto handler = [](int code) { throw std::runtime_error("SIGNAL " + std::to_string(code)); };
+  signal(SIGINT, handler);
+  signal(SIGTERM, handler);
+  signal(SIGKILL, handler);
+}
 
 namespace specfem {
 namespace periodic_tasks {
@@ -17,9 +38,7 @@ class check_signal : public periodic_task {
    *
    */
   void run() override {
-    if (PyErr_CheckSignals() != 0) {
-      throw pybind11::error_already_set();
-    }
+    catch_signals();
   }
 };
 
