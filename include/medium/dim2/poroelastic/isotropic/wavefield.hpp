5de4ae582737682842b9202fdb1f217519126fd4
Christina Morency <morency1@llnl.gov>
2025-03-25 13:49:41 -0700

    Modified and created files for wavefields for poroelastic

---
+++ include/medium/dim2/poroelastic/isotropic/wavefield.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "algorithms/dot.hpp"
+#include "algorithms/gradient.hpp"
+#include "enumerations/medium.hpp"
+#include "medium/compute_stress.hpp"
+#include "point/field_derivatives.hpp"
+#include "point/properties.hpp"
+#include <Kokkos_Core.hpp>
+
+namespace specfem {
+namespace medium {
+
+template <typename MemberType, typename IteratorType, typename ChunkFieldType,
+          typename QuadratureType, typename WavefieldViewType>
+KOKKOS_FUNCTION void impl_compute_wavefield(
+    const std::integral_constant<specfem::dimension::type,
+                                 specfem::dimension::type::dim2>,
+    const std::integral_constant<specfem::element::medium_tag,
+                                 specfem::element::medium_tag::poroelastic>,
+    const std::integral_constant<specfem::element::property_tag,
+                                 specfem::element::property_tag::isotropic>,
+    const MemberType &team, const IteratorType &iterator,
+    const specfem::compute::assembly &assembly,
+    const QuadratureType &quadrature, const ChunkFieldType &field,
+    const specfem::wavefield::type wavefield_component,
+    WavefieldViewType wavefield) {
+
+  using FieldDerivativesType = specfem::point::field_derivatives<
+      specfem::dimension::type::dim2, specfem::element::medium_tag::poroelastic,
+      false>;
+
+  using PointPropertyType = specfem::point::properties<
+      specfem::dimension::type::dim2, specfem::element::medium_tag::poroelastic,
+      specfem::element::property_tag::isotropic, false>;
+
+  const auto &properties = assembly.properties;
+
+  const auto &active_field = [&]() {
+    if (wavefield_component == specfem::wavefield::type::displacement) {
+      return field.displacement;
+    } else if (wavefield_component == specfem::wavefield::type::velocity) {
+      return field.velocity;
+    } else if (wavefield_component == specfem::wavefield::type::acceleration) {
+      return field.acceleration;
+    } else if (wavefield_component == specfem::wavefield::type::pressure) {
+      return field.displacement;
+    } else {
+      Kokkos::abort("component not supported");
+    }
+  }();
+
+  if (wavefield_component == specfem::wavefield::type::pressure) {
+
+    specfem::algorithms::gradient(
+        team, iterator, assembly.partial_derivatives, quadrature.hprime_gll,
+        active_field,
+        [&](const typename IteratorType::index_type &iterator_index,
+            const FieldDerivativesType::ViewType &du) {
+          const auto &index = iterator_index.index;
+          PointPropertyType point_property;
+
+          specfem::compute::load_on_device(index, properties, point_property);
+
+          // Fluid pressure
+          // sigmap = -pf = C_biot*(dux_dxl + duz_dzl) + M_biot*(dwx_dxl +
+          // dwz_dzl)
+
+          wavefield(iterator_index.ielement, index.iz, index.ix, 0) =
+              -(point_property.C_Biot() * (du(0, 0) + du(1, 1)) +
+                point_property.M_Biot() * (du(0, 2) + du(1, 3)));
+        });
+
+    return;
+  }
+
+  Kokkos::parallel_for(
+      Kokkos::TeamThreadRange(team, iterator.chunk_size()), [&](const int &i) {
+        const auto iterator_index = iterator(i);
+        const auto &index = iterator_index.index;
+        wavefield(iterator_index.ielement, index.iz, index.ix, 0) =
+            active_field(iterator_index.ielement, index.iz, index.ix, 0);
+        wavefield(iterator_index.ielement, index.iz, index.ix, 1) =
+            active_field(iterator_index.ielement, index.iz, index.ix, 1);
+      });
+
+  return;
+}
+
+} // namespace medium
+} // namespace specfem
