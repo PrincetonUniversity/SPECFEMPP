e07cd1f91328a58efef9e1ffaec374660535d566
Lucas Sawade <lsawade@princeton.edu>
2025-04-08 15:33:20 -0400

    Update the mass_matrix computation

--- include/medium/compute_mass_matrix.hpp
+++ include/medium/compute_mass_matrix.hpp
@@ -3,6 +3,7 @@
 #include "dim2/acoustic/isotropic/mass_matrix.tpp"
 #include "dim2/elastic/anisotropic/mass_matrix.tpp"
 #include "dim2/elastic/isotropic/mass_matrix.tpp"
+#include "dim2/elastic/isotropic_cosserat/mass_matrix.tpp"
 #include "dim2/poroelastic/isotropic/mass_matrix.tpp"
 #include "enumerations/dimension.hpp"
 #include "enumerations/material_definitions.hpp"
