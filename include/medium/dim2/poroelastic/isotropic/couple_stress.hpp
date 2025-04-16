5e4b07bf6904816b8cf3cc8279b54bd5910823b3
Lucas Sawade <lsawade@princeton.edu>
2025-04-16 13:32:19 +0200

    Forgot to add some files; changed one sign, just to try but still nan

---
+++ include/medium/dim2/poroelastic/isotropic/couple_stress.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "enumerations/dimension.hpp"
+#include "enumerations/medium.hpp"
+#include <Kokkos_Core.hpp>
+
+namespace specfem {
+namespace medium {
+
+template <typename T, typename PointPartialDerivativesType,
+          typename PointStressIntegrandViewType, typename PointPropertiesType,
+          typename PointAccelerationType>
+KOKKOS_FUNCTION void impl_compute_couple_stress(
+    const std::integral_constant<specfem::dimension::type,
+                                 specfem::dimension::type::dim2>,
+    const std::integral_constant<specfem::element::medium_tag,
+                                 specfem::element::medium_tag::poroelastic>,
+    const std::integral_constant<specfem::element::property_tag,
+                                 specfem::element::property_tag::isotropic>,
+    const PointPartialDerivativesType &point_partial_derivatives,
+    const PointPropertiesType &point_properties, const T factor,
+    const PointStressIntegrandViewType &F,
+    PointAccelerationType &acceleration) {};
+
+} // namespace medium
+} // namespace specfem
