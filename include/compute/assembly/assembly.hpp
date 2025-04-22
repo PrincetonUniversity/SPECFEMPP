2f01748744fdbe812f548457f40b697a66962efc
Lucas Sawade <lsawade@princeton.edu>
2025-04-22 12:50:24 +0200

    Removed the seismogram type and replacede it with wavefield type

--- include/compute/assembly/assembly.hpp
+++ include/compute/assembly/assembly.hpp
@@ -13,6 +13,7 @@
 #include "compute/sources/sources.hpp"
 #include "enumerations/display.hpp"
 #include "enumerations/interface.hpp"
+#include "enumerations/wavefield.hpp"
 #include "io/reader.hpp"
 #include "mesh/mesh.hpp"
 #include "receiver/interface.hpp"
@@ -78,9 +79,9 @@ struct assembly {
       const std::vector<std::shared_ptr<specfem::sources::source> > &sources,
       const std::vector<std::shared_ptr<specfem::receivers::receiver> >
           &receivers,
-      const std::vector<specfem::enums::seismogram::type> &stypes,
-      const type_real t0, const type_real dt, const int max_timesteps,
-      const int max_sig_step, const int nsteps_between_samples,
+      const std::vector<specfem::wavefield::type> &stypes, const type_real t0,
+      const type_real dt, const int max_timesteps, const int max_sig_step,
+      const int nsteps_between_samples,
       const specfem::simulation::type simulation,
       const std::shared_ptr<specfem::io::reader> &property_reader);
 
@@ -106,7 +107,6 @@ struct assembly {
   }
 
   std::string print() const;
-
 };
 
 } // namespace compute
