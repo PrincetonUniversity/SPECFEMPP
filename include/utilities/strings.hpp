18d60f11705ec1945f5cf940a33d114f7bf25813
Congyue Cui <ccui@princeton.edu>
2025-04-23 13:21:03 -0400

    Use macros for config string test functions.

--- include/utilities/strings.hpp
+++ include/utilities/strings.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "enumerations/config_strings.hpp"
+#include "enumerations/macros_impl/config_strings.hpp"
 #include <string>
 
 namespace specfem {
@@ -14,71 +16,7 @@ std::string snake_to_pascal(const std::string &str);
 // convert string to lower case
 std::string to_lower(const std::string &str);
 
-// Check if string is hdf5
-bool is_hdf5_string(const std::string &str);
-
-// Check if string is ascii
-bool is_ascii_string(const std::string &str);
-
-// check if string indicates P-SV wave
-bool is_psv_string(const std::string &str);
-
-// check if string indicates SH wave
-bool is_sh_string(const std::string &str);
-
-// check if string indicates TE wave
-bool is_te_string(const std::string &str);
-
-// check if string indicates TM wave
-bool is_tm_string(const std::string &str);
-
-// check if string is jpg
-bool is_jpg_string(const std::string &str);
-
-// check if string is png
-bool is_png_string(const std::string &str);
-
-// check if string is sac
-bool is_sac_string(const std::string &str);
-
-// check if string is seismic unix
-bool is_su_string(const std::string &str);
-
-// check if string indicates forward simulation
-bool is_forward_string(const std::string &str);
-
-// check if string indicates combined simulation
-bool is_combined_string(const std::string &str);
-
-// check if string indicates backward simulation
-bool is_backward_string(const std::string &str);
-
-// check if string indicates adjoint simulation
-bool is_adjoint_string(const std::string &str);
-
-// check if string is GLL-4
-bool is_gll4_string(const std::string &str);
-
-// check if string is GLL-7
-bool is_gll7_string(const std::string &str);
-
-// check if string indicates displacement
-bool is_displacement_string(const std::string &str);
-
-// check if string indicates velocity
-bool is_velocity_string(const std::string &str);
-
-// check if string indicates acceleration
-bool is_acceleration_string(const std::string &str);
-
-// check if string indicates pressure
-bool is_pressure_string(const std::string &str);
-
-// check if string is Newmark
-bool is_newmark_string(const std::string &str);
-
-// check if string is on_screen
-bool is_onscreen_string(const std::string &str);
+BOOST_PP_SEQ_FOR_EACH(_DECLARE_CONFIG_STRING_FUNCTIONS, _, CONFIG_STRINGS)
 
 } // namespace utilities
 } // namespace specfem
