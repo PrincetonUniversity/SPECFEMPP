3e99685f7a2b4a4904dfe15bafa2d9d2a037b243
Congyue Cui <ccui@princeton.edu>
2025-04-24 13:51:14 -0400

    Bug fix.

--- include/enumerations/macros_impl/array.hpp
+++ include/enumerations/macros_impl/array.hpp
@@ -3,22 +3,24 @@
  * Used by medium_types(), material_systems() and element_types()
  */
 #define _MAKE_CONSTEXPR_ARRAY(seq)                                             \
-  BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_TRANSFORM(                                    \
-      BOOST_PP_CAT(_MAKE_ARRAY_, BOOST_PP_SEQ_SIZE(seq)), _, seq))
+  BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_TRANSFORM(_MAKE_ARRAY, _, seq))
 
 /**
  * @brief Sequence transformation macros for _MAKE_CONSTEXPR_ARRAY.
  */
-#define _MAKE_ARRAY_2(s, data, elem)                                           \
+#define _MAKE_ARRAY(s, data, elem)                                             \
+  BOOST_PP_CAT(_MAKE_ARRAY_, BOOST_PP_TUPLE_SIZE(elem))(elem)
+
+#define _MAKE_ARRAY_2(elem)                                                    \
   std::make_tuple(_GET_TAG(BOOST_PP_TUPLE_ELEM(0, elem)),                      \
                   _GET_TAG(BOOST_PP_TUPLE_ELEM(1, elem)))
 
-#define _MAKE_ARRAY_3(s, data, elem)                                           \
+#define _MAKE_ARRAY_3(elem)                                                    \
   std::make_tuple(_GET_TAG(BOOST_PP_TUPLE_ELEM(0, elem)),                      \
                   _GET_TAG(BOOST_PP_TUPLE_ELEM(1, elem)),                      \
                   _GET_TAG(BOOST_PP_TUPLE_ELEM(2, elem)))
 
-#define _MAKE_ARRAY_4(s, data, elem)                                           \
+#define _MAKE_ARRAY_4(elem)                                                    \
   std::make_tuple(_GET_TAG(BOOST_PP_TUPLE_ELEM(0, elem)),                      \
                   _GET_TAG(BOOST_PP_TUPLE_ELEM(1, elem)),                      \
                   _GET_TAG(BOOST_PP_TUPLE_ELEM(2, elem)),                      \
