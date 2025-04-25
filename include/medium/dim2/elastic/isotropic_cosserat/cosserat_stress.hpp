6186873259f3f82bdcd8821fbf5b5cd3441e7cd2
Lucas Sawade <lsawade@princeton.edu>
2025-04-25 14:50:18 +0200

    Fogrot to add cosserat stress

---
+++ include/medium/dim2/elastic/isotropic_cosserat/cosserat_stress.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "enumerations/dimension.hpp"
+#include "enumerations/medium.hpp"
+#include <Kokkos_Core.hpp>
+
+namespace specfem {
+namespace medium {
+
+template <typename PointPropertiesType, typename PointDisplacementType,
+          typename PointStressType>
+KOKKOS_INLINE_FUNCTION void impl_compute_cosserat_stress(
+    std::true_type,
+    const std::integral_constant<specfem::dimension::type,
+                                 specfem::dimension::type::dim2>,
+    const std::integral_constant<specfem::element::medium_tag,
+                                 specfem::element::medium_tag::elastic_psv_t>,
+    const std::integral_constant<
+        specfem::element::property_tag,
+        specfem::element::property_tag::isotropic_cosserat>,
+    const PointPropertiesType &properties,
+    const PointDisplacementType &point_displacement,
+    PointStressType &point_stress) {
+
+  using ViewType = typename PointStressType::ViewType;
+
+  // Stress and diplacement alias
+  // auto &T = point_stress.T;
+  const auto &u = point_displacement.displacement;
+
+  const typename ViewType::value_type factor = properties.nu() * u(2);
+
+  // Component t_xz = ... +  (2 \nu \phi_{y})
+  point_stress.T(0, 1) +=
+      factor; // static_cast<type_real>(2.0) * properties.nu() * u(2);
+
+  // Component t_zx = ... +  (-2 \nu \phi_{y})
+  point_stress.T(1, 0) +=
+      factor; // static_cast<type_real>(-2.0) * properties.nu() * u(2);
+
+  return;
+};
+
+} // namespace medium
+} // namespace specfem
