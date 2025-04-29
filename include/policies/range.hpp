4abecce0bb6cbcaf737bcec86b0b96e3b3e2ae1d
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2025-04-29 14:29:05 -0400

    Added tiling for Range loops
    - [ ] Adds a tiling parameter for range loops
    - [ ] Updates `range_iterator` and `league_iterator` methods so that
     starting index of the thread can be computed internall within the policies
    - [ ] Adds `is_end` method to check if the iterator is out of bounds
      (should be only true when tile_size > 1)

--- include/policies/range.hpp
+++ include/policies/range.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "point/assembly_index.hpp"
+#include <iostream>
 #include <type_traits>
 
 namespace specfem {
@@ -58,6 +59,7 @@ private:
   int starting_index; ///< Starting index for the iterator range.
   int number_points;  ///< Number of points in the iterator range. Equal to or
                       ///< less than SIMD size when using SIMD.
+  bool end_iterator = false; ///< Flag to indicate if the iterator is at the end
 
   constexpr static bool using_simd = SIMD::using_simd;
   constexpr static int simd_size = SIMD::size();
@@ -72,7 +74,7 @@ private:
 
   // range_index_type operator for simd execution
   KOKKOS_INLINE_FUNCTION
-  impl::range_index_type<true> operator()(const int i, std::true_type) const {
+  impl::range_index_type<true> operator()(std::true_type) const {
     return impl::range_index_type<true>(
         specfem::point::simd_assembly_index{ starting_index, number_points });
   }
@@ -84,7 +86,7 @@ private:
       : starting_index(starting_index), number_points(number_points) {}
 
   KOKKOS_INLINE_FUNCTION
-  impl::range_index_type<false> operator()(const int i, std::false_type) const {
+  impl::range_index_type<false> operator()(std::false_type) const {
     return impl::range_index_type<false>(
         specfem::point::assembly_index<false>{ starting_index });
   }
@@ -112,6 +114,9 @@ public:
   KOKKOS_INLINE_FUNCTION
   range() = default;
 
+  KOKKOS_INLINE_FUNCTION
+  range(const bool end_iterator) : end_iterator(end_iterator) {}
+
   /**
    * @brief Construct a range iterator with a given starting index and number of
    * points.
@@ -127,15 +132,25 @@ public:
   /**
    * @brief Returns the index within this iterator at the i-th location.
    *
-   * @param i Location within the iterator. Ignored in the current
-   * implementation since iterator only holds a single index when not using SIMD
-   * or <= SIMD size number of indices when using SIMD.
    * @return index_type Index for the given iterator at the i-th location.
    */
   KOKKOS_INLINE_FUNCTION
-  index_type operator()(const int i) const {
-    return operator()(i, std::integral_constant<bool, using_simd>());
+  index_type operator()() const {
+#ifndef NDEBUG
+    if (end_iterator) {
+      KOKKOS_ABORT("Range iterator is at the end, cannot access index.");
+    }
+#endif
+    return operator()(std::integral_constant<bool, using_simd>());
   }
+
+  /**
+   * @brief Check if there is any work to be done by this iterator.
+   *
+   * @return bool True if there are no points to iterate over, false otherwise.
+   */
+  KOKKOS_INLINE_FUNCTION
+  bool is_end() const { return end_iterator; }
 };
 } // namespace iterator
 
@@ -166,6 +181,10 @@ public:
                                             ///< type
 
   using iterator_type = specfem::iterator::range<simd>; ///< Iterator type
+  constexpr static std::size_t chunk_size =
+      ParallelConfig::chunk_size; ///< Chunk size for the range policy
+  constexpr static std::size_t tile_size =
+      ParallelConfig::tile_size; ///< Tile size for the range policy
   ///@}
 
   /**
@@ -185,9 +204,6 @@ public:
       false; ///< Indicates that this is not a element face policy
   constexpr static bool isElementPolicy =
       false; ///< Indicates that this is not an element policy
-  constexpr static int chunk_size =
-      1; ///< Number of chunks a range can be divided into. Indices within a
-         ///< chunk are processed serially in a single thread.
   ///@}
 
 private:
@@ -216,7 +232,15 @@ public:
    * @param range_size Size of the range.
    */
   KOKKOS_FUNCTION range(const int range_size)
-      : policy_type(0, range_size / simd_size + (range_size % simd_size != 0)),
+      : simd_range_size(range_size / simd_size +
+                        ((range_size % simd_size) != 0)),
+        policy_type(
+            0,
+            ((range_size / simd_size + ((range_size % simd_size) != 0)) /
+                 tile_size +
+             (((range_size / simd_size + ((range_size % simd_size) != 0)) %
+               tile_size) != 0)),
+            Kokkos::ChunkSize(chunk_size)),
         range_size(range_size) {}
   ///@}
 
@@ -231,10 +255,27 @@ public:
    * @brief Get the range iterator for a given range index.
    *
    * @param range_index starting index within the range.
+   * @param tile_index Index of the tile within the range.
    * @return iterator_type Range iterator.
    */
-  KOKKOS_FUNCTION iterator_type range_iterator(const int range_index) const {
-    const int starting_index = range_index * simd_size;
+  KOKKOS_FUNCTION iterator_type range_iterator(
+      const std::size_t range_index, const std::size_t tile_index) const {
+#ifdef KOKKOS_ENABLE_CUDA
+    const int starting_index =
+        (tile_index * (simd_range_size / tile_size +
+                       (simd_range_size % tile_size != 0)) +
+         range_index) *
+        simd_size;
+#else
+    const int starting_index =
+        (range_index * tile_size + tile_index) * simd_size;
+#endif
+    // // Ensure that the starting index does not exceed the range size
+    if (starting_index > range_size) {
+      return iterator_type(true); // Return an empty iterator if the starting
+                                  // index is out of bounds
+    }
+
     const int number_elements = (starting_index + simd_size < range_size)
                                     ? simd_size
                                     : range_size - starting_index;
@@ -242,7 +283,8 @@ public:
   }
 
 private:
-  int range_size; ///< Size of the range.
+  int range_size;      ///< Size of the range.
+  int simd_range_size; ///< Size of the range when using SIMD
 };
 } // namespace policy
 } // namespace specfem
