e4e2a9383ad4ce49ef58a1d698a7abae70ad0d9a
ccui <ccui@princeton.edu>
2025-05-13 17:24:21 -0400

    3D partial_derivative.

--- include/point/partial_derivatives.hpp
+++ include/point/partial_derivatives.hpp
@@ -388,5 +388,142 @@ private:
   };
 };
 
+/**
+ * @brief Template specialization for 3D spectral elements with storing the
+ * Jacobian
+ *
+ * @tparam UseSIMD Boolean indicating whether to use SIMD
+ */
+template <bool UseSIMD>
+struct partial_derivatives<specfem::dimension::type::dim3, true, UseSIMD>
+    : public partial_derivatives<specfem::dimension::type::dim3, false,
+                                 UseSIMD> {
+
+  /**
+   * @name Typedefs
+   *
+   */
+  ///@{
+  using simd = specfem::datatype::simd<type_real, UseSIMD>; ///< SIMD data type
+  using value_type = typename simd::datatype; ///< Datatype for storing values.
+                                              ///< Is a scalar if UseSIMD is
+                                              ///< false, otherwise is a SIMD
+                                              ///< vector.
+  constexpr static bool store_jacobian = true;
+  constexpr static auto dimension = specfem::dimension::type::dim3;
+  ///@}
+
+  value_type jacobian; ///< Jacobian
+
+  /**
+   * @name Constructors
+   *
+   */
+  ///@{
+
+  /**
+   * @brief Default constructor
+   *
+   */
+  KOKKOS_FUNCTION
+  partial_derivatives() = default;
+
+  /**
+   * @brief Constructor with values
+   *
+   * @param xix @xix
+   * @param gammax @gammax
+   * @param xiy @xiy
+   * @param gammay @gammay
+   * @param xiz @xiz
+   * @param gammaz @gammaz
+   * @param jacobian Jacobian
+   */
+  KOKKOS_FUNCTION
+  partial_derivatives(const value_type &xix, const value_type &gammax,
+                      const value_type &xiy, const value_type &gammay,
+                      const value_type &xiz, const value_type &gammaz,
+                      const value_type &jacobian)
+      : partial_derivatives<specfem::dimension::type::dim3, false, UseSIMD>(
+            xix, gammax, xiy, gammay, xiz, gammaz),
+        jacobian(jacobian) {}
+
+  /**
+   * @brief Constructor with constant value
+   *
+   * @param constant Value to initialize all members to
+   */
+  KOKKOS_FUNCTION
+  partial_derivatives(const value_type constant)
+      : partial_derivatives<specfem::dimension::type::dim3, false, UseSIMD>(
+            constant),
+        jacobian(constant) {}
+  ///@}
+
+  KOKKOS_FUNCTION
+  void init() {
+    this->xix = 0.0;
+    this->gammax = 0.0;
+    this->xiy = 0.0;
+    this->gammay = 0.0;
+    this->xiz = 0.0;
+    this->gammaz = 0.0;
+    this->jacobian = 0.0;
+    return;
+  }
+
+  /**
+   * @name Member functions
+   *
+   */
+  ///@{
+
+  //   /**
+  //    * @brief Compute the normal vector at a quadrature point
+  //    *
+  //    * @param type Type of edge (bottom, top, left, right)
+  //    * @return specfem::datatype::ScalarPointViewType<type_real, 2, UseSIMD>
+  //    * Normal vector
+  //    */
+  //   KOKKOS_FUNCTION specfem::datatype::ScalarPointViewType<type_real, 2,
+  //   UseSIMD> compute_normal(const specfem::enums::edge::type &type) const;
+  //   ///@}
+
+  // private:
+  //   KOKKOS_INLINE_FUNCTION
+  //   specfem::datatype::ScalarPointViewType<type_real, 2, UseSIMD>
+  //   impl_compute_normal_bottom() const {
+  //     return { static_cast<value_type>(static_cast<type_real>(-1.0) *
+  //                                      this->gammax * this->jacobian),
+  //              static_cast<value_type>(static_cast<type_real>(-1.0) *
+  //                                      this->gammaz * this->jacobian) };
+  //   };
+
+  //   KOKKOS_INLINE_FUNCTION
+  //   specfem::datatype::ScalarPointViewType<type_real, 2, UseSIMD>
+  //   impl_compute_normal_top() const {
+  //     return { static_cast<value_type>(this->gammax * this->jacobian),
+  //              static_cast<value_type>(this->gammaz * this->jacobian) };
+  //   };
+
+  //   KOKKOS_INLINE_FUNCTION
+  //   specfem::datatype::ScalarPointViewType<type_real, 2, UseSIMD>
+  //   impl_compute_normal_left() const {
+  //     return { static_cast<value_type>(static_cast<type_real>(-1.0) *
+  //     this->xix *
+  //                                      this->jacobian),
+  //              static_cast<value_type>(static_cast<type_real>(-1.0) *
+  //              this->xiz *
+  //                                      this->jacobian) };
+  //   };
+
+  //   KOKKOS_INLINE_FUNCTION
+  //   specfem::datatype::ScalarPointViewType<type_real, 2, UseSIMD>
+  //   impl_compute_normal_right() const {
+  //     return { static_cast<value_type>(this->xix * this->jacobian),
+  //              static_cast<value_type>(this->xiz * this->jacobian) };
+  //   };
+};
+
 } // namespace point
 } // namespace specfem
