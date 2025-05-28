c1381b06a4751c8f411fea4ad01b456c14238d11
Lucas Sawade <lsawade@princeton.edu>
2025-05-28 11:13:23 -0400

    Updated the stress and theinverse computation of the stress

--- include/point/stress.hpp
+++ include/point/stress.hpp
@@ -70,7 +70,20 @@ struct stress {
   /**
    * @brief Compute the product of the stress tensor with spatial derivatives
    *
-   * /f$ F_{ij} = \sum_{k=1}^{N} T_{ik} \partial_k xi_j /f$
+   * Following Komatitsch and Tromp (1999), the product of the stress tensor
+   * with the spatial derivatives is computed as follows:
+   *
+   * /f{equation}{ F_{ik} = \sum_{k=1}^{N} T_{ij} \partial_j xi_k /f}
+   *
+   * In detail, the stress integrand array is intuitively defined as
+   *
+   * \f{equation}{ F_{ik} = F_{x_i, \xi_k} = F(i, k),/f}
+   *
+   * where \f$x_i = [x,z]\f$, and \f$ \xi_k = [\xi, \gamma] \f$.
+   *
+   * The stress integrand is the populated as follows:
+   *
+   * \f{equation}{ F(i, k) = T(i, j) \partial xi_k / \partial \x_j \f}
    *
    * @param partial_derivatives Spatial derivatives
    * @return ViewType Result of the product
@@ -82,8 +95,18 @@ struct stress {
     ViewType F;
 
     for (int icomponent = 0; icomponent < components; ++icomponent) {
+      // F(i, k) = T(i, 0) * \partial \xi_k / \partial x
+      //         + T(i, 1) * \partial \xi_k / \partial z
+
+      // F(0, 0) = F_{x,\xi} = sigma_xx * xix + sigma_xz * xiz
+      // F(1, 0) = F_{z,\xi} = sigma_zx * xix + sigma_zz * xiz
+      // ...
       F(icomponent, 0) = T(icomponent, 0) * partial_derivatives.xix +
                          T(icomponent, 1) * partial_derivatives.xiz;
+
+      // F(0, 1) = F_{z,\gamma} = sigma_xz * gammax + sigma_zz * gammaz
+      // F(1, 1) = F_{x,\gamma} = sigma_xx * gammax + sigma_xz * gammaz
+      // ...
       F(icomponent, 1) = T(icomponent, 0) * partial_derivatives.gammax +
                          T(icomponent, 1) * partial_derivatives.gammaz;
     }
