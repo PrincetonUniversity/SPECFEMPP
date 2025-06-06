690a2309c10df740947e60c3bc61b909baf80b1c
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2025-06-06 15:05:18 -0400

    Documentation update
    - [x] Added documentation for execution
    - [x] Removed old documentation

---
+++ docs/sections/api/execution/iterators/void_iterator.rst
@@ -0,0 +1,7 @@
+.. _execution_iterators_void_iterator:
+
+Void Iterator
+================
+
+.. doxygenclass:: specfem::execution::VoidIterator
+    :members:
--- include/execution/chunked_domain_iterator.hpp
+++ include/execution/chunked_domain_iterator.hpp
@@ -20,34 +20,80 @@ class Index;
 namespace specfem {
 namespace execution {
 
+/**
+ * @brief PointIndex class is used to represent a quadrature point index within
+ * a domain.
+ *
+ * @tparam DimensionTag The dimension tag (e.g., dim2, dim3).
+ * @tparam KokkosIndexType The type of the Kokkos index, must be convertible to
+ * an integral type.
+ * @tparam UseSIMD Indicates whether SIMD is used for the index.
+ */
 template <specfem::dimension::type DimensionTag, typename KokkosIndexType,
           bool UseSIMD>
 class PointIndex {
 public:
-  using iterator_type = VoidIterator;
+  using iterator_type = VoidIterator; ///< Iterator type used to iterate over
+                                      ///< GLL points within this index.
+                                      ///< @c VoidIterator is used when the
+                                      ///< index refers to a single GLL point.
 
+  /**
+   * @brief Get the policy index that defined this point index.
+   *
+   * @return const KokkosIndexType The policy index that defined this point
+   * index.
+   */
   KOKKOS_INLINE_FUNCTION
   constexpr const KokkosIndexType get_policy_index() const {
     return this->kokkos_index;
   }
 
+  /**
+   * @brief Get the underlying index used to define the GLL point.
+   *
+   * @return const specfem::point::index<DimensionTag, UseSIMD> The point index
+   * that defines the GLL point.
+   */
   KOKKOS_INLINE_FUNCTION
   constexpr const specfem::point::index<DimensionTag, UseSIMD>
   get_index() const {
     return this->index; ///< Returns the point index
   }
 
+  /**
+   * @brief Get the iterator for this index.
+   *
+   * @return const iterator_type The iterator for this index.
+   */
   KOKKOS_INLINE_FUNCTION
   constexpr const iterator_type get_iterator() const {
     return VoidIterator{}; ///< Returns a VoidIterator
   }
 
+  /**
+   * @brief Constructor for PointIndex when SIMD is used.
+   *
+   * @param ispec The index of the spectral element.
+   * @param number_elements The number of elements in the chunk.
+   * @param iz The z-coordinate of the GLL point.
+   * @param ix The x-coordinate of the GLL point.
+   * @param kokkos_index The Kokkos index type.
+   */
   template <bool U = UseSIMD, typename std::enable_if<U, int>::type = 0>
   KOKKOS_INLINE_FUNCTION
   PointIndex(const int &ispec, const int &number_elements, const int &iz,
              const int &ix, const KokkosIndexType &kokkos_index)
       : index(ispec, number_elements, iz, ix), kokkos_index(kokkos_index) {}
 
+  /**
+   * @brief Constructor for PointIndex when SIMD is not used.
+   *
+   * @param ispec The index of the spectral element.
+   * @param iz The z-coordinate of the GLL point.
+   * @param ix The x-coordinate of the GLL point.
+   * @param kokkos_index The Kokkos index type.
+   */
   template <bool U = UseSIMD, typename std::enable_if<!U, int>::type = 0>
   KOKKOS_INLINE_FUNCTION PointIndex(const int &ispec, const int &iz,
                                     const int &ix,
@@ -60,10 +106,21 @@ public:
   }
 
 private:
-  specfem::point::index<DimensionTag, UseSIMD> index; ///< Point
+  specfem::point::index<DimensionTag, UseSIMD> index; ///< Index of the GLL
+                                                      ///< point
   KokkosIndexType kokkos_index;                       ///< Kokkos index type
 };
 
+/**
+ * @brief ChunkElementIterator class is used to iterate over all GLL points
+ * within the given chunk of elements.
+ *
+ * @tparam DimensionTag The dimension tag (e.g., dim2, dim3).
+ * @tparam SIMD The SIMD type used for vectorization.
+ * @tparam ViewType The type of the view containing indices of elements.
+ * @tparam TeamMemberType The type of the Kokkos team member.
+ *
+ */
 template <specfem::dimension::type DimensionTag, typename SIMD,
           typename ViewType, typename TeamMemberType>
 class ChunkElementIterator
@@ -76,10 +133,25 @@ private:
   constexpr static auto using_simd = SIMD::using_simd;
 
 public:
-  using base_policy_type = typename base_type::base_policy_type;
-  using policy_index_type = typename base_type::policy_index_type;
-  using index_type = PointIndex<DimensionTag, policy_index_type, using_simd>;
+  using base_policy_type =
+      typename base_type::base_policy_type; ///< Base policy type. Evaluates to
+                                            ///< @c Kokkos::TeamThreadRange
+  using policy_index_type =
+      typename base_type::policy_index_type; /// Policy index type. Must be
+                                             ///< convertible to integral type.
+  using index_type =
+      PointIndex<DimensionTag, policy_index_type,
+                 using_simd>; ///< Underlying index type. This index
+                              ///< will be passed to the closure when
+                              ///< calling @ref
+                              ///< specfem::execution::for_each_level
 
+  /**
+   * @brief Operator to get the index for a given policy index.
+   *
+   * @param i The policy index to convert to an index.
+   * @return const index_type The index corresponding to the policy index.
+   */
   template <bool U = using_simd>
   KOKKOS_INLINE_FUNCTION std::enable_if_t<U, const index_type>
   operator()(const policy_index_type &i) const {
@@ -123,6 +195,14 @@ public:
     return index_type(ispec, iz, ix, ielement);
   }
 
+  /**
+   * @brief Constructor for ChunkElementIterator.
+   *
+   * @param team The Kokkos team member type.
+   * @param indices View of indices of elements within this chunk.
+   * @param ngllz Number of GLL points in the z-direction.
+   * @param ngllx Number of GLL points in the x-direction.
+   */
   KOKKOS_INLINE_FUNCTION ChunkElementIterator(const TeamMemberType &team,
                                               const ViewType indices, int ngllz,
                                               int ngllx)
@@ -134,34 +214,79 @@ public:
                          ngllz * ngllx)) {}
 
 private:
-  ViewType indices;
-  int num_elements;
-  int ngllz;
-  int ngllx;
+  ViewType indices; ///< View of indices of elements within this chunk
+  int num_elements; ///< Number of elements in the chunk, adjusted for SIMD
+  int ngllz;        ///< Number of GLL points in the z-direction
+  int ngllx;        ///< Number of GLL points in the x-direction
 };
 
+/**
+ * @brief ChunkElementIndex class is used to represent an index for a chunk of
+ * elements in a finite element mesh.
+ *
+ * This class provides methods to access the Kokkos index, the underlying
+ * chunked element index, and an iterator for iterating over the elements in
+ * the chunk.
+ *
+ * @tparam DimensionTag The dimension tag (e.g., dim2, dim3).
+ * @tparam SIMD The SIMD type used for vectorization.
+ * @tparam ViewType The type of the view containing indices of elements.
+ * @tparam TeamMemberType The type of the Kokkos team member.
+ */
 template <specfem::dimension::type DimensionTag, typename SIMD,
           typename ViewType, typename TeamMemberType>
 class ChunkElementIndex {
 private:
-  using index_type = specfem::chunk_element::Index<DimensionTag, SIMD, ViewType,
-                                                   TeamMemberType>;
+  using index_type =
+      specfem::chunk_element::Index<DimensionTag, SIMD, ViewType,
+                                    TeamMemberType>; ///< Underlying index type
+                                                     ///< used to store the
+                                                     ///< indices within the
+                                                     ///< chunk.
 
 public:
   using iterator_type =
-      ChunkElementIterator<DimensionTag, SIMD, ViewType, TeamMemberType>;
+      ChunkElementIterator<DimensionTag, SIMD, ViewType,
+                           TeamMemberType>; ///< Iterator type for iterating
+                                            ///< over the elements in the chunk.
 
+  /**
+   * @brief Get the policy index that defined this chunk element index.
+   *
+   * @return const TeamMemberType The policy index that defined this chunk
+   * element index.
+   */
   KOKKOS_INLINE_FUNCTION
   constexpr const TeamMemberType get_policy_index() const {
-    return this->kokkos_index; ///< Returns the Kokkos index
+    return this->kokkos_index;
   }
 
+  /**
+   * @brief Get the underlying index used to store elements in the chunk.
+   *
+   * @return const index_type The underlying index that defines the chunk
+   * elements.
+   */
   KOKKOS_INLINE_FUNCTION
   constexpr const index_type get_index() const { return { *this }; }
 
+  /**
+   * @brief Get the iterator used to iterate over the elements in the chunk.
+   *
+   * @return const iterator_type The iterator for this chunk element index.
+   */
   KOKKOS_INLINE_FUNCTION
   constexpr const iterator_type get_iterator() const { return this->iterator; }
 
+  /**
+   * @brief Constructor for ChunkElementIndex.
+   *
+   *
+   * @param indices View of indices of elements within this chunk.
+   * @param ngllz Number of GLL points in the z-direction.
+   * @param ngllx Number of GLL points in the x-direction.
+   * @param kokkos_index Kokkos index type.
+   */
   KOKKOS_INLINE_FUNCTION
   ChunkElementIndex(const ViewType indices, const int &ngllz, const int &ngllx,
                     const TeamMemberType &kokkos_index)
@@ -169,6 +294,13 @@ public:
         kokkos_index(kokkos_index),
         iterator(kokkos_index, indices, ngllz, ngllx) {}
 
+  /**
+   * @brief Get a pair representing the range of indices in this chunk.
+   *
+   * @return Kokkos::pair<typename ViewType::value_type,
+   * typename ViewType::value_type> A pair containing the first and last index
+   * in the chunk.
+   */
   KOKKOS_INLINE_FUNCTION
   Kokkos::pair<typename ViewType::value_type, typename ViewType::value_type>
   get_range() const {
@@ -182,35 +314,78 @@ private:
   int ngllz;                   ///< Number of GLL points in the z-direction
   int ngllx;                   ///< Number of GLL points in the x-direction
   TeamMemberType kokkos_index; ///< Kokkos index type
-  iterator_type iterator;
+  iterator_type iterator;      ///< Iterator for iterating over the elements
+                               ///< in the chunk.
 };
 
+/**
+ * @brief ChunkedDomainIterator class is used to iterate over all quadrature
+ * points within a given set of finite elements.
+ *
+ * The iterator divides the elements into chunks of size
+ * `ParallelConfig::chunk_size` and iterates over them in parallel.
+ *
+ * @tparam ParallelConfig Configuration for parallel execution. @ref
+ * specfem::parallel_configuration::chunk_config
+ * @tparam ViewType Type of the view containing indices of elements.
+ */
 template <typename ParallelConfig, typename ViewType>
 class ChunkedDomainIterator : public TeamPolicy<ParallelConfig> {
 private:
-  using base_type = TeamPolicy<ParallelConfig>;
-  constexpr static auto simd_size = ParallelConfig::simd::size();
-  constexpr static auto chunk_size = ParallelConfig::chunk_size;
+  using base_type = TeamPolicy<ParallelConfig>; ///< Base policy type
+  constexpr static auto simd_size = ParallelConfig::simd::size(); ///< SIMD size
+  constexpr static auto chunk_size = ParallelConfig::chunk_size; ///< Chunk size
 
 public:
-  using base_policy_type = typename base_type::base_policy_type;
-  using policy_index_type = typename base_type::policy_index_type;
+  using base_policy_type =
+      typename base_type::base_policy_type; ///< Base policy type. Evaluates to
+                                            ///< @c Kokkos::TeamPolicy`
+  using policy_index_type = typename base_type::
+      policy_index_type; ///< Policy index type. Evaluates to
+                         ///< @c Kokkos::TeamPolicy::member_type
   using index_type = ChunkElementIndex<
       ParallelConfig::dimension, typename ParallelConfig::simd,
       decltype(Kokkos::subview(std::declval<ViewType>(),
                                std::declval<Kokkos::pair<int, int> >())),
-      policy_index_type>;
+      policy_index_type>; ///< Underlying index type. This index
+                          ///< will be passed to the closure when
+                          ///< calling @ref
+                          ///< specfem::execution::for_each_level
 
+  /**
+   * @brief Construct a new Chunked Domain Iterator object
+   *
+   * @param indices View of indices of elements within this iterator.
+   * @param ngllz Number of GLL points in the z-direction.
+   * @param ngllx Number of GLL points in the x-direction.
+   */
   ChunkedDomainIterator(const ViewType indices, int ngllz, int ngllx)
       : indices(indices), ngllz(ngllz), ngllx(ngllx),
         base_type(((indices.extent(0) / (chunk_size * simd_size)) +
                    ((indices.extent(0) % (chunk_size * simd_size)) != 0)),
                   Kokkos::AUTO, Kokkos::AUTO) {}
 
+  /**
+   * @brief Construct a new Chunked Domain Iterator object for a given parallel
+   * configuration
+   *
+   * @param ParallelConfig Configuration for parallel execution.
+   * @param indices View of indices of elements within this iterator.
+   * @param ngllz Number of GLL points in the z-direction.
+   * @param ngllx Number of GLL points in the x-direction.
+   */
   ChunkedDomainIterator(const ParallelConfig, const ViewType indices, int ngllz,
                         int ngllx)
       : ChunkedDomainIterator(indices, ngllz, ngllx) {}
 
+  /**
+   * @brief Compute the index for a given policy index.
+   *
+   * The operator assigns a range of elements to each team.
+   *
+   * @param team The policy index for which to compute the chunked domain index.
+   * @return index_type The computed index type.
+   */
   KOKKOS_INLINE_FUNCTION
   const index_type operator()(const policy_index_type &team) const {
     const int league_id = team.league_rank();
@@ -223,6 +398,18 @@ public:
     return index_type(my_indices, ngllz, ngllx, team);
   }
 
+  /**
+   * @brief Set scratch size for the iterator.
+   *
+   * This method sets passes its arguments to Kokkos::TeamPolicy's
+   * set_scratch_size method, allowing for the configuration of scratch space
+   * for the iterator.
+   *
+   * @tparam Args Types of arguments to be forwarded to the base policy.
+   * @param args Arguments to be forwarded to the base policy's
+   * set_scratch_size method.
+   * @return ChunkedDomainIterator& Returns a reference to itself.
+   */
   template <typename... Args>
   inline ChunkedDomainIterator &set_scratch_size(Args &&...args) {
     base_policy_type::set_scratch_size(std::forward<Args>(args)...);
