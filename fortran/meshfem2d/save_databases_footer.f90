3a2be89612bd56856f95158eb8b2128567041010
Kentaro Hanson <kh3372@princeton.edu>
2025-06-11 17:33:19 -0400

    added meshfem parameter write_adjacency_map

--- fortran/meshfem2d/save_databases_footer.f90
+++ fortran/meshfem2d/save_databases_footer.f90
@@ -3,14 +3,17 @@ contains
 
 subroutine save_databases_additional_items()
   use constants, only: IMAIN,IOUT
+
+  use shared_parameters, only: write_adjacency_map
   use save_databases_adj, only: save_databases_adjacency_map
 
   implicit none
 
   integer, parameter :: completion_footercode = 0
 
-
+  if(write_adjacency_map) then
   call save_databases_adjacency_map()
+  endif
 
   write(IOUT) completion_footercode
 
