6dfd1ca38a049cdfb796df50bacb2d6fdf4aa400
Lucas Sawade <lsawade@princeton.edu>
2025-06-18 12:48:01 -0400

    Added custom test tolerance to the configuration

--- tests/unit-tests/displacement_tests/Newmark/newmark_tests.cpp
+++ tests/unit-tests/displacement_tests/Newmark/newmark_tests.cpp
@@ -387,10 +387,11 @@ TEST(DISPLACEMENT_TESTS, newmark_scheme_tests) {
         count++;
       }
 
-      if (error / computed_norm > 1e-3 || std::isnan(error / computed_norm)) {
+      if (error / computed_norm > Test.configuration.tolerance ||
+          std::isnan(error / computed_norm)) {
         FAIL() << "--------------------------------------------------\n"
                << "\033[0;31m[FAILED]\033[0m Test failed\n"
-               << " - Test name: " << Test.name << "\n"
+               << " - Test " << Test.number << ": " << Test.name << "\n"
                << " - Error: Norm of the error is greater than 1e-3\n"
                << " - Station: " << station_name << "\n"
                << " - Network: " << network_name << "\n"
