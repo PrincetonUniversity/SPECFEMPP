bba3187aeaa209f45a57516eda3206bf5de26b79
Lucas Sawade <lsawade@princeton.edu>
2025-07-10 14:36:45 -0400

    ADded docstrings

--- core/specfem/assembly/compute_source_array/dim2/compute_moment_tensor_source_array.hpp
+++ core/specfem/assembly/compute_source_array/dim2/compute_moment_tensor_source_array.hpp
@@ -9,6 +9,21 @@
 
 namespace specfem::assembly {
 
+/**
+ * @brief Compute the lagrange interpolants for a specific source location in
+ * an element.
+ *
+ * This is a helper function that computes the source array for a given moment
+ * tensor source. We implement it here instead of the source to remove
+ * dependency of source on the assembly module.
+ *
+ * @param source The moment tensor source for which the source array is
+ * computed.
+ * @param mesh The mesh on which the source is defined.
+ * @param jacobian_matrix The Jacobian matrix for the mesh.
+ * @param element_types The element types for the mesh.
+ * @param source_array The output source array to be filled.
+ */
 void compute_source_array(
     const std::shared_ptr<specfem::sources::moment_tensor> &source,
     const specfem::assembly::mesh<specfem::dimension::type::dim2> &mesh,
