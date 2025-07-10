d82d44d4752ce9c742bd0a9e4810ad209ba3899b
Lucas Sawade <lsawade@princeton.edu>
2025-07-10 14:36:45 -0400

    ADded docstrings

--- core/specfem/assembly/compute_source_array/dim2/compute_force_source_array.hpp
+++ core/specfem/assembly/compute_source_array/dim2/compute_force_source_array.hpp
@@ -9,6 +9,20 @@
 
 namespace specfem::assembly {
 
+/**
+ * @brief Compute the lagrange interpolants for a specific source location in
+ * an element.
+ *
+ * This is a helper function that computes the source array for a given force
+ * source. We implement it here instead of the source to remove dependency of
+ * source on the assembly module.
+ *
+ * @param source The force source for which the source array is computed.
+ * @param mesh The mesh on which the source is defined.
+ * @param jacobian_matrix The Jacobian matrix for the mesh.
+ * @param element_types The element types for the mesh.
+ * @param source_array The output source array to be filled.
+ */
 void compute_source_array(
     const std::shared_ptr<specfem::sources::force> &source,
     const specfem::assembly::mesh<specfem::dimension::type::dim2> &mesh,
