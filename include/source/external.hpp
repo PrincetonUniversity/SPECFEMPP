cbe1b0fe34b50faf952c89d9faae8c02c785288f
Lucas Sawade <lsawade@princeton.edu>
2025-07-17 14:26:59 +0200

    Updated the external source vector computation

--- include/source/external.hpp
+++ include/source/external.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "enumerations/interface.hpp"
+#include "kokkos_abstractions.h"
 #include "source.hpp"
 #include "yaml-cpp/yaml.h"
 
@@ -9,6 +11,12 @@ class external : public source {
 
 public:
   external() {};
+
+  external(type_real x, type_real z,
+           std::unique_ptr<specfem::forcing_function::stf> forcing_function,
+           const specfem::wavefield::simulation_field wavefield_type)
+      : source(x, z, std::move(forcing_function)),
+        wavefield_type(wavefield_type) {};
 
   external(YAML::Node &Node, const int nsteps, const type_real dt,
            const specfem::wavefield::simulation_field wavefield_type)
@@ -29,6 +37,14 @@ public:
   source_type get_source_type() const override {
     return source_type::external_source;
   }
+
+  /**
+   * @brief Get the force vector
+   *
+   * @return Kokkos::View<type_real *, Kokkos::LayoutLeft, Kokkos::HostSpace>
+   * Force vector
+   */
+  specfem::kokkos::HostView1d<type_real> get_force_vector() const;
 
 private:
   specfem::wavefield::simulation_field wavefield_type;
--- src/source/external.cpp
+++ src/source/external.cpp
@@ -1,8 +1,62 @@
-#include "enumerations/specfem_enums.hpp"
+#include "enumerations/interface.hpp"
 #include "source/interface.hpp"
 #include "source_time_function/interface.hpp"
 #include "specfem_setup.hpp"
 #include <cmath>
+
+specfem::kokkos::HostView1d<type_real>
+specfem::sources::external::get_force_vector() const {
+
+  // Get the medium tag that the source is located in
+  specfem::element::medium_tag medium_tag = this->get_medium_tag();
+
+  // Declare the force vector
+  specfem::kokkos::HostView1d<type_real> force_vector;
+
+  // Acoustic
+  if (medium_tag == specfem::element::medium_tag::acoustic) {
+    force_vector = specfem::kokkos::HostView1d<type_real>("force_vector", 1);
+    force_vector(0) = 1.0;
+  }
+  // Elastic SH
+  else if (medium_tag == specfem::element::medium_tag::elastic_sh) {
+    force_vector = specfem::kokkos::HostView1d<type_real>("force_vector", 1);
+    force_vector(0) = 1.0;
+  }
+  // Elastic P-SV
+  else if (medium_tag == specfem::element::medium_tag::elastic_psv) {
+    force_vector = specfem::kokkos::HostView1d<type_real>("force_vector", 2);
+    force_vector(0) = 1.0;
+    force_vector(1) = 1.0;
+  }
+  // Poroelastic
+  else if (medium_tag == specfem::element::medium_tag::poroelastic) {
+    force_vector = specfem::kokkos::HostView1d<type_real>("force_vector", 4);
+    force_vector(0) = 1.0;
+    force_vector(1) = 1.0;
+    force_vector(2) = 1.0;
+    force_vector(3) = 1.0;
+  }
+  // Electromagnetic TE
+  else if (medium_tag == specfem::element::medium_tag::electromagnetic_te) {
+    force_vector = specfem::kokkos::HostView1d<type_real>("force_vector", 2);
+    force_vector(0) = 1.0;
+    force_vector(1) = 1.0;
+  }
+  // Elastic P-SV-T (note: external source uses 1.0 for all components, unlike
+  // adjoint)
+  else if (medium_tag == specfem::element::medium_tag::elastic_psv_t) {
+    force_vector = specfem::kokkos::HostView1d<type_real>("force_vector", 3);
+    force_vector(0) = 1.0;
+    force_vector(1) = 1.0;
+    force_vector(2) = 1.0;
+  } else {
+    KOKKOS_ABORT_WITH_LOCATION("External source array computation not "
+                               "implemented for requested element type.");
+  }
+
+  return force_vector;
+}
 
 std::string specfem::sources::external::print() const {
 
