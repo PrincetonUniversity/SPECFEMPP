dbd09cf3fa8217c09ba8b05c2a0e84f534d31d3a
Lucas Sawade <lsawade@princeton.edu>
2025-07-21 11:40:51 +0200

    Updated the vector and tensor source computation and added simplified tests with 1,1,1,1 jacobians

--- core/specfem/assembly/compute_source_array/impl/compute_source_array_from.hpp
+++ core/specfem/assembly/compute_source_array/impl/compute_source_array_from.hpp
@@ -4,6 +4,7 @@
 #include "kokkos_abstractions.h"
 #include "specfem/assembly/jacobian_matrix.hpp"
 #include "specfem/assembly/mesh.hpp"
+#include "specfem/point.hpp"
 #include "specfem/source.hpp"
 
 namespace specfem::assembly::compute_source_array_impl {
@@ -29,6 +30,14 @@ void from_tensor(
     const specfem::sources::tensor_source &source,
     const specfem::assembly::mesh<DimensionTag> &mesh,
     const specfem::assembly::jacobian_matrix<DimensionTag> &jacobian_matrix,
+    specfem::kokkos::HostView3d<type_real> source_array);
+
+// Testable helper function for tensor sources with pre-computed jacobian matrix
+void compute_source_array_from_tensor_and_element_jacobian(
+    const specfem::sources::tensor_source &tensor_source,
+    const specfem::kokkos::HostView2d<specfem::point::jacobian_matrix<
+        specfem::dimension::type::dim2, false, false> >
+        &element_jacobian_matrix,
     specfem::kokkos::HostView3d<type_real> source_array);
 
 } // namespace specfem::assembly::compute_source_array_impl
