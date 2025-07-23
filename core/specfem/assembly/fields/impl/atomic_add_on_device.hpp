8742f9d80d44e786c6f81a585c0a7f94b2d10a2f
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2025-07-23 15:23:08 -0400

    Update mass matrix computation routines
    - [x] Update mass matrix routines with new access functions
    - [x] Added atomic add on device functions
    - [x] Update stacey routine
    - [x] Fixed load on device accessors which were using wrong base function

--- core/specfem/assembly/fields/impl/atomic_add_on_device.hpp
+++ core/specfem/assembly/fields/impl/atomic_add_on_device.hpp
@@ -1,15 +1,17 @@
 #pragma once
 
 #include "check_accessor_compatibility.hpp"
+#include "enumerations/macros.hpp"
 #include "field_impl.hpp"
 #include <Kokkos_Core.hpp>
 #include <type_traits>
 
 namespace specfem::assembly::fields_impl {
+
 template <typename ContainerType, typename AccessorType>
 KOKKOS_FORCEINLINE_FUNCTION void
-base_load_accessor(const int iglob, const int icomp, const ContainerType &field,
-                   AccessorType &accessor) {
+base_atomic_add_accessor(const int iglob, const int icomp,
+                         const ContainerType &field, AccessorType &accessor) {
 
   static_assert(
       !AccessorType::simd::using_simd,
@@ -22,35 +24,25 @@ base_load_accessor(const int iglob, const int icomp, const ContainerType &field,
   using data_accessor =
       std::integral_constant<specfem::data_access::DataClassType,
                              AccessorType::data_class>;
-  accessor(icomp) = field.get_value(data_accessor(), iglob, icomp);
+  Kokkos::atomic_add(&(field.get_value(data_accessor(), iglob, icomp)),
+                     accessor(icomp));
 }
 
 template <typename MaskType, typename ContainerType, typename AccessorType>
 KOKKOS_FORCEINLINE_FUNCTION void
-base_load_accessor(const int iglob, const int icomp, const MaskType &mask,
-                   const ContainerType &field, AccessorType &accessor) {
+base_atomic_add_accessor(const int iglob, const int icomp, const MaskType &mask,
+                         const ContainerType &field, AccessorType &accessor) {
 
-  static_assert(
-      AccessorType::simd::using_simd,
-      "This function is only for SIMD accessors. Use the other overload.");
-
-  constexpr static int ncomponents =
-      specfem::element::attributes<AccessorType::dimension_tag,
-                                   AccessorType::medium_tag>::components;
-
-  using data_accessor =
-      std::integral_constant<specfem::data_access::DataClassType,
-                             AccessorType::data_class>;
-
-  Kokkos::Experimental::where(mask, accessor(icomp))
-      .copy_from(&(field.get_value(data_accessor(), iglob, icomp)),
-                 typename AccessorType::simd::tag_type());
+  KOKKOS_ABORT_WITH_LOCATION(
+      "This function is not implemented for SIMD accessors. Use the other "
+      "overload.");
 }
 
 template <typename MaskType, typename ContainerType, typename AccessorType>
 KOKKOS_FORCEINLINE_FUNCTION void
-base_load_accessor(const int *iglob, const int icomp, const MaskType &mask,
-                   const ContainerType &field, AccessorType &accessor) {
+base_atomic_add_accessor(const int *iglob, const int icomp,
+                         const MaskType &mask, const ContainerType &field,
+                         AccessorType &accessor) {
 
   static_assert(
       AccessorType::simd::using_simd,
@@ -65,11 +57,13 @@ base_load_accessor(const int *iglob, const int icomp, const MaskType &mask,
       std::integral_constant<specfem::data_access::DataClassType,
                              AccessorType::data_class>;
 
-  simd_type result([&](std::size_t lane) {
-    return mask(lane) ? field.get_value(data_accessor(), iglob[lane], icomp)
-                      : 0;
-  });
-  accessor(icomp) = result;
+  for (int lane = 0; lane < AccessorType::simd::size(); ++lane) {
+    if (mask(lane)) {
+      Kokkos::atomic_add(
+          &(field.get_value(data_accessor(), iglob[lane], icomp)),
+          accessor(icomp)[lane]);
+    }
+  }
   return;
 }
 
@@ -77,8 +71,8 @@ template <typename ContainerType, typename... AccessorTypes,
           typename std::enable_if_t<
               (specfem::data_access::is_field_l<AccessorTypes>::value && ...),
               int> = 0>
-KOKKOS_FORCEINLINE_FUNCTION void load_after_simd_dispatch(
-    const std::false_type, const specfem::point::assembly_index<false> &index,
+KOKKOS_FORCEINLINE_FUNCTION void atomic_add_after_simd_dispatch(
+    const std::false_type, const specfem::point::assembly_index<false> index,
     const ContainerType &field, AccessorTypes &...accessors) {
 
   check_accessor_compatibility<AccessorTypes...>();
@@ -91,9 +85,8 @@ KOKKOS_FORCEINLINE_FUNCTION void load_after_simd_dispatch(
       components;
 
   // Call load for each accessor
-
   for (int icomp = 0; icomp < ncomponents; ++icomp) {
-    (base_load_accessor(iglob, icomp, field, accessors), ...);
+    (base_atomic_add_accessor(iglob, icomp, field, accessors), ...);
   }
   return;
 }
@@ -102,8 +95,8 @@ template <typename ContainerType, typename... AccessorTypes,
           typename std::enable_if_t<
               (specfem::data_access::is_field_l<AccessorTypes>::value && ...),
               int> = 0>
-KOKKOS_FORCEINLINE_FUNCTION void load_after_simd_dispatch(
-    const std::true_type, const specfem::point::assembly_index<true> &index,
+KOKKOS_FORCEINLINE_FUNCTION void atomic_add_after_simd_dispatch(
+    std::true_type, const specfem::point::assembly_index<true> index,
     const ContainerType &field, AccessorTypes &...accessors) {
 
   check_accessor_compatibility<AccessorTypes...>();
@@ -121,7 +114,7 @@ KOKKOS_FORCEINLINE_FUNCTION void load_after_simd_dispatch(
 
   // Call load for each accessor
   for (int icomp = 0; icomp < ncomponents; ++icomp) {
-    (base_load_accessor(iglob, icomp, mask, field, accessors), ...);
+    (base_atomic_add_accessor(iglob, icomp, mask, field, accessors), ...);
   }
   return;
 }
@@ -131,9 +124,9 @@ template <typename IndexType, typename ContainerType, typename... AccessorTypes,
               (specfem::data_access::is_assembly_index<IndexType>::value &&
                (specfem::data_access::is_field_l<AccessorTypes>::value && ...)),
               int> = 0>
-KOKKOS_FORCEINLINE_FUNCTION void load_on_device(const IndexType &index,
-                                                const ContainerType &field,
-                                                AccessorTypes &...accessors) {
+KOKKOS_FORCEINLINE_FUNCTION void
+atomic_add_on_device(const IndexType &index, const ContainerType &field,
+                     AccessorTypes &...accessors) {
 
   check_accessor_compatibility<AccessorTypes...>();
 
@@ -141,7 +134,8 @@ KOKKOS_FORCEINLINE_FUNCTION void load_on_device(const IndexType &index,
       std::integral_constant<bool, IndexType::using_simd>;
 
   // Call load for each accessor
-  load_after_simd_dispatch(simd_accessor_type(), index, field, accessors...);
+  atomic_add_after_simd_dispatch(simd_accessor_type(), index, field,
+                                 accessors...);
   return;
 }
 
--- include/medium/dim2/elastic/isotropic/mass_matrix.tpp
+++ include/medium/dim2/elastic/isotropic/mass_matrix.tpp
@@ -3,27 +3,25 @@
 #include "mass_matrix.hpp"
 
 template <bool UseSIMD, specfem::element::property_tag PropertyTag>
-KOKKOS_FUNCTION specfem::point::field<specfem::dimension::type::dim2,
-                                      specfem::element::medium_tag::elastic_psv,
-                                      false, false, false, true, UseSIMD>
+KOKKOS_FUNCTION specfem::point::mass_inverse<
+    specfem::dimension::type::dim2, specfem::element::medium_tag::elastic_psv,
+    UseSIMD>
 specfem::medium::impl_mass_matrix_component(
     const specfem::point::properties<specfem::dimension::type::dim2,
                                      specfem::element::medium_tag::elastic_psv,
                                      PropertyTag, UseSIMD> &properties) {
 
-  return specfem::datatype::VectorPointViewType<type_real, 2, UseSIMD>(
-      properties.rho(), properties.rho());
+  return { properties.rho(), properties.rho() };
 }
 
 template <bool UseSIMD, specfem::element::property_tag PropertyTag>
-KOKKOS_FUNCTION specfem::point::field<specfem::dimension::type::dim2,
-                                      specfem::element::medium_tag::elastic_sh,
-                                      false, false, false, true, UseSIMD>
+KOKKOS_FUNCTION specfem::point::mass_inverse<
+    specfem::dimension::type::dim2, specfem::element::medium_tag::elastic_sh,
+    UseSIMD>
 specfem::medium::impl_mass_matrix_component(
     const specfem::point::properties<specfem::dimension::type::dim2,
                                      specfem::element::medium_tag::elastic_sh,
                                      PropertyTag, UseSIMD> &properties) {
 
-  return specfem::datatype::VectorPointViewType<type_real, 1, UseSIMD>(
-      properties.rho());
+  return { properties.rho() }; ///< Mass matrix for SH waves is isotropic
 }
