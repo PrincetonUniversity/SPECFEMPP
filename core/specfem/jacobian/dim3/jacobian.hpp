4d6e6d7b2785cbed5d4e0a1ca5657e250a9ea4c7
ccui <ccui@princeton.edu>
2025-07-25 14:51:59 -0400

    Update 3D Jacobian.

--- core/specfem/jacobian/dim3/jacobian.hpp
+++ core/specfem/jacobian/dim3/jacobian.hpp
@@ -21,13 +21,16 @@ namespace specfem::jacobian {
  * @param eta \f$ \eta \f$ value of point
  * @param gamma \f$ \gamma \f$ value of point
  * @param shape3D shape function matrix calculated at  \f$ (\xi, \gamma) \f$
- * @return std::tuple<type_real, type_real, type_real> (x,y,z) value for the
- * point
+ * @return specfem::point::global_coordinates<specfem::dimension::type::dim3>
+ * (x,y,z) value for the point
  */
-std::tuple<type_real, type_real, type_real>
-compute_locations(const specfem::kokkos::HostView2d<type_real> &s_coorg,
-                  const int ngnod, const type_real xi, const type_real eta,
-                  const type_real gamma);
+specfem::point::global_coordinates<specfem::dimension::type::dim3>
+compute_locations(
+    const Kokkos::View<
+        point::global_coordinates<specfem::dimension::type::dim3> *,
+        Kokkos::HostSpace> &coorg,
+    const int ngnod, const type_real xi, const type_real eta,
+    const type_real gamma);
 
 /**
  * @brief Compute Jacobian matrix at  \f$ (\xi, \gamma) \f$
@@ -35,25 +38,27 @@ compute_locations(const specfem::kokkos::HostView2d<type_real> &s_coorg,
  * @note This function can only be called within a team policy
  *
  * @param teamMember Kokkos team policy team member
- * @param s_coorg View of coorg subviewed at required element
+ * @param coorg View of coorg subviewed at required element
  * @param ngnod Total number of control nodes per element
  * @param xi \f$ \xi \f$ value of the point
  * @param eta \f$ \eta \f$ value of point
  * @param gamma \f$ \gamma \f$ value of point
  * @param dershape3D derivative of shape function matrix calculated at (xi,
  * eta, gamma)
- * @return std::tuple<type_real, type_real, type_real, type_real, type_real,
-                        type_real, type_real, type_real, type_real> partial
+    * @return std::tuple<type_real, type_real, type_real, type_real, type_real,
+                            type_real, type_real, type_real, type_real> partial
  derivatives \f$ (\partial \xi/ \partial x, \partial \eta/ \partial
  * x,
  * \partial \xi/ \partial z, \partial \eta/ \partial z, \partial \gamma/
  \partial
  * z) \f$
  */
-std::tuple<type_real, type_real, type_real, type_real, type_real, type_real,
-           type_real, type_real, type_real>
-compute_derivatives(const specfem::kokkos::HostView2d<type_real> &s_coorg,
-                    const int ngnod, const type_real xi, const type_real eta,
-                    const type_real gamma);
+specfem::point::jacobian_matrix<specfem::dimension::type::dim3, true, false>
+compute_derivatives(
+    const Kokkos::View<
+        point::global_coordinates<specfem::dimension::type::dim3> *,
+        Kokkos::HostSpace> &coorg,
+    const int ngnod, const type_real xi, const type_real eta,
+    const type_real gamma);
 
 } // namespace specfem::jacobian
