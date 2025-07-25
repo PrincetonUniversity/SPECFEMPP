7373123e2e5b27fbd9e2e93eab224fea663bae39
ccui <ccui@princeton.edu>
2025-07-25 14:51:59 -0400

    Update 3D Jacobian.

--- core/specfem/jacobian/dim3/jacobian.cpp
+++ core/specfem/jacobian/dim3/jacobian.cpp
@@ -2,13 +2,39 @@
 #include "macros.hpp"
 #include "specfem/shape_functions.hpp"
 
-namespace specfem::jacobian::impl {
+specfem::point::global_coordinates<specfem::dimension::type::dim3>
+specfem::jacobian::compute_locations(
+    const Kokkos::View<
+        point::global_coordinates<specfem::dimension::type::dim3> *,
+        Kokkos::HostSpace> &coorg,
+    const int ngnod, const type_real xi, const type_real eta,
+    const type_real gamma) {
 
-std::tuple<type_real, type_real, type_real, type_real, type_real, type_real,
-           type_real, type_real, type_real>
-compute_jacobian_matrix3d(
-    const specfem::kokkos::HostView2d<type_real> &s_coorg, const int ngnod,
-    const std::vector<std::vector<type_real> > &dershape3D) {
+  auto shape3D = specfem::shape_function::shape_function(xi, eta, gamma, ngnod);
+
+  type_real xcor = 0.0;
+  type_real ycor = 0.0;
+  type_real zcor = 0.0;
+
+  for (int in = 0; in < ngnod; in++) {
+    xcor += shape3D[in] * coorg(in).x;
+    ycor += shape3D[in] * coorg(in).y;
+    zcor += shape3D[in] * coorg(in).z;
+  }
+
+  return { xcor, ycor, zcor };
+}
+
+specfem::point::jacobian_matrix<specfem::dimension::type::dim3, true, false>
+specfem::jacobian::compute_derivatives(
+    const Kokkos::View<
+        point::global_coordinates<specfem::dimension::type::dim3> *,
+        Kokkos::HostSpace> &coorg,
+    const int ngnod, const type_real xi, const type_real eta,
+    const type_real gamma) {
+  const auto dershape3D = specfem::shape_function::shape_function_derivatives(
+      xi, eta, gamma, ngnod);
+
   type_real xxi = 0.0;
   type_real yxi = 0.0;
   type_real zxi = 0.0;
@@ -20,76 +46,20 @@ compute_jacobian_matrix3d(
   type_real zgamma = 0.0;
 
   for (int in = 0; in < ngnod; in++) {
-    xxi += dershape3D[0][in] * s_coorg(0, in);
-    yxi += dershape3D[0][in] * s_coorg(1, in);
-    zxi += dershape3D[0][in] * s_coorg(2, in);
-    xeta += dershape3D[1][in] * s_coorg(0, in);
-    yeta += dershape3D[1][in] * s_coorg(1, in);
-    zeta += dershape3D[1][in] * s_coorg(2, in);
-    xgamma += dershape3D[2][in] * s_coorg(0, in);
-    ygamma += dershape3D[2][in] * s_coorg(1, in);
-    zgamma += dershape3D[2][in] * s_coorg(2, in);
+    xxi += dershape3D[0][in] * coorg(in).x;
+    yxi += dershape3D[0][in] * coorg(in).y;
+    zxi += dershape3D[0][in] * coorg(in).z;
+    xeta += dershape3D[1][in] * coorg(in).x;
+    yeta += dershape3D[1][in] * coorg(in).y;
+    zeta += dershape3D[1][in] * coorg(in).z;
+    xgamma += dershape3D[2][in] * coorg(in).x;
+    ygamma += dershape3D[2][in] * coorg(in).y;
+    zgamma += dershape3D[2][in] * coorg(in).z;
   }
 
-  return std::make_tuple(xxi, yxi, zxi, xeta, yeta, zeta, xgamma, ygamma,
-                         zgamma);
-}
-
-type_real
-compute_jacobian3d(const specfem::kokkos::HostView2d<type_real> &s_coorg,
-                   const int ngnod,
-                   const std::vector<std::vector<type_real> > &dershape3D) {
-  auto [xxi, yxi, zxi, xeta, yeta, zeta, xgamma, ygamma, zgamma] =
-      compute_jacobian_matrix3d(s_coorg, ngnod, dershape3D);
-  return xxi * (yeta * zgamma - ygamma * zeta) -
-         xeta * (yxi * zgamma - ygamma * zxi) +
-         xgamma * (yxi * zeta - yeta * zxi);
-};
-
-} // namespace specfem::jacobian::impl
-
-std::tuple<type_real, type_real, type_real>
-specfem::jacobian::compute_locations(
-    const specfem::kokkos::HostView2d<type_real> &s_coorg, const int ngnod,
-    const type_real xi, const type_real eta, const type_real gamma) {
-
-  auto shape3D = specfem::shape_function::shape_function(xi, eta, gamma, ngnod);
-
-  ASSERT(s_coorg.extent(0) == ndim, "Dimension mismatch");
-  ASSERT(s_coorg.extent(1) == ngnod, "Number of nodes mismatch");
-  ASSERT(shape3D.size() == ngnod, "Number of nodes mismatch");
-
-  type_real xcor = 0.0;
-  type_real ycor = 0.0;
-  type_real zcor = 0.0;
-
-  for (int in = 0; in < ngnod; in++) {
-    xcor += shape3D[in] * s_coorg(0, in);
-    ycor += shape3D[in] * s_coorg(1, in);
-    zcor += shape3D[in] * s_coorg(2, in);
-  }
-
-  return std::make_tuple(xcor, ycor, zcor);
-}
-
-std::tuple<type_real, type_real, type_real, type_real, type_real, type_real,
-           type_real, type_real, type_real>
-specfem::jacobian::compute_derivatives(
-    const specfem::kokkos::HostView2d<type_real> &s_coorg, const int ngnod,
-    const type_real xi, const type_real eta, const type_real gamma) {
-  const auto dershape3D = specfem::shape_function::shape_function_derivatives(
-      xi, eta, gamma, ngnod);
-
-  ASSERT(s_coorg.extent(0) == ndim, "Dimension mismatch");
-  ASSERT(s_coorg.extent(1) == ngnod, "Number of nodes mismatch");
-  ASSERT(dershape3D.size() == ndim, "Dimension mismatch");
-  ASSERT(dershape3D[0].size() == ngnod, "Number of nodes mismatch");
-
-  auto [xxi, yxi, zxi, xeta, yeta, zeta, xgamma, ygamma, zgamma] =
-      specfem::jacobian::impl::compute_jacobian_matrix3d(s_coorg, ngnod,
-                                                         dershape3D);
-  auto jacobian =
-      specfem::jacobian::impl::compute_jacobian3d(s_coorg, ngnod, dershape3D);
+  auto jacobian = xxi * (yeta * zgamma - ygamma * zeta) -
+                  xeta * (yxi * zgamma - ygamma * zxi) +
+                  xgamma * (yxi * zeta - yeta * zxi);
 
   type_real xix = (yeta * zgamma - ygamma * zeta) / jacobian;
   type_real xiy = (xgamma * zeta - xeta * zgamma) / jacobian;
@@ -101,6 +71,5 @@ specfem::jacobian::compute_derivatives(
   type_real gammay = (xeta * zxi - xxi * zeta) / jacobian;
   type_real gammaz = (xxi * yeta - xeta * yxi) / jacobian;
 
-  return std::make_tuple(xix, etax, gammax, xiy, etay, gammay, xiz, etaz,
-                         gammaz);
+  return { xix, etax, gammax, xiy, etay, gammay, xiz, etaz, gammaz, jacobian };
 }
