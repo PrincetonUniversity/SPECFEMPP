0a9db594409b3855d4f5b075eb8a2a7f96a3afac
Lucas Sawade <lsawade@princeton.edu>
2025-08-01 11:14:37 -0400

    Added new cookbook for the migration from specfem2d fortran to specfempp

--- docs/conf.py
+++ docs/conf.py
@@ -115,7 +115,7 @@ extlinks = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # These folders are copied to the documentation's HTML output
-html_static_path = ["sections/_static"]
+html_static_path = ["_static"]
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
