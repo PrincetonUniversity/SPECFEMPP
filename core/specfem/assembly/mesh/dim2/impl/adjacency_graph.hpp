1677e1c6a10b6b348e3a4c5f643c239bc97c5b61
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2025-08-22 15:19:07 -0400

    Update locate point using adjacency map
    - [x] Add adjacency map to assembly::mesh
    - [x] Update best candidates in locate point

---
+++ core/specfem/assembly/mesh/dim2/impl/adjacency_graph.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace specfem::assembly::mesh_impl {
+
+template <>
+class adjacency_graph<specfem::dimension::type::dim2>
+    : public specfem::mesh::adjacency_graph<specfem::dimension::type::dim2> {
+
+private:
+  using base_type =
+      specfem::mesh::adjacency_graph<specfem::dimension::type::dim2>;
+
+public:
+  using base_type::base_type;
+};
+
+} // namespace specfem::assembly::mesh_impl
