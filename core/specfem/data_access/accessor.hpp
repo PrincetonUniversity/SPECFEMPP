8ae8fe1fab02b9f0514c9fac7fd9dedddf88013a
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2025-08-25 14:15:49 -0400

    Added accessor and field for chunk edge
    - [x] Implements data access class for chunk edge
    - [x] Adds different fields withink chunk edge

--- core/specfem/data_access/accessor.hpp
+++ core/specfem/data_access/accessor.hpp
@@ -6,7 +6,7 @@
 #include <type_traits>
 
 namespace specfem::data_access {
-enum class AccessorType { point, chunk_element };
+enum class AccessorType { point, chunk_element, chunk_edge };
 
 template <specfem::data_access::AccessorType AccessorType,
           specfem::data_access::DataClassType DataClass,
@@ -70,6 +70,38 @@ struct Accessor<specfem::data_access::AccessorType::chunk_element, DataClass,
       Kokkos::MemoryTraits<Kokkos::Unmanaged> >;
 };
 
+template <specfem::data_access::DataClassType DataClass,
+          specfem::dimension::type DimensionTag, bool UseSIMD>
+struct Accessor<specfem::data_access::AccessorType::chunk_edge, DataClass,
+                DimensionTag, UseSIMD> {
+  constexpr static auto accessor_type =
+      specfem::data_access::AccessorType::chunk_edge;
+  constexpr static auto data_class = DataClass;
+  constexpr static auto dimension_tag = DimensionTag;
+  constexpr static bool using_simd = UseSIMD;
+
+  template <typename T>
+  using simd = specfem::datatype::simd<T, UseSIMD>; ///< SIMD data type
+
+  template <typename T, int nelements, int ngll>
+  using scalar_type =
+      Kokkos::View<typename simd<T>::datatype[nelements][ngll],
+                   Kokkos::DefaultExecutionSpace::scratch_memory_space,
+                   Kokkos::MemoryTraits<Kokkos::Unmanaged> >;
+
+  template <typename T, int nelements, int ngll, int components>
+  using vector_type =
+      Kokkos::View<typename simd<T>::datatype[nelements][ngll][components],
+                   Kokkos::DefaultExecutionSpace::scratch_memory_space,
+                   Kokkos::MemoryTraits<Kokkos::Unmanaged> >;
+
+  template <typename T, int nelements, int ngll, int components, int dimension>
+  using tensor_type = Kokkos::View<
+      typename simd<T>::datatype[nelements][ngll][components][dimension],
+      Kokkos::DefaultExecutionSpace::scratch_memory_space,
+      Kokkos::MemoryTraits<Kokkos::Unmanaged> >;
+};
+
 template <typename T, typename = void> struct is_point : std::false_type {};
 
 template <typename T>
@@ -84,6 +116,15 @@ template <typename T>
 struct is_chunk_element<
     T, std::enable_if_t<T::accessor_type ==
                         specfem::data_access::AccessorType::chunk_element> >
+    : std::true_type {};
+
+template <typename T, typename = void>
+struct is_chunk_edge : std::false_type {};
+
+template <typename T>
+struct is_chunk_edge<
+    T, std::enable_if_t<T::accessor_type ==
+                        specfem::data_access::AccessorType::chunk_edge> >
     : std::true_type {};
 
 template <typename T, typename = void> struct is_accessor : std::false_type {};
