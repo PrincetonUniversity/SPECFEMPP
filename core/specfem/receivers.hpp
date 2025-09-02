e1524efd25de2bc8d5ab1f928bb18a5ed4da49a3
Lucas Sawade <lsawade@princeton.edu>
2025-09-02 16:51:34 -0400

    Added receiver template specializations to the docs.

--- core/specfem/receivers.hpp
+++ core/specfem/receivers.hpp
@@ -5,6 +5,11 @@ namespace specfem::receivers {
 /**
  * @brief Receiver Class
  *
+ * This class is responsible for handling the reception of seismic data at a
+ * specific station.
+ *
+ * @tparam DimensionTag The dimension tag (2D or 3D) for the receiver.
+ *
  */
 template <specfem::dimension::type DimensionTag> class receiver;
 
