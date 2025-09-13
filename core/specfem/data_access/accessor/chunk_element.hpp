805f7e63a16a561913ed57dadc51ca0461441578
ccui <ccui@princeton.edu>
2025-09-13 07:19:34 -0400

    Add datatype/interface.hpp

--- core/specfem/data_access/accessor/chunk_element.hpp
+++ core/specfem/data_access/accessor/chunk_element.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "datatypes/simd.hpp"
+#include "datatypes/interface.hpp"
 #include "enumerations/interface.hpp"
 #include "specfem/data_access/accessor.hpp"
 #include <Kokkos_Core.hpp>
@@ -28,16 +28,16 @@ struct Accessor<specfem::data_access::AccessorType::chunk_element, DataClass,
                    Kokkos::MemoryTraits<Kokkos::Unmanaged> >;
 
   template <typename T, int nelements, int ngll, int components>
-  using vector_type = typename Kokkos::View<
-      typename simd<T>::datatype[nelements][ngll][ngll][components],
+  using vector_type = specfem::datatype::VectorChunkViewType<
+      T, nelements, ngll, components,
       Kokkos::DefaultExecutionSpace::scratch_memory_space,
-      Kokkos::MemoryTraits<Kokkos::Unmanaged> >;
+      Kokkos::MemoryTraits<Kokkos::Unmanaged>, UseSIMD>;
 
   template <typename T, int nelements, int ngll, int components, int dimension>
-  using tensor_type = Kokkos::View<
-      typename simd<T>::datatype[nelements][ngll][ngll][components][dimension],
+  using tensor_type = specfem::datatype::TensorChunkViewType<
+      T, nelements, ngll, components, dimension,
       Kokkos::DefaultExecutionSpace::scratch_memory_space,
-      Kokkos::MemoryTraits<Kokkos::Unmanaged> >;
+      Kokkos::MemoryTraits<Kokkos::Unmanaged>, UseSIMD>;
 };
 
 template <typename T, typename = void>
---
+++ include/datatypes/interface.hpp
@@ -0,0 +1,3 @@
+#include "chunk_element_view.hpp"
+#include "point_view.hpp"
+#include "simd.hpp"
