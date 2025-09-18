33fee8f02688647f281e84c3839a132d7ff881eb
Kentaro Hanson <kh3372@princeton.edu>
2025-09-18 19:42:33 -0400

    populate functions + add test

--- core/specfem/assembly/nonconforming_interfaces/dim2/impl/compute_intersection_in_assembly.cpp
+++ core/specfem/assembly/nonconforming_interfaces/dim2/impl/compute_intersection_in_assembly.cpp
@@ -1,14 +1,21 @@
 
+#include "compute_intersection_in_assembly.hpp"
 #include "compute_intersection.hpp"
 #include "enumerations/mesh_entities.hpp"
-#include "compute_intersection_in_assembly.hpp"
 #include "specfem_setup.hpp"
 #include <sstream>
 #include <stdexcept>
 
 template <typename EdgeType>
-std::vector<std::pair<type_real, type_real> >
-specfem::assembly::nonconforming_interfaces_impl::compute_intersection(
+inline std::tuple<
+    Kokkos::View<
+        specfem::point::global_coordinates<specfem::dimension::type::dim2> *,
+        Kokkos::HostSpace>,
+    Kokkos::View<
+        specfem::point::global_coordinates<specfem::dimension::type::dim2> *,
+        Kokkos::HostSpace>,
+    specfem::mesh_entity::type, specfem::mesh_entity::type>
+_expand_edge_index(
     const specfem::assembly::mesh<specfem::dimension::type::dim2> &mesh,
     const EdgeType &edge,
     const Kokkos::View<type_real *, Kokkos::HostSpace> &mortar_quadrature) {
@@ -42,8 +49,51 @@ specfem::assembly::nonconforming_interfaces_impl::compute_intersection(
     coorg1(i).x = mesh.h_control_node_coord(0, ispec, i);
     coorg2(i).z = mesh.h_control_node_coord(1, jspec, i);
   }
+  return { coorg1, coorg2, iorientation, jorientation };
+}
 
+template <typename EdgeType>
+std::vector<std::pair<type_real, type_real> >
+specfem::assembly::nonconforming_interfaces_impl::compute_intersection(
+    const specfem::assembly::mesh<specfem::dimension::type::dim2> &mesh,
+    const EdgeType &edge, const Kokkos::View<type_real *> &mortar_quadrature) {
+  const auto edgeinfo = expand_edge_index(mesh, edge);
+  return specfem::assembly::nonconforming_interfaces_impl::compute_intersection(
+      std::get<0>(edgeinfo), std::get<1>(edgeinfo), std::get<2>(edgeinfo),
+      std::get<2>(edgeinfo), mortar_quadrature);
+}
+
+template <typename EdgeType>
+void set_transfer_functions(
+    const specfem::assembly::mesh<specfem::dimension::type::dim2> &mesh,
+    const EdgeType &edge,
+    const Kokkos::View<type_real *, Kokkos::HostSpace> &mortar_quadrature,
+    const Kokkos::View<type_real *, Kokkos::HostSpace> &element_quadrature,
+    Kokkos::View<type_real **, Kokkos::HostSpace> &transfer_function1,
+    Kokkos::View<type_real **, Kokkos::HostSpace> &transfer_function2) {
+  const auto edgeinfo = expand_edge_index(mesh, edge);
   return specfem::assembly::nonconforming_interfaces_impl::
-      compute_intersection(coorg1, coorg2, iorientation, jorientation,
-                           mortar_quadrature);
+      set_transfer_functions(
+          std::get<0>(edgeinfo), std::get<1>(edgeinfo), std::get<2>(edgeinfo),
+          std::get<2>(edgeinfo), mortar_quadrature, mortar_quadrature,
+          element_quadrature, transfer_function1, transfer_function2);
+}
+
+template <typename EdgeType>
+void set_transfer_functions(
+    const specfem::assembly::mesh<specfem::dimension::type::dim2> &mesh,
+    const EdgeType &edge,
+    const Kokkos::View<type_real *, Kokkos::HostSpace> &mortar_quadrature,
+    const Kokkos::View<type_real *, Kokkos::HostSpace> &element_quadrature,
+    Kokkos::View<type_real **, Kokkos::HostSpace> &transfer_function1,
+    Kokkos::View<type_real **, Kokkos::HostSpace> &transfer_function1_prime,
+    Kokkos::View<type_real **, Kokkos::HostSpace> &transfer_function2,
+    Kokkos::View<type_real **, Kokkos::HostSpace> &transfer_function2_prime) {
+  const auto edgeinfo = expand_edge_index(mesh, edge);
+  return specfem::assembly::nonconforming_interfaces_impl::
+      set_transfer_functions(std::get<0>(edgeinfo), std::get<1>(edgeinfo),
+                             std::get<2>(edgeinfo), std::get<2>(edgeinfo),
+                             mortar_quadrature, element_quadrature,
+                             transfer_function1, transfer_function1_prime,
+                             transfer_function2, transfer_function2_prime);
 }
