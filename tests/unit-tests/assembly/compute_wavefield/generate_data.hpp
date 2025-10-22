5ed7ed2b752c1a50935b81c4e29176aac457a931
ccui <ccui@princeton.edu>
2025-10-22 01:33:42 -0400

    Remove comment.

--- tests/unit-tests/assembly/compute_wavefield/generate_data.hpp
+++ tests/unit-tests/assembly/compute_wavefield/generate_data.hpp
@@ -50,10 +50,6 @@ void generate_data(
     for (int ix = 0; ix < ngllx; ix++) {
       const IndexType index(ispec, iz, ix);
 
-      // PointDisplacementType displacement(1.0);
-      // PointVelocityType velocity(1.0);
-      // PointAccelerationType acceleration(1.0);
-
       PointDisplacementType displacement(1.1 * iz + 2.6 * ix);
       if constexpr (num_components == 2) {
         displacement(1) = 1.7 * iz + 2.3;
