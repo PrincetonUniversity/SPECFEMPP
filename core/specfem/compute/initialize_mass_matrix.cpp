454eb0fd63207faac889239338db2c7441027c0f
ccui <ccui@princeton.edu>
2026-01-24 21:41:55 -0500

    Move init mass matrix cpp.

---
+++ core/specfem/compute/initialize_mass_matrix.cpp
@@ -0,0 +1,81 @@
+#include "initialize_mass_matrix.hpp"
+#include "enumerations/interface.hpp"
+#include "impl/compute_mass_matrix.hpp"
+#include "impl/compute_mass_matrix.tpp"
+#include "impl/invert_mass_matrix.hpp"
+#include "impl/invert_mass_matrix.tpp"
+
+// Explicit template instantiation
+// 2D, NGLL=5,8, forward, backward and adjoint wavefields
+template void specfem::compute::initialize_mass_matrix<
+    specfem::wavefield::simulation_field::forward,
+    specfem::dimension::type::dim2, 5>(
+    specfem::assembly::assembly<specfem::dimension::type::dim2> &,
+    const type_real &);
+
+template void specfem::compute::initialize_mass_matrix<
+    specfem::wavefield::simulation_field::forward,
+    specfem::dimension::type::dim2, 8>(
+    specfem::assembly::assembly<specfem::dimension::type::dim2> &,
+    const type_real &);
+
+template void specfem::compute::initialize_mass_matrix<
+    specfem::wavefield::simulation_field::backward,
+    specfem::dimension::type::dim2, 5>(
+    specfem::assembly::assembly<specfem::dimension::type::dim2> &,
+    const type_real &);
+
+template void specfem::compute::initialize_mass_matrix<
+    specfem::wavefield::simulation_field::backward,
+    specfem::dimension::type::dim2, 8>(
+    specfem::assembly::assembly<specfem::dimension::type::dim2> &,
+    const type_real &);
+
+template void specfem::compute::initialize_mass_matrix<
+    specfem::wavefield::simulation_field::adjoint,
+    specfem::dimension::type::dim2, 5>(
+    specfem::assembly::assembly<specfem::dimension::type::dim2> &,
+    const type_real &);
+
+template void specfem::compute::initialize_mass_matrix<
+    specfem::wavefield::simulation_field::adjoint,
+    specfem::dimension::type::dim2, 8>(
+    specfem::assembly::assembly<specfem::dimension::type::dim2> &,
+    const type_real &);
+
+// 3D, NGLL=5,8, forward, backward and adjoint wavefields
+template void specfem::compute::initialize_mass_matrix<
+    specfem::wavefield::simulation_field::forward,
+    specfem::dimension::type::dim3, 5>(
+    specfem::assembly::assembly<specfem::dimension::type::dim3> &,
+    const type_real &);
+
+template void specfem::compute::initialize_mass_matrix<
+    specfem::wavefield::simulation_field::forward,
+    specfem::dimension::type::dim3, 8>(
+    specfem::assembly::assembly<specfem::dimension::type::dim3> &,
+    const type_real &);
+
+template void specfem::compute::initialize_mass_matrix<
+    specfem::wavefield::simulation_field::backward,
+    specfem::dimension::type::dim3, 5>(
+    specfem::assembly::assembly<specfem::dimension::type::dim3> &,
+    const type_real &);
+
+template void specfem::compute::initialize_mass_matrix<
+    specfem::wavefield::simulation_field::backward,
+    specfem::dimension::type::dim3, 8>(
+    specfem::assembly::assembly<specfem::dimension::type::dim3> &,
+    const type_real &);
+
+template void specfem::compute::initialize_mass_matrix<
+    specfem::wavefield::simulation_field::adjoint,
+    specfem::dimension::type::dim3, 5>(
+    specfem::assembly::assembly<specfem::dimension::type::dim3> &,
+    const type_real &);
+
+template void specfem::compute::initialize_mass_matrix<
+    specfem::wavefield::simulation_field::adjoint,
+    specfem::dimension::type::dim3, 8>(
+    specfem::assembly::assembly<specfem::dimension::type::dim3> &,
+    const type_real &);
