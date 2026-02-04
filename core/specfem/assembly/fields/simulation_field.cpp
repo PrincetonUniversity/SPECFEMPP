8ccbbfe409ea74b99a9a0d84b289abfef424f295
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2026-02-04 10:23:05 -0500

    Add missing file

---
+++ core/specfem/assembly/fields/simulation_field.cpp
@@ -0,0 +1,29 @@
+#include "dim2/simulation_field.tpp"
+#include "dim3/simulation_field.tpp"
+#include "enumerations/interface.hpp"
+#include "specfem/assembly/fields.hpp"
+#include <boost/preprocessor.hpp>
+
+#define DIMENSION_TAGS                                                         \
+  (specfem::dimension::type::dim2)(specfem::dimension::type::dim3)
+
+#define WAVEFIELD_TYPES                                                        \
+  (specfem::simulation::field_type::forward)(                                  \
+      specfem::simulation::field_type::adjoint)(                               \
+      specfem::simulation::field_type::backward)(                              \
+      specfem::simulation::field_type::buffer)
+
+#define INSTANTIATE_SIMULATION_FIELD(r, elem)                                  \
+  template class specfem::assembly::simulation_field<                          \
+      BOOST_PP_SEQ_ELEM(0, elem), BOOST_PP_SEQ_ELEM(1, elem)>;
+
+#define INSTANTIATE_SIMULATION_COPY_MEMBERS(r, elem)                           \
+  template void specfem::assembly::simulation_field<                           \
+      BOOST_PP_SEQ_ELEM(0, elem), BOOST_PP_SEQ_ELEM(1, elem)>::copy_to_host(); \
+                                                                               \
+  template void specfem::assembly::simulation_field<                           \
+      BOOST_PP_SEQ_ELEM(0, elem),                                              \
+      BOOST_PP_SEQ_ELEM(1, elem)>::copy_to_device();
+
+BOOST_PP_SEQ_FOR_EACH_PRODUCT(INSTANTIATE_SIMULATION_FIELD,
+                              (DIMENSION_TAGS)(WAVEFIELD_TYPES))
