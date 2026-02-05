6ceeaf0ba35febba7570d508ab415646b3e08db6
ccui <ccui@princeton.edu>
2026-02-05 14:59:20 -0500

    Move element_coupling.

--- core/specfem/element_coupling.hpp
+++ core/specfem/element_coupling.hpp
@@ -203,25 +203,6 @@ struct attributes<specfem::element::dimension_tag::dim2,
                                    false>; ///< vector displacement
 };
 
-/**
- * @brief Convert interface tag to string.
- * @param interface_tag Interface coupling type
- * @return String representation ("elastic_acoustic" or "acoustic_elastic")
- */
-std::string to_string(const interface_tag &interface_tag);
-
-/**
- * @brief Convert flux scheme tag to string.
- *
- * @param flux_scheme_tag
- * @return String representation
- */
-std::string to_string(const flux_scheme_tag &flux_scheme_tag);
-
-std::ostream &
-operator<<(std::ostream &stream,
-           const specfem::element_coupling::interface_tag &interface_tag);
-std::ostream &
-operator<<(std::ostream &stream,
-           const specfem::element_coupling::flux_scheme_tag &flux_scheme_tag);
 } // namespace specfem::element_coupling
+
+#include "element_coupling/to_string.hpp"
