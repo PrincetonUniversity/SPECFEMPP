75e3dfdc8a9e3abebb19ff0d0cba81ee020860ec
Kentaro Hanson <kh3372@princeton.edu>
2026-08-24 20:38:33 -0400

    fix: local_point_on_face_test after local-face-coord swap

--- tests/unit-tests/algorithms/dim3/locate_point_on_face_test.cpp
+++ tests/unit-tests/algorithms/dim3/locate_point_on_face_test.cpp
@@ -1,4 +1,6 @@
 #include "locate_point_fixture.hpp"
+#include <gtest/gtest.h>
+#include <sstream>
 
 using specfem::utilities::is_close;
 
@@ -32,7 +34,7 @@ TEST_F(LocatePoint3D, LocateFaceOnCoreUnitCube) {
   for (auto [side, target] :
        std::vector<std::pair<specfem::mesh_entity::dim3::type,
                              specfem::point::global_coordinates<
-                                 specfem::element::dimension_tag::dim3> > >{
+                                 specfem::element::dimension_tag::dim3>>>{
            { specfem::mesh_entity::dim3::type::bottom,
              { 0.5, 0.5, 0.0 } }, // Bottom
                                   // edge
@@ -157,29 +159,61 @@ TEST_F(LocatePoint3D, LocateFaceOnCoreOutsideMesh) {
 
   // Test corner point (1.5, -0.5, 0.25) should map to (2, -2, -0.5),
   // out-of-bounds for bottom, local coord will clamp to (1, -1, -0.5)
+  // remember: for local face coordinates, second coordinate first.
   target = { 1.5, -0.5, 0.25 };
-  auto result =
-      specfem::algorithms::locate_point_impl::get_local_face_coordinate(
-          target, coorg, specfem::mesh_entity::dim3::type::right, { 0, 0 });
-  EXPECT_TRUE(is_close(result.first.first, type_real{ -1 }))
-      << expected_got(0.0, result.first.first);
-  EXPECT_TRUE(is_close(result.first.second, type_real{ -0.5 }))
-      << expected_got(0.0, result.first.second);
-  EXPECT_FALSE(result.second);
+  type_real target_clamped[3] = { 1, -1, -0.5 };
+  std::vector<std::string> axis_names = { "xi", "eta", "gamma" };
+  std::vector<specfem::mesh_entity::dim3::type> sides_to_test = {
+    specfem::mesh_entity::dim3::type::right,
+    specfem::mesh_entity::dim3::type::front,
+    specfem::mesh_entity::dim3::type::bottom
+  };
+  // face-local to element-local for each side in sides_to_test.
+  std::vector<std::array<int, 2>> local_axes = { { 2, 1 }, { 2, 0 }, { 1, 0 } };
 
-  result = specfem::algorithms::locate_point_impl::get_local_face_coordinate(
-      target, coorg, specfem::mesh_entity::dim3::type::front, { 0, 0 });
-  EXPECT_TRUE(is_close(result.first.first, type_real{ 1 }))
-      << expected_got(0.0, result.first.first);
-  EXPECT_TRUE(is_close(result.first.second, type_real{ -0.5 }))
-      << expected_got(0.0, result.first.second);
-  EXPECT_FALSE(result.second);
+  std::ostringstream fail_case_expand;
+  bool fails = false;
+  for (int iside = 0; iside < sides_to_test.size(); iside++) {
+    int axis[2] = { local_axes[iside][0], local_axes[iside][1] };
+    type_real expect[2] = { target_clamped[axis[0]], target_clamped[axis[1]] };
 
-  result = specfem::algorithms::locate_point_impl::get_local_face_coordinate(
-      target, coorg, specfem::mesh_entity::dim3::type::bottom, { 0, 0 });
-  EXPECT_TRUE(is_close(result.first.first, type_real{ 1 }))
-      << expected_got(0.0, result.first.first);
-  EXPECT_TRUE(is_close(result.first.second, type_real{ -1 }))
-      << expected_got(0.0, result.first.second);
-  EXPECT_FALSE(result.second);
+    auto result =
+        specfem::algorithms::locate_point_impl::get_local_face_coordinate(
+            target, coorg, sides_to_test[iside], { 0, 0 });
+
+    bool correct_coords = is_close(result.first.first, expect[0]) &&
+                          is_close(result.first.second, expect[1]);
+    bool correct_isinside_flag = !result.second;
+
+    if (!(correct_coords && correct_isinside_flag)) {
+      fails = true;
+      fail_case_expand
+          << "  - Failure on get_local_face_coordinate() for right side:\n";
+      if (!correct_coords) {
+        fail_case_expand << "   [✗] Expects: (" << axis_names[axis[0]] << " = "
+                         << expect[0] << ", " << axis_names[axis[1]] << " = "
+                         << expect[1] << ")\n"
+                         << "           Got: (" << result.first.first << ","
+                         << result.first.second << ")\n";
+      } else {
+        fail_case_expand << "   [✓] Got correct coords (" << axis_names[axis[0]]
+                         << " = " << expect[0] << ", " << axis_names[axis[1]]
+                         << " = " << expect[1] << ")\n";
+      }
+      if (result.second) {
+        fail_case_expand
+            << "   [✗] Expected out-of-bounds. Second return value was instead "
+               "true (is inside face)\n";
+      } else {
+        fail_case_expand
+            << "   [✓] Expected out-of-bounds. Second return value correctly "
+               "false (is not in face)\n";
+      }
+    }
+  }
+
+  if (fails) {
+    ADD_FAILURE() << "global(1.5, -0.5, 0.25) -> local(2, -2, -0.5)\n"
+                  << fail_case_expand.str();
+  }
 }
